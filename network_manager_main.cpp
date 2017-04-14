3ae8f280ab703ed7d43b9b4852fe6ca18d1974ea
Ratan Gupta <ratagupt@in.ibm.com>
2017-04-14 17:46:05 +0530

    Refactor: Implementing the main function
    Resolves openbmc/openbmc#1293
    
    Change-Id: I1ba38b0c273538f55dd9d4305541989e72deb839
    Signed-off-by: Ratan Gupta <ratagupt@in.ibm.com>

--- network_manager_main.cpp
+++ network_manager_main.cpp
@@ -1,5 +1,24 @@
+#include <sdbusplus/bus.hpp>
+#include <sdbusplus/server/manager.hpp>
+#include "config.h"
+#include "network_manager.hpp"
 
 int main(int argc, char *argv[])
 {
+    auto bus = sdbusplus::bus::new_default();
+
+    // Add sdbusplus Object Manager for the 'root' path of the network manager.
+    sdbusplus::server::manager::manager objManager(bus, OBJ_NETWORK);
+
+    phosphor::network::Manager manager(bus, OBJ_NETWORK);
+
+    bus.request_name(BUSNAME_NETWORK);
+
+    while(true)
+    {
+        bus.process_discard();
+        bus.wait();
+    }
+
     return 0;
 }
