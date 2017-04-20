18b6b6d2628397f91bab16eadcd701184fc91274
Ratan Gupta <ratagupt@in.ibm.com>
2017-04-20 19:19:15 +0530

    Refactor: Implementation of the ipaddress
    Change-Id: Ic4d7953c5aca2bf85a609c736e5c817322d29e4b
    Signed-off-by: Ratan Gupta <ratagupt@in.ibm.com>

---
+++ ipaddress.cpp
@@ -0,0 +1,38 @@
+#include "ipaddress.hpp"
+#include "ethernet_interface.hpp"
+
+#include <phosphor-logging/log.hpp>
+
+namespace phosphor
+{
+namespace network
+{
+
+using namespace phosphor::logging;
+
+IPAddress::IPAddress(sdbusplus::bus::bus& bus,
+          const char* objPath,
+          EthernetInterface& parent,
+          IPProtocol::Protocol type,
+          std::string& ipaddress,
+          uint8_t prefixLength,
+          std::string& gateway):
+          IPIfaces(bus,objPath,true),
+          parent(parent)
+{
+   this->address(ipaddress);
+   this->prefixLength(prefixLength);
+   this->gateway(gateway);
+   this->type(type);
+   // Emit deferred signal.
+   this->emit_object_added();
+}
+
+
+void IPAddress::delete_()
+{
+
+}
+
+}//namespace network
+}//namespace phosphor
