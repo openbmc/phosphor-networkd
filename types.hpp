a2416da7436ff36368f909e1127187d12e2b5d3e
Ratan Gupta <ratagupt@in.ibm.com>
2017-04-21 08:45:23 +0530

    Refactor: Implement create interface for ipaddress
    Change-Id: Ia4598c27c11667dafb70a8af58871661b7042d0f
    Signed-off-by: Ratan Gupta <ratagupt@in.ibm.com>

---
+++ types.hpp
@@ -0,0 +1,23 @@
+#include <list>
+#include <string>
+#include <vector>
+
+namespace phosphor
+{
+namespace network
+{
+
+using IntfName = std::string;
+
+struct AddrInfo {
+    uint8_t addrType;
+    std::string ipaddress;
+    uint16_t prefix;
+};
+
+using AddrList = std::list<AddrInfo>;
+using IntfAddrMap = std::map<IntfName, AddrList>;
+
+
+}//namespace network
+}//namespace phosphor
