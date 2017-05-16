9fa75df0f417814319fbe10be3528e612477b2e1
Ratan Gupta <ratagupt@in.ibm.com>
2017-05-16 22:48:35 +0530

    test: Add unit test cases for util
    Change-Id: I75cc1c1642bd4160191853d6ebfa5427e7f534c3
    Signed-off-by: Ratan Gupta <ratagupt@in.ibm.com>

---
+++ test/test_util.cpp
@@ -0,0 +1,89 @@
+#include <gtest/gtest.h>
+#include <netinet/in.h>
+#include "util.hpp"
+
+namespace phosphor
+{
+namespace network
+{
+
+class TestUtil : public testing::Test
+{
+    public:
+        TestUtil()
+        {
+            // Empty
+        }
+
+};
+
+TEST_F(TestUtil, ConvertV4MasktoPrefix)
+{
+    std::string mask = "255.255.255.0";
+    uint8_t prefix = toCidr(AF_INET, mask);
+    EXPECT_EQ(prefix, 24);
+
+    mask = "255.255.0.0";
+    prefix = toCidr(AF_INET, mask);
+    EXPECT_EQ(prefix, 16);
+
+    mask = "255.0.0.0";
+    prefix = toCidr(AF_INET, mask);
+    EXPECT_EQ(prefix, 8);
+
+    mask = "255.224.0.0";
+    prefix = toCidr(AF_INET, mask);
+    EXPECT_EQ(prefix, 11);
+
+    //Invalid Mask
+    mask = "255.0.255.0";
+    prefix = toCidr(AF_INET, mask);
+    EXPECT_EQ(prefix, 0);
+}
+
+TEST_F(TestUtil, convertV6MasktoPrefix)
+{
+    std::string mask = "ffff:ffff::";
+    uint8_t prefix = toCidr(AF_INET6, mask);
+    EXPECT_EQ(prefix, 32);
+
+    mask = "ffff:ffff:ffff::";
+    prefix = toCidr(AF_INET6, mask);
+    EXPECT_EQ(prefix, 48);
+
+    mask = "ffff:ffff:fc00::";
+    prefix = toCidr(AF_INET6, mask);
+    EXPECT_EQ(prefix, 38);
+
+    //Invalid Mask
+    mask = "ffff:0fff::";
+    prefix = toCidr(AF_INET6, mask);
+    EXPECT_EQ(prefix, 0);
+}
+
+TEST_F(TestUtil, isLinLocaladdress)
+{
+    std::string ipaddress = "fe80:fec0::";
+    EXPECT_TRUE(isLinkLocal(ipaddress));
+
+    ipaddress = "2000:4567:789::";
+    EXPECT_FALSE(isLinkLocal(ipaddress));
+
+    ipaddress = "2000:fe80::";
+    EXPECT_FALSE(isLinkLocal(ipaddress));
+}
+
+TEST_F(TestUtil, convertPrefixToMask)
+{
+    std::string mask = toMask(AF_INET, 24);
+    EXPECT_EQ(mask, "255.255.255.0");
+
+    mask = toMask(AF_INET, 8);
+    EXPECT_EQ(mask, "255.0.0.0");
+
+    mask = toMask(AF_INET, 27);
+    EXPECT_EQ(mask, "255.255.255.224");
+}
+
+}// namespce network
+}// namespace phosphor
