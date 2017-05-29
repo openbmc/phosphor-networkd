d9492bec293fc0c93b05c3c977d57df6f6e3bae4
Ratan Gupta <ratagupt@in.ibm.com>
2017-05-29 08:41:48 +0530

    Implement the network utility function
    This function fetches the network address from the
    given ip address and the prefix.
    
    Change-Id: I5e46ce7b371360c90ef74fac175c8379c6f31d4c
    Signed-off-by: Ratan Gupta <ratagupt@in.ibm.com>

--- util.cpp
+++ util.cpp
@@ -45,7 +45,7 @@ uint8_t toV6Cidr(const std::string& subnetMask)
         if (sscanf(str.c_str(), "%x", &buff) <= 0)
         {
             log<level::ERR>("Invalid Mask",
-                             entry("Mask=%s", subnetMask));
+                            entry("Mask=%s", subnetMask));
 
             return 0;
         }
@@ -91,7 +91,7 @@ uint8_t toCidr(int addressFamily, const std::string& subnetMask)
     if (rc <= 0)
     {
         log<level::ERR>("inet_pton failed:",
-                         entry("SUBNETMASK=%s", subnetMask));
+                        entry("SUBNETMASK=%s", subnetMask));
         return 0;
     }
 
@@ -104,12 +104,12 @@ uint8_t toCidr(int addressFamily, const std::string& subnetMask)
     else
     {
         log<level::ERR>("Invalid Mask",
-                         entry("SUBNETMASK=%s", subnetMask));
+                        entry("SUBNETMASK=%s", subnetMask));
         return 0;
     }
 }
 
-std::string toMask(int addressFamily,uint8_t prefix)
+std::string toMask(int addressFamily, uint8_t prefix)
 {
     if (addressFamily == AF_INET6)
     {
@@ -117,24 +117,89 @@ std::string toMask(int addressFamily,uint8_t prefix)
         return "";
     }
 
-    if ( prefix < 1 || prefix > 30 ) {
-        fprintf(stderr,"Invalid net mask bits (1-30): %d\n",prefix);
+    if (prefix < 1 || prefix > 30)
+    {
+        fprintf(stderr, "Invalid net mask bits (1-30): %d\n", prefix);
         exit(1);
     }
     /* Create the netmask from the number of bits */
     unsigned long mask = 0;
-    for ( auto i = 0 ; i < prefix ; i++ )
-        mask |= 1<<(31-i);
+    for (auto i = 0 ; i < prefix ; i++)
+    {
+        mask |= 1 << (31 - i);
+    }
     struct in_addr netmask;
     netmask.s_addr = htonl(mask);
     return inet_ntoa(netmask);
 }
 
+std::string getNetwork(int addressFamily, const std::string& ipaddress,
+                       uint8_t prefix)
+{
+    unsigned char* pntMask;
+    unsigned char* pntNetwork;
+    int bit;
+    int offset;
+    struct in6_addr netmask;
+    const u_char maskbit[] = {0x00, 0x80, 0xc0, 0xe0, 0xf0,
+                              0xf8, 0xfc, 0xfe, 0xff
+                             };
+    std::cout << "IPADDRESS=" << ipaddress << "\n";
+
+    // Convert prefix into netmask network address
+    memset(&netmask, 0, sizeof(struct in6_addr));
+    pntMask = reinterpret_cast<unsigned char*>(&netmask);
+
+    offset = prefix / 8;
+    bit = prefix % 8;
+
+    while (offset--)
+    {
+        *pntMask++ = 0xff;
+    }
+
+    if (bit)
+    {
+        *pntMask = maskbit[bit];
+    }
+
+    // convert ipaddres string  into network address
+    struct in6_addr ipaddressNetwork;
+    if (inet_pton(addressFamily, ipaddress.c_str(), &ipaddressNetwork) <= 0)
+    {
+        std::cout << "inet_pton failure\n";
+        log<level::ERR>("inet_pton failure");
+        return "";
+    }
+
+    // Now bit wise and gets you the network address
+    pntMask = reinterpret_cast<unsigned char*>(&netmask);
+    pntNetwork = reinterpret_cast<unsigned char*>(&ipaddressNetwork);
+
+    for (int i = 0; i < 16 ; i++)
+    {
+        pntNetwork[i] = pntNetwork[i] & pntMask[i];
+    }
+
+    //convert the network address into string fomat.
+    char networkString[INET6_ADDRSTRLEN];
+    if (inet_ntop(addressFamily, &ipaddressNetwork , networkString,
+                  INET6_ADDRSTRLEN) == NULL)
+    {
+        log<level::ERR>("inet_ntop failure");
+        std::cout << "inet_ntop failure\n";
+
+    }
+    std::cout << "NetworkAddress=" << networkString << "\n";
+    return networkString;
+}
+
 bool isLinkLocal(const std::string& address)
 {
     std::string linklocal = "fe80";
-    return
-    std::mismatch(linklocal.begin(),linklocal.end(),address.begin()).first == linklocal.end() ? true : false;
+    return  std::mismatch(linklocal.begin(), linklocal.end(),
+                          address.begin()).first == linklocal.end() ? true :
+            false;
 }
 
 }//namespace network
--- util.hpp
+++ util.hpp
@@ -7,16 +7,22 @@ namespace phosphor
 namespace network
 {
 
-/** @brief converts the given subnet into prefix notation **/    
+/** @brief converts the given subnet into prefix notation **/
 uint8_t toCidr(int addressFamily,  const std::string& mask);
 
 /** @brief converts the prefix into subnetmask **/
-std::string toMask(int addressFamily,uint8_t prefix);
+std::string toMask(int addressFamily, uint8_t prefix);
 
 /* @brief checks that the given ip address is link local or not.
  *  @returns true if it is linklocal otherwise false
  */
 bool isLinkLocal(const std::string& address);
+
+/* @brief checks that the given ip address is link local or not.
+ *  @returns true if it is linklocal otherwise false
+ */
+std::string getNetwork(int addressFamily, const std::string& ipaddress,
+                       uint8_t prefix);
 
 } //namespace network
 
@@ -31,13 +37,13 @@ class Descriptor
         Descriptor(const Descriptor&) = delete;
         Descriptor& operator=(const Descriptor&) = delete;
         Descriptor(Descriptor&&) = delete;
-        Descriptor& operator=(Descriptor&&) = delete;
+        Descriptor& operator=(Descriptor &&) = delete;
 
-        Descriptor(int fd) : fd(fd){}
+        Descriptor(int fd) : fd(fd) {}
 
         ~Descriptor()
         {
-            if (fd >=0)
+            if (fd >= 0)
             {
                 close(fd);
             }
