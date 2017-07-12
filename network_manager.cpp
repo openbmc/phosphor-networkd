166f63788cddd03e009a7922252857e8637afac3
Ratan Gupta <ratagupt@in.ibm.com>
2017-07-12 11:41:55 +0530

    Write DHCP configuration parameters
    Write the configuration parameters to the
    systemd-networkd conf file.
    
    Resolves openbmc/openbmc#1770
    
    Change-Id: I6c9427034827d612ad7f75961461c2f33688fbf9
    Signed-off-by: Ratan Gupta <ratagupt@in.ibm.com>

--- network_manager.cpp
+++ network_manager.cpp
@@ -170,13 +170,13 @@ void Manager::writeToConfigurationFile()
         // DHCP
         if (intf.second->dHCPEnabled() == true)
         {
-            stream << "DHCP=true\n";
-            // write the dhcp section
-            stream << "[DHCP]\n";
-            stream << "ClientIdentifier=mac\n";
+            // write the dhcp section if interface is
+            // configured as dhcp.
+            writeDHCPSection(stream);
             stream.close();
             continue;
         }
+
         // Static
         for (const auto& addr : addrs)
         {
@@ -223,6 +223,27 @@ void Manager::writeToConfigurationFile()
         stream.close();
     }
     restartSystemdUnit("systemd-networkd.service");
+}
+
+void Manager::writeDHCPSection(std::fstream& stream)
+{
+    using namespace std::string_literals;
+    stream << "DHCP=true\n";
+    // write the dhcp section
+    stream << "[DHCP]\n";
+
+    // Hardcoding the client identifier to mac, to address below issue
+    // https://github.com/openbmc/openbmc/issues/1280
+    stream << "ClientIdentifier=mac\n";
+
+    auto value = dhcpConf->dNS() ? "true"s : "false"s;
+    stream << "UseDNS="s + value + "\n";
+
+    value = dhcpConf->nTP() ? "true"s : "false"s;
+    stream << "UseNTP="s + value + "\n";
+
+    value = dhcpConf->hostName() ? "true"s : "false"s;
+    stream << "UseHostname="s + value + "\n";
 }
 
 bool Manager::getDHCPValue(const std::string& intf)
