3bd2037d1a4e82e0b5a1601b06e683bdea72f72f
Ratan Gupta <ratagupt@in.ibm.com>
2017-07-25 13:38:19 +0530

    Implement the Vlan Interface
    Change-Id: I6085868ba4e30bb9e1c6f6d9895a40ebff82804f
    Signed-off-by: Ratan Gupta <ratagupt@in.ibm.com>

---
+++ vlan_interface.cpp
@@ -0,0 +1,76 @@
+#include "config.h"
+#include "ethernet_interface.hpp"
+#include "vlan_interface.hpp"
+#include "network_manager.hpp"
+
+#include <phosphor-logging/log.hpp>
+#include "xyz/openbmc_project/Common/error.hpp"
+#include <phosphor-logging/elog-errors.hpp>
+
+#include <string>
+#include <algorithm>
+#include <fstream>
+#include <experimental/filesystem>
+
+namespace phosphor
+{
+namespace network
+{
+
+using namespace phosphor::logging;
+using namespace sdbusplus::xyz::openbmc_project::Common::Error;
+
+VlanInterface::VlanInterface(sdbusplus::bus::bus& bus,
+                             const std::string& objPath,
+                             bool dhcpEnabled,
+                             uint32_t vlanID,
+                             EthernetInterface& intf,
+                             Manager& parent ) :
+        VlanIfaces(bus, objPath.c_str(), true),
+        EthernetInterface(bus, objPath, dhcpEnabled,
+                          parent),
+        parentInterface(intf)
+{
+    id(vlanID);
+    //Emit deferred signal.
+    VlanIfaces::emit_object_added();
+}
+
+void VlanInterface::writeDeviceFile()
+{
+    using namespace std::string_literals;
+    fs::path confPath = NETWORK_CONF_DIR;
+
+    if (!fs::create_directories(confPath))
+    {
+        log<level::ERR>("Unable to create the network conf dir",
+                         entry("DIR=%s", confPath.c_str()));
+        elog<InternalFailure>();
+    }
+
+    std::string fileName = EthernetInterface::interfaceName() + ".netdev"s;
+    confPath /= fileName;
+    std::fstream stream;
+    try
+    {
+        stream.open(confPath.c_str(), std::fstream::out);
+    }
+    catch (std::ios_base::failure& e)
+    {
+        log<level::ERR>("Unable to open the VLAN device file",
+                         entry("FILE=%s", confPath.c_str()),
+                         entry("ERROR=%s", e.what()));
+        elog<InternalFailure>();
+
+    }
+
+    stream << "[" << "NetDev" << "]\n";
+    stream << "Name=" << EthernetInterface::interfaceName() << "\n";
+    stream << "Kind=vlan" << "\n";
+    stream << "[VLAN]" << "\n";
+    stream << "Id=" << id() << "\n";
+    stream.close();
+}
+
+}//namespace network
+}//namespace phosphor
---
+++ vlan_interface.hpp
@@ -0,0 +1,77 @@
+#pragma once
+
+#include "types.hpp"
+
+#include "xyz/openbmc_project/Object/Delete/server.hpp"
+#include "xyz/openbmc_project/Network/VLAN/server.hpp"
+
+#include <sdbusplus/bus.hpp>
+#include <sdbusplus/server/object.hpp>
+
+#include <string>
+
+namespace phosphor
+{
+namespace network
+{
+
+class EthernetInterface;
+class Manager;
+
+
+using DeleteIface = sdbusplus::xyz::openbmc_project::Object::server::Delete;
+using VlanIface = sdbusplus::xyz::openbmc_project::Network::server::VLAN;
+using VlanIfaces =
+        sdbusplus::server::object::object<DeleteIface,VlanIface>;
+
+/** @class VlanInterface
+ *  @brief OpenBMC vlan Interface implementation.
+ *  @details A concrete implementation for the vlan interface
+ */
+class VlanInterface : public VlanIfaces, public EthernetInterface
+{
+    public:
+        VlanInterface() = delete;
+        VlanInterface(const VlanInterface&) = delete;
+        VlanInterface& operator=(const VlanInterface&) = delete;
+        VlanInterface(VlanInterface&&) = delete;
+        VlanInterface& operator=(VlanInterface&&) = delete;
+        virtual ~VlanInterface() = default;
+
+        /** @brief Constructor to put object onto bus at a dbus path.
+         *  @param[in] bus - Bus to attach to.
+         *  @param[in] objPath - Path to attach at.
+         *  @param[in] dhcpEnabled - DHCP enable value.
+         *  @param[in] vlanID - vlan identifier.
+         *  @param[in] intf - ethernet interface object.
+         *  @param[in] manager - network manager object.
+         */
+        VlanInterface(sdbusplus::bus::bus& bus,
+                      const std::string& objPath,
+                      bool dhcpEnabled,
+                      uint32_t vlanID,
+                      EthernetInterface& intf,
+                      Manager& manager);
+
+        /** @brief Delete this d-bus object.
+         */
+        void delete_() override {};
+
+        /** @brief writes the device configuration.
+                   systemd reads this configuration file
+                   and creates the vlan interface.*/
+
+        void writeDeviceFile();
+
+    private:
+
+        /** @brief VLAN Identifier. */
+        using VlanIface::id;
+
+        EthernetInterface& parentInterface;
+
+        friend class TestVlanInterface;
+};
+
+} // namespace network
+} // namespace phosphor
