dfd58d6c54334717553272e0ca4c8409e1061485
Ratan Gupta <ratagupt@in.ibm.com>
2017-07-25 13:38:19 +0530

    Implement the Vlan Interface
    Change-Id: I6085868ba4e30bb9e1c6f6d9895a40ebff82804f
    Signed-off-by: Ratan Gupta <ratagupt@in.ibm.com>

---
+++ vlan_interface.cpp
@@ -0,0 +1,67 @@
+#include "config.h"
+#include "ethernet_interface.hpp"
+#include "vlan_interface.hpp"
+#include "network_manager.hpp"
+
+#include <phosphor-logging/log.hpp>
+#include "xyz/openbmc_project/Common/error.hpp"
+
+#include <phosphor-logging/elog-errors.hpp>
+
+#include <string>
+#include <algorithm>
+#include <fstream>
+#include <experimental/filesystem>
+
+namespace phosphor
+{
+namespace network
+{
+
+using namespace phosphor::logging;
+using namespace sdbusplus::xyz::openbmc_project::Common::Error;
+
+VlanInterface::VlanInterface(sdbusplus::bus::bus& bus,
+                             const std::string& objPath,
+                             bool dhcpEnabled,
+                             uint16_t vlanID,
+                             EthernetInterface& intf,
+                             Manager& parent ) :
+        DeleteIface(bus, objPath.c_str(), true),
+        EthernetInterface(bus, objPath, dhcpEnabled,
+                          parent),
+        vlanID(vlanID),
+        parentInterface(intf)
+{
+    // Emit deferred signal.
+    DeleteIface::emit_object_added();
+}
+
+void VlanInterface::writeDeviceFile()
+{
+    using namespace std::string_literals;
+    fs::path confPath = NETWORK_CONF_DIR;
+    std::string fileName = interfaceName() + ".netdev"s;
+    confPath /= fileName;
+    std::fstream stream;
+    stream.open(confPath.c_str(), std::fstream::out);
+    if (!stream.is_open())
+    {
+        log<level::ERR>("Unable to write the VLAN device file",
+                        entry("FILE=%s",confPath.c_str()));
+        elog<InternalFailure>();
+    }
+    stream << "[" << "NetDev" << "]\n";
+    stream << "Name=" << interfaceName() << "\n";
+    stream << "Kind=vlan" << "\n";
+    stream << "[VLAN]" << "\n";
+    stream << "Id=" << vlanID << "\n";
+    stream.close();
+}
+
+void VlanInterface::delete_()
+{
+}
+
+}//namespace network
+}//namespace phosphor
