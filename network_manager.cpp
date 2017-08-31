6df8e3c4865c53028deae63f42521658f1d49f99
Michael Tritz <mtritz@us.ibm.com>
2017-08-31 15:05:18 -0500

    Network factory reset: Log ResetFailed in case of failure
    This commit implements the ResetFailed error defined for
    openbmc/openbmc#1721 for network factory reset. If the reset fails, log
    a ResetFailed error.
    
    Resolves openbmc/openbmc#1721
    
    Change-Id: Ia13df96823c3566da71a10cc945db15c205a2932
    Signed-off-by: Michael Tritz <mtritz@us.ibm.com>

--- network_manager.cpp
+++ network_manager.cpp
@@ -152,13 +152,13 @@ void Manager::reset()
         else
         {
             log<level::ERR>("No network interfaces are mapped.");
-            // TODO: openbmc/openbmc#1721 - Log ResetFailed error here.
+            elog<ResetFailed>();
         }
     }
     else
     {
         log<level::ERR>("Network configuration directory not found!");
-        // TODO: openbmc/openbmc#1721 - Log ResetFailed error here.
+        elog<ResetFailed>();
     }
 
     return;
