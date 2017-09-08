dee50a823efb38fbc9868eebc698a5a25fd3a37e
Ratan Gupta <ratagupt@in.ibm.com>
2017-09-08 17:05:46 +0530

    Recreate the network objects once the timer expires
    As we get multiple network change events once systemd-
    networkd starts,This commit starts the timer once it
    gets the network change event and refresh the network
    objects once the timer expires.
    
    Change-Id: I86d1547763d03f527a8bdb04e2b2780f6cd6fdc2
    Signed-off-by: Ratan Gupta <ratagupt@in.ibm.com>

--- network_manager_main.cpp
+++ network_manager_main.cpp
@@ -1,9 +1,17 @@
+#include "config.h"
 #include "network_manager.hpp"
 #include "rtnetlink_server.hpp"
+#include "timer.hpp"
+#include "types.hpp"
+#include "xyz/openbmc_project/Common/error.hpp"
 
+#include <chrono>
+#include <functional>
 #include <memory>
 #include <iostream>
 
+#include <phosphor-logging/elog-errors.hpp>
+#include <phosphor-logging/log.hpp>
 #include <sdbusplus/bus.hpp>
 #include <sdbusplus/server/manager.hpp>
 
@@ -17,6 +25,7 @@ namespace phosphor
 namespace network
 {
 
+std::unique_ptr<Timer> refreshTimer = nullptr;
 std::unique_ptr<Manager> manager = nullptr;
 
 namespace rtnetlink
@@ -41,8 +50,18 @@ static int eventHandler(sd_event_source* es, int fd, uint32_t revents,
             if (netLinkHeader->nlmsg_type == RTM_NEWADDR ||
                 netLinkHeader->nlmsg_type == RTM_DELADDR)
             {
-                // TODO delete the below trace in later commit.
-                std::cout << "Address Changed\n";
+                // starting the timer here to make sure that we don't want
+                // create the child objects multiple times.
+                if (refreshTimer->isExpired())
+                {
+                    using namespace std::chrono;
+                    auto time = duration_cast<microseconds>(
+                        std::chrono::milliseconds(NETWORK_CHANGE_TIMEOUT_SEC));
+
+                    // if start timer throws exception then let the application
+                    // crash
+                    refreshTimer->startTimer(time);
+                } // end if
 
             } // end if
 
@@ -55,12 +74,25 @@ static int eventHandler(sd_event_source* es, int fd, uint32_t revents,
 
 } // namespace rtnetlink
 
+void refreshObjects()
+{
+    using namespace phosphor::logging;
+    using namespace sdbusplus::xyz::openbmc_project::Common::Error;
+    std::cout <<  "Send the signal to manger to refresh the interfaces\n";
+    phosphor::network::manager->createChildObjects();
+}
 
 } // namespace network
 } // namespace phosphor
 
 int main(int argc, char *argv[])
 {
+    std::function<void()> func(
+            std::bind(&phosphor::network::refreshObjects));
+
+    phosphor::network::refreshTimer =
+        std::make_unique<phosphor::network::Timer>(func);
+
     auto bus = sdbusplus::bus::new_default();
 
     // Add sdbusplus Object Manager for the 'root' path of the network manager.
