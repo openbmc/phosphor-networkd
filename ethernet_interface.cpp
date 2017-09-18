677ae120d6f9ec9944c4473adf85cf0e96943019
Ratan Gupta <ratagupt@in.ibm.com>
2017-09-18 16:28:50 +0530

    Restart the systemd-networkd after setting the MAC address
    In the case of DHCP enabled box,DHCP negotiation should
    be initiated after change of MAC address.
    
    Shouldn't wait for auto-renegotiation,as if different
    machine gets the old mac address then we can have
    duplicate IP problem.
    
    Change-Id: Ifde1a09762c9b9a717ffe7b258d5046f92b9bf1a
    Signed-off-by: Ratan Gupta <ratagupt@in.ibm.com>

--- ethernet_interface.cpp
+++ ethernet_interface.cpp
@@ -568,6 +568,13 @@ std::string EthernetInterface::mACAddress(std::string value)
     {
         intf.second->updateMacAddress();
     }
+
+    // restart the systemd networkd so that dhcp client gets the
+    // ip for the changed mac address.
+    if (dHCPEnabled())
+    {
+        restartSystemdUnit("systemd-networkd.service");
+    }
     return mac;
 
 }
