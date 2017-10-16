3bfb6fce00caa6f7decaa3cc9d4e8b8e8b963fb4
Vishwanatha Subbanna <vishwa@linux.vnet.ibm.com>
2017-10-16 19:08:59 +0530

    Add function to write DNS entries
    Added function to write DNS entries to specified file
    Also, added test case
    
    Change-Id: I64250c7ee3cf7db3e7f8b5cf1669c7b4a7738637
    Signed-off-by: Vishwanatha Subbanna <vishwa@linux.vnet.ibm.com>

---
+++ dns_updater.cpp
@@ -0,0 +1,59 @@
+#include "config.h"
+#include "dns_updater.hpp"
+
+#include <phosphor-logging/log.hpp>
+#include <xyz/openbmc_project/Common/error.hpp>
+#include <phosphor-logging/elog-errors.hpp>
+#include <sdbusplus/bus.hpp>
+
+#include <fstream>
+
+namespace phosphor
+{
+namespace network
+{
+namespace dns
+{
+namespace updater
+{
+
+void processDNSEntries(const fs::path& inFile,
+                       const fs::path& outFile)
+{
+    using namespace phosphor::logging;
+    using namespace sdbusplus::xyz::openbmc_project::Common::Error;
+
+    std::fstream outStream(outFile, std::fstream::out);
+    if (!outStream.is_open())
+    {
+        log<level::ERR>("Unable to open output file",
+                        entry("FILE=%s", outFile.c_str()));
+        elog<InternalFailure>();
+    }
+
+    std::fstream inStream(inFile, std::fstream::in);
+    if (!inStream.is_open())
+    {
+        log<level::ERR>("Unable to open the input file",
+                        entry("FILE=%s", inFile.c_str()));
+        elog<InternalFailure>();
+    }
+
+    outStream << "### Generated through DHCP ###\n";
+
+    for (std::string line; std::getline(inStream, line);)
+    {
+        auto index = line.find("DNS=");
+        if(index != std::string::npos)
+        {
+           auto dns = line.substr(index + 4);
+           outStream << "nameserver " << dns << "\n" ;
+        }
+    }
+    return;
+}
+
+} // namespace updater
+} // namespace dns
+} // namespace network
+} // namespace phosphor
---
+++ test/test_dns_updater.cpp
@@ -0,0 +1,78 @@
+#include "dns_updater.hpp"
+
+#include <gtest/gtest.h>
+
+#include <fstream>
+#include <experimental/filesystem>
+
+static constexpr auto IN_FILE = "/tmp/netif_state";
+static constexpr auto OUT_FILE = "/tmp/resolv.conf";
+static constexpr auto COMPARE_FILE = "/tmp/resolv_compare.conf";
+static constexpr auto DNS_ENTRY_1 = "DNS=1.2.3.4\n";
+static constexpr auto DNS_ENTRY_2 = "DNS=5.6.7.8\n";
+
+namespace fs = std::experimental::filesystem;
+
+class DnsUpdateTest : public ::testing::Test
+{
+    public:
+        // Gets called as part of each TEST_F construction
+        DnsUpdateTest()
+        {
+            // Create a file containing DNS entries like in netif/state
+            std::ofstream file(IN_FILE);
+            file << DNS_ENTRY_1;
+            file << DNS_ENTRY_2;
+
+            // Create a file to compare the results against
+            std::ofstream compare(COMPARE_FILE);
+            compare << "### Generated through DHCP ###\n";
+            compare << "nameserver 1.2.3.4\n";
+            compare << "nameserver 5.6.7.8\n";
+        }
+
+        // Gets called as part of each TEST_F destruction
+        ~DnsUpdateTest()
+        {
+            if (fs::exists(IN_FILE))
+            {
+                fs::remove(IN_FILE);
+            }
+            if (fs::exists(OUT_FILE))
+            {
+                fs::remove(OUT_FILE);
+            }
+            if (fs::exists(COMPARE_FILE))
+            {
+                fs::remove(COMPARE_FILE);
+            }
+        }
+};
+
+/** @brief Makes outfile is updated with right contents
+ */
+TEST_F(DnsUpdateTest, validateOutFile)
+{
+    phosphor::network::dns::updater::processDNSEntries(IN_FILE, OUT_FILE);
+
+    // Read files and compare
+    std::ifstream resolv(OUT_FILE);
+    std::ifstream compare(COMPARE_FILE);
+
+    // From actual file
+    std::string resolvEntry{};
+    std::string resolvContent{};
+    while (std::getline(resolv, resolvEntry))
+    {
+        resolvContent += resolvEntry;
+    }
+
+    // From compare file
+    std::string compareEntry{};
+    std::string compareContent{};
+    while (std::getline(compare, compareEntry))
+    {
+        compareContent += compareEntry;
+    }
+    EXPECT_EQ(resolvContent, compareContent);
+}
