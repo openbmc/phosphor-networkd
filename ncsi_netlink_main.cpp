76e26dbd12482dc06e6ed1d7a05e8ea7eb4275c1
Ratan Gupta <ratagupt@in.ibm.com>
2018-03-23 00:27:52 +0530

    ncsi: impelment the main function
    Change-Id: I682f91452a3ab6362be0de8ac2658ffc9e160c29
    Signed-off-by: Ratan Gupta <ratagupt@in.ibm.com>

--- ncsi_netlink_main.cpp
+++ ncsi_netlink_main.cpp
@@ -14,9 +14,64 @@
  * limitations under the License.
  */
 
+#include "argument.hpp"
+#include "ncsi_util.hpp"
+
+#include <iostream>
+#include <string>
+
+static void exitWithError(const char* err, char** argv)
+{
+    phosphor::network::ArgumentParser::usage(argv);
+    std::cerr << "ERROR: " << err << "\n";
+    exit(EXIT_FAILURE);
+}
+
 int main(int argc, char** argv)
 {
-    //TODO will implement in later commits
+    // Read arguments.
+    auto options = phosphor::network::ArgumentParser(argc, argv);
+
+    // Parse out package argument.
+    auto package = (options)["package"];
+    auto packageInt = atoi(package.c_str());
+    if (packageInt < 0)
+    {
+        exitWithError("Package not specified.", argv);
+    }
+
+    // Parse out channel argument.
+    auto channel = (options)["channel"];
+    auto channelInt = atoi(channel.c_str());
+    if (channelInt < 0 )
+    {
+        exitWithError("Channel not specified.", argv);
+    }
+
+    auto index = (options)["index"];
+    auto indexInt = atoi(index.c_str());
+    if (indexInt < 0 )
+    {
+        exitWithError("Index not specified.", argv);
+    }
+
+  // TODO : Convert package and channel into integer
+    //
+    auto setCmd = (options)["set"];
+    printf("CommnadString = %s\n", setCmd.c_str());
+    if(setCmd == "true")
+    {
+        printf("set is being called\n");
+        phosphor::network::ncsi::setInterface(indexInt, packageInt, channelInt);
+    }
+
+    auto infoCmd = (options)["info"];
+    printf("InfoCommnadString = %s\n", infoCmd.c_str());
+    if(infoCmd == "true")
+    {
+        printf("info is being called\n");
+        phosphor::network::ncsi::getInfo(indexInt, packageInt);
+    }
     return 0;
 }
 
