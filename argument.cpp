9f57d3fa57248a3f3748bbecddfaf59dea99258e
Patrick Venture <venture@google.com>
2018-07-09 12:30:59 -0700

    reorder headers
    Change-Id: Ia74a5c761a3029819366159de3cfd40c5e4c4a2c
    Signed-off-by: Patrick Venture <venture@google.com>

--- argument.cpp
+++ argument.cpp
@@ -13,9 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+#include <algorithm>
 #include <iostream>
 #include <iterator>
-#include <algorithm>
 #include "argument.hpp"
 
 namespace phosphor
