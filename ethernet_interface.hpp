809c1c173ebfbe8fcef1bf862926145f01617c31
Ravi Teja <raviteja28031990@gmail.com>
2019-04-25 10:10:18 -0500

    Network:add link local auto configuration support on ethernet interface.
    Here is commit to add linkLocalAutoConf dbus property
    https://gerrit.openbmc-project.xyz/c/openbmc/phosphor-dbus-interfaces/+/20904
    
    Tested by:
    configured linkLocalAutoConf to ipv6,ipv4,none and both
    and verified link local address objects created
    
    Signed-off-by: Ravi Teja <raviteja28031990@gmail.com>
    Change-Id: I70c4b3b2502e37478c9cd747bc9eadfca224ff4e

--- ethernet_interface.hpp
+++ ethernet_interface.hpp
@@ -31,6 +31,8 @@ using EthernetInterfaceIntf =
     sdbusplus::xyz::openbmc_project::Network::server::EthernetInterface;
 using MacAddressIntf =
     sdbusplus::xyz::openbmc_project::Network::server::MACAddress;
+using LinkLocalAutoConf = sdbusplus::xyz::openbmc_project::Network::server::
+    EthernetInterface::LinkLocalConf;
 
 using ServerList = std::vector<std::string>;
 using ObjectPath = sdbusplus::message::object_path;
@@ -150,6 +152,9 @@ class EthernetInterface : public Ifaces
     /** Set value of DHCPEnabled */
     bool dHCPEnabled(bool value) override;
 
+    /** Set value of LinkLocalAutoConf */
+    LinkLocalAutoConf linkLocalAutoConf(LinkLocalAutoConf value) override;
+
     /** @brief sets the MAC address.
      *  @param[in] value - MAC address which needs to be set on the system.
      *  @returns macAddress of the interface or throws an error.
@@ -187,8 +192,8 @@ class EthernetInterface : public Ifaces
 
     using EthernetInterfaceIntf::dHCPEnabled;
     using EthernetInterfaceIntf::interfaceName;
+    using EthernetInterfaceIntf::linkLocalAutoConf;
     using MacAddressIntf::mACAddress;
-
     /** @brief Absolute path of the resolv conf file */
     static constexpr auto resolvConfFile = "/etc/resolv.conf";
 
@@ -267,6 +272,11 @@ class EthernetInterface : public Ifaces
      *
      */
     ServerList getNameServerFromConf();
+
+    /** @brief get the link local auto conf from the network conf
+     *
+     */
+    LinkLocalAutoConf getLinkLocalAutoConfFromConf();
 
     /** @brief Persistent sdbusplus DBus bus connection. */
     sdbusplus::bus::bus& bus;
