3a63525969840c6caf32c31c3d4045be871190e7
Jennifer Lee <jennifer1.lee@intel.com>
2019-05-08 15:40:32 -0700

    Initialize Speed and AutoNeg when creating EthernetInterface
    These 2 properties were not initialized to reflect actual status of NIC.
    This patch makes up the gap.
    
    Tested:
    After updating BMC firmware, check for DBus
    xyz.openbmc_project.Network /xyz/openbmc_project/network/{eth ID}
    xyz.openbmc_project.Network.EthernetInterface
    AutoNeg and Speed property reflects the correct status of NIC.
    
    Signed-off-by: Jennifer Lee <jennifer1.lee@intel.com>
    Change-Id: Id59a9c525a68ce459de7a909a1c1063b01b23a8a

--- ethernet_interface.cpp
+++ ethernet_interface.cpp
@@ -50,6 +50,10 @@ EthernetInterface::EthernetInterface(sdbusplus::bus::bus& bus,
     EthernetInterfaceIntf::nTPServers(getNTPServersFromConf());
     EthernetInterfaceIntf::nameservers(getNameServerFromConf());
 
+    InterfaceInfo ethInfo(getInterfaceInfo());
+    EthernetInterfaceIntf::speed(std::get<0>(ethInfo));
+    EthernetInterfaceIntf::autoNeg(std::get<2>(ethInfo));
+
     // Emit deferred signal.
     if (emitSignal)
     {
@@ -154,13 +158,6 @@ ObjectPath EthernetInterface::iP(IP::Protocol protType, std::string ipaddress,
     manager.writeToConfigurationFile();
     return objectPath;
 }
-
-/*
-Note: We don't have support for  ethtool now
-will enable this code once we bring the ethtool
-in the image.
-TODO: https://github.com/openbmc/openbmc/issues/1484
-*/
 
 InterfaceInfo EthernetInterface::getInterfaceInfo() const
 {
