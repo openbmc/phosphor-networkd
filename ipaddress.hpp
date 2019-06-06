b0f50a7592b6e6c2c8c2088f9c8e8252e818f996
Ravi Teja <raviteja28031990@gmail.com>
2019-06-06 03:27:55 -0500

    Network: Remove IP dbus properties update support
    Currently, we support IP object properties update,
    it just update dbus object, actually it wont change
    properties on ethernet interface.
    
    Signed-off-by: Ravi Teja <raviteja28031990@gmail.com>
    Change-Id: I60fdc2e07cdb728d661dd0baf1bbba8f7fff3702

--- ipaddress.hpp
+++ ipaddress.hpp
@@ -50,9 +50,21 @@ class IPAddress : public IPIfaces
               const std::string& ipAddress, IP::AddressOrigin origin,
               uint8_t prefixLength, const std::string& gateway);
 
+    std::string address(std::string ipAddress) override;
+    uint8_t prefixLength(uint8_t) override;
+    std::string gateway(std::string gateway) override;
+    IP::Protocol type(IP::Protocol type) override;
+    IP::AddressOrigin origin(IP::AddressOrigin origin) override;
+
     /** @brief Delete this d-bus object.
      */
     void delete_() override;
+
+    using IP::address;
+    using IP::gateway;
+    using IP::origin;
+    using IP::prefixLength;
+    using IP::type;
 
   private:
     /** @brief Parent Object. */
