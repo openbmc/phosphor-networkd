56065971fd62587e7ef347007db964c23baaa4ec
Ravi Teja <raviteja28031990@gmail.com>
2019-06-06 03:27:55 -0500

    Network: Remove IP dbus properties update support
    Currently, we support IP object properties update,
    it just update dbus object, actually it wont change
    properties on ethernet interface.
    
    Signed-off-by: Ravi Teja <raviteja28031990@gmail.com>
    Change-Id: I60fdc2e07cdb728d661dd0baf1bbba8f7fff3702

--- ipaddress.cpp
+++ ipaddress.cpp
@@ -5,10 +5,10 @@
 #include "ethernet_interface.hpp"
 #include "util.hpp"
 
+#include <iostream>
 #include <phosphor-logging/elog-errors.hpp>
 #include <phosphor-logging/log.hpp>
 #include <xyz/openbmc_project/Common/error.hpp>
-
 namespace phosphor
 {
 namespace network
@@ -16,6 +16,8 @@ namespace network
 
 using namespace phosphor::logging;
 using namespace sdbusplus::xyz::openbmc_project::Common::Error;
+using NotAllowed = sdbusplus::xyz::openbmc_project::Common::Error::NotAllowed;
+using Reason = xyz::openbmc_project::Common::NotAllowed::REASON;
 
 IPAddress::IPAddress(sdbusplus::bus::bus& bus, const char* objPath,
                      EthernetInterface& parent, IP::Protocol type,
@@ -24,16 +26,41 @@ IPAddress::IPAddress(sdbusplus::bus::bus& bus, const char* objPath,
     IPIfaces(bus, objPath, true),
     parent(parent)
 {
-    this->address(ipaddress);
-    this->prefixLength(prefixLength);
-    this->gateway(gateway);
-    this->type(type);
-    this->origin(origin);
+
+    IP::address(ipaddress);
+    IP::prefixLength(prefixLength);
+    IP::gateway(gateway);
+    IP::type(type);
+    IP::origin(origin);
 
     // Emit deferred signal.
     emit_object_added();
 }
-
+std::string IPAddress::address(std::string ipAddress)
+{
+    elog<NotAllowed>(Reason("Property update is not allowed"));
+    return ipAddress;
+}
+uint8_t IPAddress::prefixLength(uint8_t value)
+{
+    elog<NotAllowed>(Reason("Property update is not allowed"));
+    return value;
+}
+std::string IPAddress::gateway(std::string gateway)
+{
+    elog<NotAllowed>(Reason("Property update is not allowed"));
+    return gateway;
+}
+IP::Protocol IPAddress::type(IP::Protocol type)
+{
+    elog<NotAllowed>(Reason("Property update is not allowed"));
+    return type;
+}
+IP::AddressOrigin IPAddress::origin(IP::AddressOrigin origin)
+{
+    elog<NotAllowed>(Reason("Property update is not allowed"));
+    return origin;
+}
 void IPAddress::delete_()
 {
     if (parent.dHCPEnabled())
