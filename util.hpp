382bde2ca5c0ba8f7824a065162cfc47b414a46e
Alvin Wang <wangat@tw.ibm.com>
2019-08-29 22:56:46 +0800

    Add two MAC addresses support for Mihawk's 2 NICs
    In somes systms, there are 2 MAC addresses for 2 NICs like mihawk. In
    order to support system like Mihawk, check the object path of
    NetworkInterface to find the corresponding object of NIC.
    
    Tested: Get the 2 MAC addresses of eth0 and eth1 from inventory
    
    Signed-off-by: Alvin Wang <alvinwang@msn.com>
    Change-Id: I2ee08c020010bccd7e46b94d69f5fd80a639aef7

--- util.hpp
+++ util.hpp
@@ -2,6 +2,7 @@
 
 #include "config.h"
 
+#include "ethernet_interface.hpp"
 #include "types.hpp"
 
 #include <netinet/ether.h>
@@ -29,8 +30,10 @@ namespace mac_address
 
 /** @brief gets the MAC address from the Inventory.
  *  @param[in] bus - DBUS Bus Object.
+ *  @param[in] intfName - Interface name
  */
-ether_addr getfromInventory(sdbusplus::bus::bus& bus);
+ether_addr getfromInventory(sdbusplus::bus::bus& bus,
+                            const std::string& intfName);
 
 /** @brief Converts the given mac address into byte form
  *  @param[in] str - The mac address in human readable form
