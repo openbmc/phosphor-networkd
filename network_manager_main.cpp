90cf3ddf6294a637acbb6a431c40014d1cb3197f
Manojkiran Eda <manojkiran.eda@gmail.com>
2020-05-11 14:25:16 +0530

    Enable Fist Boot Set MAC Feature for Network Service
    - This commit enables the Network Service to listen for
      the Added inventory objects & set the MAC address respectively
      on the ethernet interfaces on the first boot.
    
    - An Early start to Inventory Manager service can also drop the
      Objects Added Signal, in those cases the Network Serice would
      read the Inventory Objects and sets the MAC during First Boot
      and does not register for Signal matchers to Wait for the Objects
      to be Added.
    
    - Through this commit, Network Service can now also set the MAC
      addresses at Runtime(if the Inventory Objects are Added at Runtime).
      This would help in case of MultiNode systems with Concurrent
      maintainence.
    
    - The Mapping of the ethernet Interfaces on Network Service and
      the Inventory objects are configured by the administator via a
      machine specific config.json file.
    
    Tested By :
    
    - Clear the VPD and Boot the System and observed that Network
      service registers a Signal which listens on the Object Added
      on the Inventory Manager.
    
    - Force Collect the VPD , and that should trigger a MATCH on the
      Network service there by setting the MAC Addresses based in the
      Signals received on various interfaces.
    
    - Once the MAC is read from the VPD and set on the repective Interface,
      a restart of Network Service should not set the MAC Address again.
    
    Signed-off-by: Manojkiran Eda <manojkiran.eda@gmail.com>
    Change-Id: If06e79e6381933e58ad552bbd7dbb61fe852384b

--- network_manager_main.cpp
+++ network_manager_main.cpp
@@ -7,11 +7,15 @@
 
 #include <linux/netlink.h>
 
+#include <filesystem>
+#include <fstream>
 #include <functional>
 #include <memory>
+#include <nlohmann/json.hpp>
 #include <phosphor-logging/elog-errors.hpp>
 #include <phosphor-logging/log.hpp>
 #include <sdbusplus/bus.hpp>
+#include <sdbusplus/bus/match.hpp>
 #include <sdbusplus/server/manager.hpp>
 #include <sdeventplus/event.hpp>
 #include <xyz/openbmc_project/Common/error.hpp>
@@ -26,6 +30,12 @@ constexpr char NETWORK_CONF_DIR[] = "/etc/systemd/network";
 
 constexpr char DEFAULT_OBJPATH[] = "/xyz/openbmc_project/network";
 
+constexpr auto firstBootFile = "/var/lib/network/firstBoot";
+constexpr auto configFile = "/usr/share/network/config.json";
+
+constexpr auto invNetworkIntf =
+    "xyz.openbmc_project.Inventory.Item.NetworkInterface";
+
 namespace phosphor
 {
 namespace network
@@ -34,6 +44,98 @@ namespace network
 std::unique_ptr<phosphor::network::Manager> manager = nullptr;
 std::unique_ptr<Timer> refreshObjectTimer = nullptr;
 std::unique_ptr<Timer> restartTimer = nullptr;
+
+bool setInventoryMACOnSystem(sdbusplus::bus::bus& bus,
+                             const nlohmann::json& configJson)
+{
+    // At the time of calling the API, the ethenet Interface objects
+    // are not populated,so force it to create the objects
+    manager->createChildObjects();
+    for (const auto& interfaces : configJson.items())
+    {
+        try
+        {
+            auto inventoryMAC =
+                mac_address::getfromInventory(bus, interfaces.key());
+            if (!mac_address::toString(inventoryMAC).empty())
+            {
+                log<level::INFO>("Mac Address in Inventory on "),
+                    entry("Interface : ", interfaces.key().c_str()),
+                    entry("MAC Address :",
+                          (mac_address::toString(inventoryMAC)).c_str());
+                manager->setFistBootMACOnInterface(std::make_pair(
+                    interfaces.key(), mac_address::toString(inventoryMAC)));
+            }
+            else
+            {
+                log<level::INFO>("Nothing is present in Inventory");
+                return false;
+            }
+        }
+        catch (const std::exception& e)
+        {
+            log<level::ERR>("Exception occurred during getting of MAC "
+                            "address from Inventory");
+            return false;
+        }
+    }
+    return true;
+}
+
+// register the macthes to be monitored from inventory manager
+void registerSignals(sdbusplus::bus::bus& bus, const nlohmann::json& configJson)
+{
+    using DbusObjectPath = std::string;
+    using DbusInterface = std::string;
+    using PropertyValue = std::string;
+
+    log<level::INFO>("Registering the Inventory Signals Matcher");
+
+    static std::unique_ptr<sdbusplus::bus::match::match> MacAddressMatch;
+
+    auto callback = [&](sdbusplus::message::message& m) {
+        std::map<DbusObjectPath,
+                 std::map<DbusInterface, std::variant<PropertyValue>>>
+            interfacesProperties;
+
+        sdbusplus::message::object_path objPath;
+        std::pair<std::string, std::string> ethPair;
+        m.read(objPath, interfacesProperties);
+
+        for (const auto& pattern : configJson.items())
+        {
+            if (objPath.str.find(pattern.value()) != std::string::npos)
+            {
+                for (auto& interface : interfacesProperties)
+                {
+                    if (interface.first == invNetworkIntf)
+                    {
+                        for (const auto& path : interface.second)
+                        {
+                            if (path.first.find("MAC") != std::string::npos)
+                            {
+                                ethPair = std::make_pair(
+                                    pattern.key(),
+                                    std::get<std::string>(path.second));
+                            }
+                        }
+                    }
+                }
+                if (!(ethPair.first.empty() || ethPair.second.empty()))
+                {
+                    manager->setFistBootMACOnInterface(ethPair);
+                }
+            }
+        }
+    };
+
+    MacAddressMatch = std::make_unique<sdbusplus::bus::match::match>(
+        bus,
+        "interface='org.freedesktop.DBus.ObjectManager',type='signal',"
+        "member='InterfacesAdded',path='/xyz/openbmc_project/"
+        "inventory'",
+        callback);
+}
 
 /** @brief refresh the network objects. */
 void refreshObjects()
@@ -134,5 +236,22 @@ int main(int argc, char* argv[])
     // RTNETLINK event handler
     phosphor::network::rtnetlink::Server svr(eventPtr, smartSock);
 
+#if ENABLE_IBM_CONFIG
+    std::ifstream in(configFile);
+    nlohmann::json configJson;
+    in >> configJson;
+
+    // Incase if phosphor-inventory-manager started early and the VPD is already
+    // collected by the time network service has come up, better to check the
+    // VPD directly and set the MAC Address on the respective Interface.
+    if (!std::filesystem::exists(firstBootFile))
+    {
+
+        if (!phosphor::network::setInventoryMACOnSystem(bus, configJson))
+        {
+            phosphor::network::registerSignals(bus, configJson);
+        }
+    }
+#endif
     sd_event_loop(eventPtr.get());
 }
