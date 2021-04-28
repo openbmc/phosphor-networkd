2f8de23654b9fdb89dcd1b6cabd2a1256fa6cdd4
Johnathan Mantey <johnathanx.mantey@intel.com>
2021-04-28 13:59:46 -0700

    Permit control of systemd-networkd IPv6AcceptRA configuration
    The IPv6AcceptRA boolean is always set to "false". This effectively
    blocks IPv6 DHCP address assignment. There may be OpenBMC users that
    will prefer this boolean to be "true" the first time the BMC boots.
    
    This commit adds a compile time switch, that is added to the
    phosphor-network.bbappend file. Adding
    EXTRA_OECONF_append = " --enable-ipv6-accept-ra=yes"
    to the bbappend file changes the IPv6AcceptRA boolean in the network
    configuration file to "true". This does not overwrite the IPv6AcceptRA
    value present in a network configuration file that already exists. It
    only assigns a value if the network configuration file is not present.
    
    Tested:
    * Erased the entire SPI device, and programmed a new image with the
      --enable-ipv6-accept-ra=yes. Confirmed that the IPv6AcceptRA value in
      the NIC configuration file was "true".
    
    * Performed "rm /etc/system/network/00-bmc-eth0.network", and
      rebooted. Confirmed that the IPv6AcceptRA value in the NIC
      configuration file was "true".
    
    * Removed the --enable-ipv6-accept-ra from the bbappend file, rebuilt,
      erased the entire SPI. Confirmed that the IPv6AcceptRA value in the
      NIC configuration file was "false".
    
    * Performed "rm /etc/system/network/00-bmc-eth0.network", and
      rebooted. Confirmed that the IPv6AcceptRA value in the NIC
      configuration file was "false".
    
    * Rebooted in both test cases and confirmed the setting did not change
      in the ".network" file already present.
    
    Change-Id: Ieb6309ddcf2d003ed398e1c0c09bdee0670f6fcf
    Signed-off-by: Johnathan Mantey <johnathanx.mantey@intel.com>

--- network_config.cpp
+++ network_config.cpp
@@ -17,6 +17,14 @@ void writeDHCPDefault(const std::string& filename, const std::string& interface)
     std::ofstream filestream;
 
     filestream.open(filename);
+    // Add the following line to your phosphor-network bbappend file
+    // to control IPV6_ACCEPT_RA
+    //   EXTRA_OECONF_append = " --enable-ipv6-accept-ra=yes"
+    // If this switch is not present or set to 'no'
+    // ENABLE_IPV6_ACCEPT_RA will be undefined.
+    // The new value is only assigned on first boot, when the default
+    // file is not present, or after the default file has been
+    // manually removed.
     filestream << "[Match]\nName=" << interface <<
                 "\n[Network]\nDHCP=true\n"
 #ifdef LINK_LOCAL_AUTOCONFIGURATION
@@ -24,7 +32,12 @@ void writeDHCPDefault(const std::string& filename, const std::string& interface)
 #else
                 "LinkLocalAddressing=no\n"
 #endif
+#ifdef ENABLE_IPV6_ACCEPT_RA
+                "IPv6AcceptRA=true\n"
+#else
                 "IPv6AcceptRA=false\n"
+
+#endif
                 "[DHCP]\nClientIdentifier=mac\n";
     filestream.close();
 }
