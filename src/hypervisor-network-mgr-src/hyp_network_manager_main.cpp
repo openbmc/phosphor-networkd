760babbab6826adac42cca10cb13389f14b9c911
Asmitha Karunanithi <asmitk01@in.ibm.com>
2021-05-13 02:54:29 -0500

    An application for hypervisor network configuration
    This application takes care of hypervisor network configuration
    by communicating with the bios-settings-mgr in the backend.
    
    It hosts xyz.openbmc_project.Network.Hypervisor.service similar
    to that of network service and implements ethernet and ip
    interfaces whose properties are mapped to the appropriate
    properties in the base bios table.
    
    The flow will be like:
    - User fires the redfish command to update hypervisor
    network configuration properties
    - BMCWeb updates the properties in the dbus object
    hosted by this application
    - This application would update these new values in
    the base bios table
    
    The dbus object will be updated whenever there's a
    change in the bios table properties
    
    This is divided into multiple commits and this commit
    hosts xyz.openbmc_project.Network.Hypervisor.service and
    creates the /xyz/openbmc_project/network/hypervisor object.
    
    Tested By:
    
    busctl tree xyz.openbmc_project.Network.Hypervisor
    └─/xyz
      └─/xyz/openbmc_project
        └─/xyz/openbmc_project/network
          └─/xyz/openbmc_project/network/hypervisor
    
    Signed-off-by: Asmitha Karunanithi <asmitk01@in.ibm.com>
    Change-Id: I2d4582bc9c1dbf72c9963f3e43169b695b34b1ed

---
+++ src/hypervisor-network-mgr-src/hyp_network_manager_main.cpp
@@ -0,0 +1,29 @@
+#include "hyp_network_manager.hpp"
+
+#include <sdeventplus/event.hpp>
+
+constexpr char DEFAULT_HYP_OBJPATH[] =
+    "/xyz/openbmc_project/network/hypervisor";
+constexpr char HYP_DEFAULT_BUSNAME[] = "xyz.openbmc_project.Network.Hypervisor";
+
+int main(int /*argc*/, char** /*argv*/)
+{
+    auto bus = sdbusplus::bus::new_default();
+
+    // Add sdbusplus ObjectManager
+    sdbusplus::server::manager::manager objManager(bus, DEFAULT_HYP_OBJPATH);
+
+    // Get default event loop
+    auto event = sdeventplus::Event::get_default();
+
+    // Attach the bus to sd_event to service user requests
+    bus.attach_event(event.get(), SD_EVENT_PRIORITY_NORMAL);
+
+    // Create hypervisor network manager dbus object
+    phosphor::network::HypNetworkMgr manager(bus, event, DEFAULT_HYP_OBJPATH);
+
+    bus.request_name(HYP_DEFAULT_BUSNAME);
+
+    event.loop();
+    return 0;
+}
