d59bc8be00851a6337e45abd00d0ffe4f38678ee
Asmitha Karunanithi <asmitk01@in.ibm.com>
2021-05-13 02:54:29 -0500

    An application for hypervisor network configuration
    This application takes care of hypervisor network configuration
    by communicating with the bios-settings-mgr in the backend.
    
    It hosts xyz.openbmc_project.Network.Hypervisor.service similar
    to that of network service and implements ethernet and ip
    interfaces whose properties are mapped to the appropriate
    properties in the base bios table.
    
    The flow will be like:
    - User fires the redfish command to update hypervisor
    network configuration properties
    - BMCWeb updates the properties in the dbus object
    hosted by this application
    - This application would update these new values in
    the base bios table
    
    The dbus object will be updated whenever there's a
    change in the bios table properties
    
    Tested By:
    
    busctl tree xyz.openbmc_project.Network.Hypervisor
    └─/xyz
      └─/xyz/openbmc_project
        └─/xyz/openbmc_project/network
          └─/xyz/openbmc_project/network/hypervisor
            ├─/xyz/openbmc_project/network/hypervisor/if0
            │ └─/xyz/openbmc_project/network/hypervisor/if0/ipv4
            │   └─/xyz/openbmc_project/network/hypervisor/if0/ipv4/42e2186b
            └─/xyz/openbmc_project/network/hypervisor/if1
              └─/xyz/openbmc_project/network/hypervisor/if1/ipv4
                └─/xyz/openbmc_project/network/hypervisor/if1/ipv4/52166655
    
    busctl introspect xyz.openbmc_project.Network.Hypervisor /xyz/openbmc_project/network/hypervisor/if0/ipv4/42e2186b
    NAME                                TYPE      SIGNATURE RESULT/VALUE                             FLAGS
    org.freedesktop.DBus.Introspectable interface -         -                                        -
    .Introspect                         method    -         s                                        -
    org.freedesktop.DBus.Peer           interface -         -                                        -
    .GetMachineId                       method    -         s                                        -
    .Ping                               method    -         -                                        -
    org.freedesktop.DBus.Properties     interface -         -                                        -
    .Get                                method    ss        v                                        -
    .GetAll                             method    s         a{sv}                                    -
    .Set                                method    ssv       -                                        -
    .PropertiesChanged                  signal    sa{sv}as  -                                        -
    xyz.openbmc_project.Network.IP      interface -         -                                        -
    .Address                            property  s         "9.8.2.1"                                emits-change writable
    .Gateway                            property  s         "9.8.1.1"                                emits-change writable
    .Origin                             property  s         "xyz.openbmc_project.Network.IP.Address… emits-change writable
    .PrefixLength                       property  y         26                                       emits-change writable
    .Type                               property  s         "xyz.openbmc_project.Network.IP.Protoco… emits-change writable
    xyz.openbmc_project.Object.Delete   interface -         -                                        -
    .Delete                             method    -         -                                        -
    
    Signed-off-by: Asmitha Karunanithi <asmitk01@in.ibm.com>
    Change-Id: I2d4582bc9c1dbf72c9963f3e43169b695b34b1ed

---
+++ hyp-network-manager/hyp_network_manager_main.cpp
@@ -0,0 +1,40 @@
+#include "config.h"
+
+#include "hyp_network_manager.hpp"
+
+#include <phosphor-logging/elog-errors.hpp>
+#include <phosphor-logging/elog.hpp>
+#include <phosphor-logging/log.hpp>
+
+#include <filesystem>
+#include <fstream>
+#include <sdeventplus/event.hpp>
+#include <string>
+
+using namespace phosphor::logging;
+using sdbusplus::exception::SdBusError;
+
+constexpr char DEFAULT_HYP_OBJPATH[] = "/xyz/openbmc_project/network/hypervisor";
+constexpr char DEFAULT_HYP_BUSNAME[] = "xyz.openbmc_project.Network.Hypervisor";
+
+int main(int argc, char** argv)
+{
+    auto bus = sdbusplus::bus::new_default();
+
+    // Add sdbusplus ObjectManager
+    sdbusplus::server::manager::manager objManager(bus, DEFAULT_HYP_OBJPATH);
+
+    // Get default event loop
+    auto event = sdeventplus::Event::get_default();
+
+    // Attach the bus to sd_event to service user requests
+    bus.attach_event(event.get(), SD_EVENT_PRIORITY_NORMAL);
+
+    // Create hypervisor network manager dbus object
+    phosphor::network::HypNetworkMgr manager(bus, event, DEFAULT_HYP_OBJPATH);
+
+    bus.request_name(DEFAULT_HYP_BUSNAME);
+
+    event.loop();
+    return 0;
+}
