dc2f1b6e5414439781334667b2c459419c91b997
William A. Kennington III <wak@google.com>
2021-05-13 21:20:19 -0700

    types: Remove unused assignment
    clang-tidy warns about this harmless but unused assignment.
    
    Change-Id: I5255dd6b49775c4515ceab753295d915a390f5c9
    Signed-off-by: William A. Kennington III <wak@google.com>

--- types.hpp
+++ types.hpp
@@ -72,7 +72,7 @@ struct EventDeleter
 {
     void operator()(sd_event* event) const
     {
-        event = sd_event_unref(event);
+        sd_event_unref(event);
     }
 };
 using EventPtr = std::unique_ptr<sd_event, EventDeleter>;
