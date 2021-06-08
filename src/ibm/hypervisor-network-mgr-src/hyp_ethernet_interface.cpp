6813260ce4d49a93e02454498b038ec2203fe464
Asmitha Karunanithi <asmitk01@in.ibm.com>
2021-06-08 11:56:00 -0500

    Add ethernet objects under hypervisor network obj
    In this commit, ethernet interface dbus objects are created
    under the hypervisor network service.
    
    busctl tree xyz.openbmc_project.Network.Hypervisor
    └─/xyz
      └─/xyz/openbmc_project
        └─/xyz/openbmc_project/network
          └─/xyz/openbmc_project/network/hypervisor
            ├─/xyz/openbmc_project/network/hypervisor/config
            ├─/xyz/openbmc_project/network/hypervisor/eth0
            └─/xyz/openbmc_project/network/hypervisor/eth1
    
    Signed-off-by: Asmitha Karunanithi <asmitk01@in.ibm.com>
    Change-Id: I4222fa29a9df607c8291d7f81f7f3e837442bc26

---
+++ src/ibm/hypervisor-network-mgr-src/hyp_ethernet_interface.cpp
@@ -0,0 +1,78 @@
+#include "hyp_ethernet_interface.hpp"
+
+class HypEthInterface;
+class HypIPAddress;
+
+namespace phosphor
+{
+namespace network
+{
+
+using namespace phosphor::logging;
+using namespace sdbusplus::xyz::openbmc_project::Common::Error;
+using Argument = xyz::openbmc_project::Common::InvalidArgument;
+
+constexpr char IP_INTERFACE[] = "xyz.openbmc_project.Network.IP";
+
+constexpr char biosStrType[] =
+    "xyz.openbmc_project.BIOSConfig.Manager.AttributeType.String";
+constexpr char biosIntType[] =
+    "xyz.openbmc_project.BIOSConfig.Manager.AttributeType.Integer";
+constexpr char biosEnumType[] =
+    "xyz.openbmc_project.BIOSConfig.Manager.AttributeType.Enumeration";
+
+biosTableType HypEthInterface::getBiosAttrsMap()
+{
+    return manager.getBIOSTableAttrs();
+}
+
+void HypEthInterface::disableDHCP(HypIP::Protocol protocol)
+{
+    DHCPConf dhcpState = HypEthernetIntf::dhcpEnabled();
+    if (dhcpState == HypEthInterface::DHCPConf::both)
+    {
+        if (protocol == HypIP::Protocol::IPv4)
+        {
+            dhcpEnabled(HypEthInterface::DHCPConf::v6);
+        }
+        else if (protocol == HypIP::Protocol::IPv6)
+        {
+            dhcpEnabled(HypEthInterface::DHCPConf::v4);
+        }
+    }
+    else if ((dhcpState == HypEthInterface::DHCPConf::v4) &&
+             (protocol == HypIP::Protocol::IPv4))
+    {
+        dhcpEnabled(HypEthInterface::DHCPConf::none);
+    }
+    else if ((dhcpState == HypEthInterface::DHCPConf::v6) &&
+             (protocol == HypIP::Protocol::IPv6))
+    {
+        dhcpEnabled(HypEthInterface::DHCPConf::none);
+    }
+}
+
+bool HypEthInterface::isDHCPEnabled(HypIP::Protocol family, bool ignoreProtocol)
+{
+    return (
+        (HypEthernetIntf::dhcpEnabled() == HypEthInterface::DHCPConf::both) ||
+        ((HypEthernetIntf::dhcpEnabled() == HypEthInterface::DHCPConf::v6) &&
+         ((family == HypIP::Protocol::IPv6) || ignoreProtocol)) ||
+        ((HypEthernetIntf::dhcpEnabled() == HypEthInterface::DHCPConf::v4) &&
+         ((family == HypIP::Protocol::IPv4) || ignoreProtocol)));
+}
+
+HypEthernetIntf::DHCPConf
+    HypEthInterface::dhcpEnabled(HypEthernetIntf::DHCPConf value)
+{
+    if (value == HypEthernetIntf::dhcpEnabled())
+    {
+        return value;
+    }
+
+    HypEthernetIntf::dhcpEnabled(value);
+    return value;
+}
+
+} // namespace network
+} // namespace phosphor
---
+++ src/ibm/hypervisor-network-mgr-src/hyp_ethernet_interface.hpp
@@ -0,0 +1,118 @@
+#pragma once
+
+#include "ethernet_interface.hpp"
+#include "hyp_network_manager.hpp"
+#include "xyz/openbmc_project/Network/IP/Create/server.hpp"
+
+#include <phosphor-logging/elog-errors.hpp>
+#include <phosphor-logging/elog.hpp>
+#include <phosphor-logging/log.hpp>
+#include <sdbusplus/bus.hpp>
+#include <xyz/openbmc_project/Common/error.hpp>
+
+namespace phosphor
+{
+namespace network
+{
+
+class HypNetworkMgr; // forward declaration of hypervisor network manager.
+
+using namespace phosphor::logging;
+using HypIP = sdbusplus::xyz::openbmc_project::Network::server::IP;
+
+using CreateIface = sdbusplus::server::object::object<
+    sdbusplus::xyz::openbmc_project::Network::server::EthernetInterface,
+    sdbusplus::xyz::openbmc_project::Network::IP::server::Create>;
+
+using biosTableRetAttrValueType = std::variant<std::string, int64_t>;
+
+using biosTableType = std::map<std::string, std::variant<int64_t, std::string>>;
+
+using PendingAttributesType =
+    std::map<std::string,
+             std::tuple<std::string, std::variant<int64_t, std::string>>>;
+
+using HypEthernetIntf =
+    sdbusplus::xyz::openbmc_project::Network::server::EthernetInterface;
+
+using ObjectPath = sdbusplus::message::object_path;
+
+static std::shared_ptr<sdbusplus::bus::match::match> matchBIOSAttrUpdate;
+
+/** @class HypEthernetInterface
+ *  @brief Hypervisor Ethernet Interface implementation.
+ */
+class HypEthInterface : public CreateIface
+{
+  public:
+    HypEthInterface() = delete;
+    HypEthInterface(const HypEthInterface&) = delete;
+    HypEthInterface& operator=(const HypEthInterface&) = delete;
+    HypEthInterface(HypEthInterface&&) = delete;
+    HypEthInterface& operator=(HypEthInterface&&) = delete;
+    virtual ~HypEthInterface() = default;
+
+    /** @brief Constructor to put object onto bus at a dbus path.
+     *  @param[in] bus - Bus to attach to.
+     *  @param[in] path - Path to attach at.
+     *  @param[in] parent - parent object.
+     */
+    HypEthInterface(sdbusplus::bus::bus& bus, const char* path,
+                    const std::string& intfName, HypNetworkMgr& parent) :
+        CreateIface(bus, path, CreateIface::action::defer_emit),
+        bus(bus), objectPath(path), manager(parent)
+    {
+        HypEthernetIntf::interfaceName(intfName);
+    };
+
+    /** @brief Function to create ipAddress dbus object.
+     *  @param[in] addressType - Type of ip address.
+     *  @param[in] ipAddress- IP address.
+     *  @param[in] prefixLength - Length of prefix.
+     *  @param[in] gateway - Gateway ip address.
+     */
+
+    ObjectPath ip(HypIP::Protocol /*addressType*/, std::string /*ipAddress*/,
+                  uint8_t /*prefixLength*/, std::string /*gateway*/) override
+    {
+        return std::string();
+    };
+
+    /* @brief Function that returns parent's bios attrs map
+     */
+    biosTableType getBiosAttrsMap();
+
+    /* @brief Returns the dhcp enabled property
+     * @param[in] protocol - ipv4/ipv6
+     * @return bool - true if dhcpEnabled
+     */
+    bool isDHCPEnabled(IP::Protocol protocol, bool ignoreProtocol = false);
+
+    /* @brief Disables DHCP conf
+     * @param[in] protocol - ipv4/ipv6
+     */
+    void disableDHCP(IP::Protocol protocol);
+
+    /* @brief Set value of DHCPEnabled
+     * @param[in] value - value that determines if the dhcp is enabled/not
+     *                    possible values: both, none, v4, v6
+     */
+    HypEthernetIntf::DHCPConf
+        dhcpEnabled(HypEthernetIntf::DHCPConf value) override;
+
+    using HypEthernetIntf::dhcpEnabled;
+    using HypEthernetIntf::interfaceName;
+
+  protected:
+    /** @brief sdbusplus DBus bus connection. */
+    sdbusplus::bus::bus& bus;
+
+    /** @brief object path */
+    std::string objectPath;
+
+    /** @brief Parent of this object */
+    HypNetworkMgr& manager;
+};
+
+} // namespace network
+} // namespace phosphor
