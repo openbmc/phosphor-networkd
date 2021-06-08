512f1e261174c5d597cc6ef46a0b6e92fe2064d8
Asmitha Karunanithi <asmitk01@in.ibm.com>
2021-06-08 11:56:00 -0500

    Add ethernet objects under hypervisor network obj
    In this commit, ethernet interface dbus objects are created
    under the hypervisor network service.
    
    busctl tree xyz.openbmc_project.Network.Hypervisor
    └─/xyz
      └─/xyz/openbmc_project
        └─/xyz/openbmc_project/network
          └─/xyz/openbmc_project/network/hypervisor
            ├─/xyz/openbmc_project/network/hypervisor/config
            ├─/xyz/openbmc_project/network/hypervisor/eth0
            └─/xyz/openbmc_project/network/hypervisor/eth1
    
    Signed-off-by: Asmitha Karunanithi <asmitk01@in.ibm.com>
    Change-Id: I4222fa29a9df607c8291d7f81f7f3e837442bc26

--- src/ibm/hypervisor-network-mgr-src/hyp_network_manager_main.cpp
+++ src/ibm/hypervisor-network-mgr-src/hyp_network_manager_main.cpp
@@ -1,5 +1,7 @@
 #include "hyp_network_manager.hpp"
 
+#include <fmt/format.h>
+
 #include <sdeventplus/event.hpp>
 
 constexpr char DEFAULT_HYP_NW_OBJPATH[] =
@@ -7,7 +9,13 @@ constexpr char DEFAULT_HYP_NW_OBJPATH[] =
 constexpr char HYP_DEFAULT_NETWORK_BUSNAME[] =
     "xyz.openbmc_project.Network.Hypervisor";
 
-int main(int /*argc*/, char** /*argv*/)
+namespace phosphor
+{
+namespace network
+{
+std::unique_ptr<HypNetworkMgr> manager = nullptr;
+
+int main()
 {
     auto bus = sdbusplus::bus::new_default();
 
@@ -16,25 +24,42 @@ int main(int /*argc*/, char** /*argv*/)
 
     // Get default event loop
     auto event = sdeventplus::Event::get_default();
+    stdplus::signal::block(SIGTERM);
+    sdeventplus::source::Signal(event, SIGTERM, termCb).set_floating(true);
 
     // Attach the bus to sd_event to service user requests
     bus.attach_event(event.get(), SD_EVENT_PRIORITY_NORMAL);
 
     // Create hypervisor network manager dbus object
-    phosphor::network::HypNetworkMgr manager(bus, event,
-                                             DEFAULT_HYP_NW_OBJPATH);
+    manager =
+        std::make_unique<HypNetworkMgr>(bus, event, DEFAULT_HYP_NW_OBJPATH);
 
     // Create the hypervisor eth interface objects
-    manager.createIfObjects();
+    manager->createIfObjects();
 
     // Create the hypervisor system config object
-    manager.createSysConfObj();
-    const phosphor::network::SystemConfPtr& systemConfigObj =
-        manager.getSystemConf();
+    manager->createSysConfObj();
+    const SystemConfPtr& systemConfigObj = manager.getSystemConf();
     systemConfigObj->setHostName();
 
     bus.request_name(HYP_DEFAULT_NETWORK_BUSNAME);
 
-    event.loop();
-    return 0;
+    return event.loop();
+}
+
+} // namespace network
+} // namespace phosphor
+
+int main(int /*argc*/, char** /*argv*/)
+{
+    try
+    {
+        return phosphor::network::main();
+    }
+    catch (const std::exception& e)
+    {
+        auto msg = fmt::format("FAILED: {}", e.what());
+        log<level::ERR>(msg.c_str(), entry("ERROR", e.what()));
+        return 1;
+    }
 }
