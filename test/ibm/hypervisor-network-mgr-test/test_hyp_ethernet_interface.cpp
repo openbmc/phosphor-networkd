7d476650f4eda044e4991442c4a3fc86ff5a3b9a
Asmitha Karunanithi <asmitk01@in.ibm.com>
2021-06-08 11:56:00 -0500

    Adding ethernet and ip address objects & UTs
    In this commit, ethernet and ip address objects are created
    under the service.
    
    busctl tree xyz.openbmc_project.Network.Hypervisor
    └─/xyz
      └─/xyz/openbmc_project
        └─/xyz/openbmc_project/network
          └─/xyz/openbmc_project/network/hypervisor
            ├─/xyz/openbmc_project/network/hypervisor/config
            ├─/xyz/openbmc_project/network/hypervisor/eth0
            │ └─/xyz/openbmc_project/network/hypervisor/eth0/ipv4
            │   └─/xyz/openbmc_project/network/hypervisor/eth0/ipv4/addr0
            └─/xyz/openbmc_project/network/hypervisor/eth1
              └─/xyz/openbmc_project/network/hypervisor/eth1/ipv4
                └─/xyz/openbmc_project/network/hypervisor/eth1/ipv4/addr0
    
    The values for the properties of Ethernet and IP interfaces
    will be initialised by parsing the bios table.
    
    Tested By:
    
    busctl introspect xyz.openbmc_project.Network.Hypervisor \
    /xyz/openbmc_project/network/hypervisor/eth0/ipv4/addr0
    
    * Set the Dbus object property:
    busctl set-property xyz.openbmc_project.Network.Hypervisor \
    /xyz/openbmc_project/network/hypervisor/eth0/ipv4/addr0 \
    xyz.openbmc_project.Network.IP PrefixLength y 20
    
    * Get corresponding bios table attribute - the updated value will be reflected
    busctl call xyz.openbmc_project.BIOSConfigManager /xyz/openbmc_project/bios_config/manager
    xyz.openbmc_project.BIOSConfig.Manager GetAttribute s vmi_eth0_ipv4_prefix_length
    svv "xyz.openbmc_project.BIOSConfig.Manager.AttributeType.Integer" x 20 x 0
    
    The delete method will reset all the ip address properties
    to its defualt.
    
    The create method will update the ip address object with user-given
    values.
    
    Fixup:
    
    whenever the dhcp is
    configured, and 'dhcp enabled' prop is set to true, the ip addr
    dbus objects are updated with the dynamic ip and on a GET operation
    on that ethernet interface would give the ip address back to the
    user.
    
    PATCH -d '{"DHCPv4": {"DHCPEnabled" :true}}' https://${bmc}/redfish/v1/Systems/hypervisor/EthernetInterfaces/eth1
    
    GET https://${bmc}/redfish/v1/Systems/hypervisor/EthernetInterfaces/eth1{
    
    Whenever there is a change in the range of ip address in the dhcp
    configuration, the dhcp server assigns new ip & vmi sends back the
    notification to pldm, also this ip will be updated in the bios table.
    
    This change would listen on the properties
    changed signal on BaseBIOSTable property of bios config manager
    and in every dhcp ip updation, the respective dbus object will be
    updated with the new values that are in the bios table.
    
    Tested By:
    
    * DHCP already configured on eth1
    * Change the ip range in the dhcp conf file and restart the dhcpd
    * Bios table will be updated with the new ip
    * When the above updation happens, hyp networkd restarts
    * introspect on the eth1 dbus object to see the updated ip
    
    Signed-off-by: Asmitha Karunanithi <asmitk01@in.ibm.com>
    Change-Id: I4222fa29a9df607c8291d7f81f7f3e837442bc26

---
+++ test/ibm/hypervisor-network-mgr-test/test_hyp_ethernet_interface.cpp
@@ -0,0 +1,120 @@
+#include "mock_hyp_ethernet_interface.hpp"
+
+#include <sdbusplus/bus.hpp>
+#include <xyz/openbmc_project/Common/error.hpp>
+
+#include <gtest/gtest.h>
+
+namespace phosphor
+{
+namespace network
+{
+
+class TestHypEthernetInterface : public testing::Test
+{
+  public:
+    sdbusplus::bus::bus bus;
+    HypNetworkMgr manager;
+    sdeventplus::Event event = sdeventplus::Event::get_default();
+
+    MockHypEthernetInterface interface;
+    TestHypEthernetInterface() :
+        bus(sdbusplus::bus::new_default()),
+        manager(bus, event, "/xyz/openbmc_test/network/hypervisor"),
+        interface(makeInterface(bus, manager))
+    {
+        manager.setDefaultBIOSTableAttrsOnIntf("if0");
+        manager.setDefaultBIOSTableAttrsOnIntf("if1");
+        manager.setDefaultHostnameInBIOSTableAttrs();
+        interface.createIPAddrObj();
+    }
+
+    static MockHypEthernetInterface makeInterface(sdbusplus::bus::bus& bus,
+                                                  HypNetworkMgr& manager)
+    {
+        return {bus, "/xyz/openbmc_test/network/hypervisor/eth0", "eth0",
+                manager};
+    }
+
+    bool isIPObjExist(const std::string& intf, const std::string& ipaddress)
+    {
+        auto it = interface.addrs.find(intf);
+        if (it != interface.addrs.end())
+        {
+            if (ipaddress == (it->second)->address())
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    bool deleteIPObj(const std::string& intf, const std::string& ipaddress)
+    {
+        auto it = interface.addrs.find(intf);
+        if (it == interface.addrs.end())
+        {
+            return false;
+        }
+
+        if (ipaddress == (it->second)->address())
+        {
+            interface.addrs.erase(intf);
+            return true;
+        }
+        return false;
+    }
+};
+
+TEST_F(TestHypEthernetInterface, AddIPAddress)
+{
+    IP::Protocol addressType = IP::Protocol::IPv4;
+    bool createip = interface.createIP(interface, "eth0", addressType,
+                                       "10.10.10.10", 16, "10.10.10.1");
+    if (createip)
+    {
+        EXPECT_EQ(true, isIPObjExist("eth0", "10.10.10.10"));
+    }
+}
+
+TEST_F(TestHypEthernetInterface, AddMultipleAddress)
+{
+    IP::Protocol addressType = IP::Protocol::IPv4;
+    bool createip1 = interface.createIP(interface, "eth0", addressType,
+                                        "10.10.10.10", 16, "10.10.10.1");
+    if (createip1)
+    {
+        bool createip2 = interface.createIP(interface, "eth0", addressType,
+                                            "20.20.20.20", 16, "20.20.20.1");
+        if (createip2)
+        {
+            EXPECT_EQ(false, isIPObjExist("eth0", "10.10.10.10"));
+            EXPECT_EQ(true, isIPObjExist("eth0", "20.20.20.20"));
+        }
+    }
+}
+
+TEST_F(TestHypEthernetInterface, DeleteIPAddress)
+{
+    IP::Protocol addressType = IP::Protocol::IPv4;
+    bool createip = interface.createIP(interface, "eth0", addressType,
+                                       "20.20.20.20", 16, "20.20.20.1");
+    if (createip)
+    {
+        EXPECT_EQ(true, deleteIPObj("eth0", "20.20.20.20"));
+        EXPECT_EQ(false, isIPObjExist("eth0", "20.20.20.20"));
+    }
+}
+
+TEST_F(TestHypEthernetInterface, DeleteNonConfiguredIPAddr)
+{
+    IP::Protocol addressType = IP::Protocol::IPv4;
+    bool createip = interface.createIP(interface, "eth0", addressType,
+                                       "20.20.20.20", 16, "20.20.20.1");
+    if (createip)
+    {
+        EXPECT_EQ(false, deleteIPObj("eth0", "10.10.10.10"));
+    }
+}
+} // namespace network
+} // namespace phosphor
