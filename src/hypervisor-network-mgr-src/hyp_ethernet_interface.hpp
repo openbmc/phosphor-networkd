8741f62bca7ecfe4ed2cdbc3740cb762d89d43fa
Asmitha Karunanithi <asmitk01@in.ibm.com>
2021-06-08 11:56:00 -0500

    Adding ethernet and ip address objects
    In this commit, ethernet and ip address objects are created
    under the service.
    
    busctl tree xyz.openbmc_project.Network.Hypervisor
    └─/xyz
      └─/xyz/openbmc_project
        └─/xyz/openbmc_project/network
          └─/xyz/openbmc_project/network/hypervisor
            └─/xyz/openbmc_project/network/hypervisor/if0
              └─/xyz/openbmc_project/network/hypervisor/if0/ipv4
                └─/xyz/openbmc_project/network/hypervisor/if0/ipv4/a6528ad1
    
    The values for the properties of Ethernet and IP interfaces
    will be initialised by parsing the bios table.
    
    Whenever there is an update in the bios table, the corresponding
    dbus object will be updated, and vice-versa.
    
    Tested By:
    
    busctl introspect xyz.openbmc_project.Network.Hypervisor /xyz/openbmc_project/network/hypervisor/if0/ipv4/a6528ad1
    NAME                                TYPE      SIGNATURE RESULT/VALUE                             FLAGS
    org.freedesktop.DBus.Introspectable interface -         -                                        -
    .Introspect                         method    -         s                                        -
    org.freedesktop.DBus.Peer           interface -         -                                        -
    .GetMachineId                       method    -         s                                        -
    .Ping                               method    -         -                                        -
    org.freedesktop.DBus.Properties     interface -         -                                        -
    .Get                                method    ss        v                                        -
    .GetAll                             method    s         a{sv}                                    -
    .Set                                method    ssv       -                                        -
    .PropertiesChanged                  signal    sa{sv}as  -                                        -
    xyz.openbmc_project.Network.IP      interface -         -                                        -
    .Address                            property  s         "9.5.2.6"                                emits-change writable
    .Gateway                            property  s         "9.5.2.1"                                emits-change writable
    .Origin                             property  s         "xyz.openbmc_project.Network.IP.Address… emits-change writable
    .PrefixLength                       property  y         25                                       emits-change writable
    .Type                               property  s         "xyz.openbmc_project.Network.IP.Protoco… emits-change writable
    xyz.openbmc_project.Object.Delete   interface -         -                                        -
    .Delete                             method    -         -                                        -
    StaticNameServers                            property  as        0                                        emits-change writable
    
    * Set the Dbus object property:
    busctl set-property xyz.openbmc_project.Network.Hypervisor
    /xyz/openbmc_project/network/hypervisor/if0/ipv4/a6528ad1
    xyz.openbmc_project.Network.IP PrefixLength y 20
    
    * Get corresponding bios table attribute - the updated value will be reflected
    busctl call xyz.openbmc_project.BIOSConfigManager /xyz/openbmc_project/bios_config/manager
    xyz.openbmc_project.BIOSConfig.Manager GetAttribute s vmi_if0_ipv4_prefix_length
    svv "xyz.openbmc_project.BIOSConfig.Manager.AttributeType.Integer" x 20 x 0
    
    Signed-off-by: Asmitha Karunanithi <asmitk01@in.ibm.com>
    Change-Id: I4222fa29a9df607c8291d7f81f7f3e837442bc26

---
+++ src/hypervisor-network-mgr-src/hyp_ethernet_interface.hpp
@@ -0,0 +1,156 @@
+#pragma once
+
+#include "ethernet_interface.hpp"
+#include "hyp_ip_interface.hpp"
+#include "hyp_network_manager.hpp"
+
+#include <phosphor-logging/elog-errors.hpp>
+#include <phosphor-logging/log.hpp>
+#include <sdbusplus/bus.hpp>
+
+namespace phosphor
+{
+namespace network
+{
+
+class HypNetworkMgr; // forward declaration of hypervisor network manager.
+
+class HypIPAddress;
+
+using namespace phosphor::logging;
+using HypIP = sdbusplus::xyz::openbmc_project::Network::server::IP;
+
+using CreateIface = sdbusplus::server::object::object<
+    sdbusplus::xyz::openbmc_project::Network::server::EthernetInterface,
+    sdbusplus::xyz::openbmc_project::Collection::server::DeleteAll>;
+
+using biosTableType = std::map<std::string, std::variant<int64_t, std::string>>;
+
+using HypEthernetInterfaceIntf =
+    sdbusplus::xyz::openbmc_project::Network::server::EthernetInterface;
+
+static std::shared_ptr<sdbusplus::bus::match::match> matchBIOSAttrUpdate;
+
+/** @class HypEthernetInterface
+ *  @brief Hypervisor Ethernet Interface implementation.
+ */
+class HypEthInterface : public CreateIface
+{
+  public:
+    HypEthInterface() = delete;
+    HypEthInterface(const HypEthInterface&) = delete;
+    HypEthInterface& operator=(const HypEthInterface&) = delete;
+    HypEthInterface(HypEthInterface&&) = delete;
+    HypEthInterface& operator=(HypEthInterface&&) = delete;
+    virtual ~HypEthInterface() = default;
+
+    /** @brief Constructor to put object onto bus at a dbus path.
+     *  @param[in] bus - Bus to attach to.
+     *  @param[in] path - Path to attach at.
+     *  @param[in] parent - parent object.
+     */
+    HypEthInterface(sdbusplus::bus::bus& bus, const char* path,
+                    const std::string& intfName, HypNetworkMgr& parent) :
+        CreateIface(bus, path),
+        bus(bus), objectPath(path), manager(parent)
+    {
+        HypEthernetInterfaceIntf::interfaceName(intfName);
+
+        // register signal - watch bios tale for any updates
+        watchBaseBiosTable();
+        createIPAddressObjects();
+    };
+
+    /* @brief creates the IP dbus object
+     */
+    virtual void createIPAddressObjects();
+
+    /* @brief Function to delete the IP dbus object
+     *  @param[in] ipaddress - ipaddress to delete.
+     */
+    void deleteObject(const std::string& ipaddress);
+
+    /** @brief Delete all IP dbus objects.
+     */
+    void deleteAll();
+
+    /* @brief Fuction to watch the Base Bios Table for any
+     *        property change and update the aprropriate
+     *        property in the dbus object
+     */
+    void watchBaseBiosTable();
+
+    /* @brief Fuction to get the ip dbus object
+     *        w.r.t the attribute name given.
+     * @param[in] attrName - attrName
+     * @param[in] oldIpAddr - optional (needed in case of ip address change in
+     * bios table)
+     *
+     * @return pointer to the ip dbus object
+     */
+    std::shared_ptr<phosphor::network::HypIPAddress>
+        getIPAddrObject(std::string attrName, std::string /*oldIpAddr*/);
+
+    /* @brief Function to set the bios properties in the
+     *        ip dbus object given.
+     * @param[in] ipObj - pointer to the ip dbus object.
+     * @param[in] attrName - bios attribute name.
+     * @param[in] attrValue - bios attribute value.
+     */
+    void setBiosPropInDbus(
+        std::shared_ptr<phosphor::network::HypIPAddress> ipObj,
+        std::string attrName, std::variant<std::string, uint8_t> attrValue);
+
+    /* @brief Utility function to generate unique id
+              for the ip dbus object
+     * @param[in] ipaddress - ip address.
+     * @param[in] prefixLength - prefix length.
+     * @param[in] gateway - gateway
+     */
+    virtual std::string generateId(const std::string& ipaddress,
+                                   uint8_t prefixLength,
+                                   const std::string& gateway);
+
+    /* @brief Function to update the ip address property in
+              the dbus object
+     * @detail if there is a change in ip address in bios
+               table, the ip is updated in the dbus obj path
+     * @param[in] updatedIp - ip to update
+     */
+    void updateIPAddress(std::string ip, std::string updatedIp);
+
+    /* @brief Function tohat returns parent's bios attrs map
+     */
+    biosTableType getBiosAttrsMap();
+
+    /* @brief Function to set ip address properties in
+              the parent's bios attrs map
+     * @detail if there is a change in any properties either in bios
+               table or on the dbus object, the bios attrs map data member
+               of the parent should be updated with the latest value
+     * @param[in] attrName - attrName for which there is a change in value
+     * @param[in] attrValue - updated value
+     * @param[in] attrType - type of the attrValue (string/integer)
+     */
+    void setIpPropsInMap(std::string attrName,
+                         std::variant<std::string, int64_t> attrValue,
+                         std::string attrType);
+
+    using HypEthernetInterfaceIntf::interfaceName;
+
+  private:
+    /** @brief sdbusplus DBus bus connection. */
+    sdbusplus::bus::bus& bus;
+
+    /** @brief object path */
+    std::string objectPath;
+
+    /** @brief Parent of this object */
+    HypNetworkMgr& manager;
+
+    /** @brief List of the ipaddress and the ip dbus objects */
+    std::map<std::string, std::shared_ptr<HypIPAddress>> addrs;
+};
+
+} // namespace network
+} // namespace phosphor
