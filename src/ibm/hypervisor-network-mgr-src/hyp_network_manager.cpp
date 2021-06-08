a15de0a1548d9d0c424c174ea3a5702f8adf6882
Asmitha Karunanithi <asmitk01@in.ibm.com>
2021-06-08 11:56:00 -0500

    Add ethernet objects under hypervisor network obj
    In this commit, ethernet interface dbus objects are created
    under the hypervisor network service.
    
    busctl tree xyz.openbmc_project.Network.Hypervisor
    └─/xyz
      └─/xyz/openbmc_project
        └─/xyz/openbmc_project/network
          └─/xyz/openbmc_project/network/hypervisor
            ├─/xyz/openbmc_project/network/hypervisor/config
            ├─/xyz/openbmc_project/network/hypervisor/eth0
            └─/xyz/openbmc_project/network/hypervisor/eth1
    
    Signed-off-by: Asmitha Karunanithi <asmitk01@in.ibm.com>
    Change-Id: I4222fa29a9df607c8291d7f81f7f3e837442bc26

--- src/ibm/hypervisor-network-mgr-src/hyp_network_manager.cpp
+++ src/ibm/hypervisor-network-mgr-src/hyp_network_manager.cpp
@@ -216,11 +216,6 @@ void HypNetworkMgr::setBIOSTableAttrs()
     }
 }
 
-biosTableType HypNetworkMgr::getBIOSTableAttrs()
-{
-    return biosTableAttrs;
-}
-
 void HypNetworkMgr::createIfObjects()
 {
     setBIOSTableAttrs();
@@ -235,14 +230,20 @@ void HypNetworkMgr::createIfObjects()
     // created during init time to support the static
     // network configurations on the both.
     // create eth0 and eth1 objects
-    log<level::INFO>("Create eth0 and eth1 objects");
+    log<level::INFO>("Creating eth0 and eth1 objects");
+    interfaces.emplace("eth0",
+                       std::make_unique<HypEthInterface>(
+                           bus, (objectPath + "/eth0").c_str(), "eth0", *this));
+    interfaces.emplace("eth1",
+                       std::make_unique<HypEthInterface>(
+                           bus, (objectPath + "/eth1").c_str(), "eth1", *this));
 }
 
 void HypNetworkMgr::createSysConfObj()
 {
     systemConf.reset(nullptr);
-    this->systemConf = std::make_unique<phosphor::network::HypSysConfig>(
-        bus, objectPath + "/config", *this);
+    this->systemConf =
+        std::make_unique<HypSysConfig>(bus, objectPath + "/config", *this);
 }
 
 } // namespace network
