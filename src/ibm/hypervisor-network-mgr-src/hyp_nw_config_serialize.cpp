b0f28cef09d5326edcb4f28b7268d5107f228c85
Asmitha Karunanithi <asmitk01@in.ibm.com>
2021-11-19 06:22:53 -0600

    ibm/hyp-nw-mgr: Persist Enabled property on reboot
    The "Enabled" property of xyz.openbmc_project.Object.Enable
    interface of the ip address object
    (eg: /xyz/openbmc_project/network/hypervisor/eth<0/1>/ipv4/addr0)
    will be set by pldm once the host consumes the ip address set by
    the user. When the system reboots, this value is not persisted
    and is defaulted to false.
    Pldm will not be updating this value on reboot, it only sets it
    when phyp sends the sensor event when the ip is consumed and
    pldm receives this and sets the "Enabled" property.
    
    '''
    busctl introspect xyz.openbmc_project.Network.Hypervisor \
    /xyz/openbmc_project/network/hypervisor/eth0/ipv4/addr0
    '''
    
    The values will be stored under /var/lib/network/hypervisor/ path
    with the file name being eth0_network/eth1_network.
    
    Note:
    "Enabled" property is only persisted with this change, since all
    the other properties are read from the bios table.
    
    Tested By:
    
    * Set vmi ip through redfish command:
    [1] PATCH -D patch.txt -d '{"IPv4StaticAddresses":[{"Address": "10.6.6.2", "SubnetMask": "255.255.252.0","Gateway":"10.6.6.1"}]}' https://${bmc}/redfish/v1/Systems/hypervisor/EthernetInterfaces/eth0
    * reboot the system
    * check if the enabled property is still true on the ip dbus object
    
    Signed-off-by: Asmitha Karunanithi <asmitk01@in.ibm.com>
    Change-Id: Id7acd94c728bf4ab4167868f80b48c9171f2d5eb

---
+++ src/ibm/hypervisor-network-mgr-src/hyp_nw_config_serialize.cpp
@@ -0,0 +1,109 @@
+#include "hyp_nw_config_serialize.hpp"
+
+#include <phosphor-logging/lg2.hpp>
+
+#include <fstream>
+
+namespace phosphor
+{
+namespace network
+{
+namespace persistdata
+{
+
+void serialize(const NwConfigPropMap& list, std::string intf, std::string type)
+{
+    std::string filePath = HYP_NW_CONFIG_PERSIST_PATH + intf + "_" + type +
+                           "_network";
+
+    // Create directory if it doesnot exist
+    if (!std::filesystem::exists(HYP_NW_CONFIG_PERSIST_PATH.c_str()))
+    {
+        lg2::info("Creating directory to store hypervisor nw config data...");
+        std::filesystem::create_directory(HYP_NW_CONFIG_PERSIST_PATH.c_str());
+    }
+
+    std::ofstream serializeFile(filePath.c_str(), std::ios::out);
+    if (serializeFile)
+    {
+        for (auto itr = list.begin(); itr != list.end(); itr++)
+        {
+            if (auto value = std::get_if<bool>(&itr->second))
+            {
+                serializeFile << itr->first << " " << *value;
+            }
+            else if (auto value = std::get_if<std::string>(&itr->second))
+            {
+                serializeFile << itr->first << " " << *value;
+            }
+            else if (auto value = std::get_if<int64_t>(&itr->second))
+            {
+                serializeFile << itr->first << " " << *value;
+            }
+        }
+        serializeFile.close();
+    }
+    else
+    {
+        lg2::error("Couldn't open file. Exiting serialization");
+    }
+}
+
+bool deserialize(NwConfigPropMap& list, std::string intf, std::string type)
+{
+    std::string filePath = HYP_NW_CONFIG_PERSIST_PATH + intf + "_" + type +
+                           "_network";
+
+    try
+    {
+        if (std::filesystem::exists(filePath))
+        {
+            std::ifstream deSerializeFile(filePath.c_str(), std::ifstream::in);
+            std::string fileEntry;
+
+            while (getline(deSerializeFile, fileEntry))
+            {
+                std::string key;
+                std::string value;
+                std::stringstream sStream(fileEntry);
+
+                sStream >> key >> value;
+
+                // Check for each key and extract the value acc to the data type
+                // It should be converted to int for boolean and int64_t
+                // properties
+                // Bool and string types are handled here. This can be extended
+                // to properties of other types.
+                if (key == "Enabled")
+                {
+                    if (std::stoi(value))
+                    {
+                        list[key] = true;
+                    }
+                    else
+                    {
+                        list[key] = false;
+                    }
+                }
+                else
+                {
+                    // else - for all other properties of type string
+                    list[key] = value;
+                }
+            }
+            deSerializeFile.close();
+            return true;
+        }
+        return false;
+    }
+    catch (std::exception& e)
+    {
+        lg2::error("Failed to deserialize. Error: {ERR}", "ERR", e.what());
+        std::filesystem::remove(filePath);
+        return false;
+    }
+}
+
+} // namespace persistdata
+} // namespace network
+} // namespace phosphor
