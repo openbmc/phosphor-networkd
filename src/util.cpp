2f359ed8504121be2705a14c5dba3e9bc02963a4
Potin Lai <potin.lai@quantatw.com>
2021-12-26 20:08:20 +0800

    util: enhance MAC address parsing
    Current ether_aton does not support the MAC string without colons.
    Add extra conversion when string length equal to 12.
    
    Test log:
    1/7 config_parser             OK              0.01s
    2/7 neighbor                  OK              0.01s
    3/7 netlink                   OK              0.01s
    4/7 util                      OK              0.01s
    5/7 network_manager           OK              0.01s
    6/7 ethernet_interface        OK              0.01s
    7/7 vlan_interface            OK              0.01s
    
    Ok:                 7
    Expected Fail:      0
    Fail:               0
    Unexpected Pass:    0
    Skipped:            0
    Timeout:            0
    
    Signed-off-by: Potin Lai <potin.lai@quantatw.com>
    Change-Id: Id2125af85f1c3d75424ff6ea0980c1460614f14e

--- src/util.cpp
+++ src/util.cpp
@@ -633,14 +633,27 @@ ether_addr getfromInventory(sdbusplus::bus::bus& bus,
     return fromString(std::get<std::string>(value));
 }
 
-ether_addr fromString(const char* str)
+ether_addr fromString(const std::string& str)
 {
-    struct ether_addr* mac = ether_aton(str);
-    if (mac == nullptr)
+    std::string mac_str;
+    struct ether_addr mac;
+
+    // MAC address without colons
+    if (str.size() == 12)
     {
-        throw std::invalid_argument("Invalid MAC Address");
+        mac_str = str.substr(0, 2) + ":" + str.substr(2, 2) + ":" +
+                  str.substr(4, 2) + ":" + str.substr(6, 2) + ":" +
+                  str.substr(8, 2) + ":" + str.substr(10, 2);
     }
-    return *mac;
+    else
+        mac_str = str;
+
+    if (ether_aton_r(mac_str.c_str(), &mac) != nullptr)
+    {
+        return mac;
+    }
+
+    throw std::invalid_argument("Invalid MAC Address");
 }
 
 std::string toString(const ether_addr& mac)
