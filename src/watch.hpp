b1440e97e98a80e6b2c83fa2e1b3adcd8d2da69c
William A. Kennington III <wak@google.com>
2022-01-24 17:15:36 -0800

    watch: Simplify to use sd-event inotify
    We no longer need to set up our own watches and other logic, sd-event
    can do this for us.
    
    Change-Id: Ic40530bdd1958f52aa786a2eb0863c4a9455011e
    Signed-off-by: William A. Kennington III <wak@google.com>

--- src/watch.hpp
+++ src/watch.hpp
@@ -1,15 +1,8 @@
 #pragma once
-
-#include "dns_updater.hpp"
 #include "types.hpp"
-#include "util.hpp"
-
-#include <sys/inotify.h>
-#include <systemd/sd-event.h>
 
 #include <filesystem>
 #include <functional>
-#include <map>
 
 namespace phosphor
 {
@@ -18,10 +11,8 @@ namespace network
 namespace inotify
 {
 
-namespace fs = std::filesystem;
-
 // Auxiliary callback to be invoked on inotify events
-using UserCallBack = std::function<void(const std::string&)>;
+using UserCallBack = std::function<void(const char*)>;
 
 /** @class Watch
  *
@@ -42,66 +33,24 @@ class Watch
      *
      *  @param[in] eventPtr - Reference to sd_event wrapped in unique_ptr
      *  @param[in] path     - File path to be watched
-     *  @param[in] userFunc - User specific callback function on events
-     *  @param[in] flags    - Flags to be supplied to inotify
      *  @param[in] mask     - Mask of events to be supplied to inotify
-     *  @param[in] events   - Events to be watched
+     *  @param[in] userFunc - User specific callback function on events
      */
-    Watch(phosphor::network::EventPtr& eventPtr, const fs::path path,
-          UserCallBack userFunc, int flags = IN_NONBLOCK,
-          uint32_t mask = IN_CLOSE_WRITE, uint32_t events = EPOLLIN);
+    Watch(phosphor::network::EventPtr& eventPtr,
+          const std::filesystem::path path, uint32_t mask,
+          UserCallBack userFunc);
 
-    /** @brief Remove inotify watch and close fd's */
-    ~Watch()
-    {
-        if ((fd() >= 0) && (wd >= 0))
-        {
-            inotify_rm_watch(fd(), wd);
-        }
-    }
+    ~Watch();
 
   private:
-    /** @brief Callback invoked when inotify event fires
-     *
-     *  @details On a matching event, calls back into user supplied
-     *           function if there is one registered
-     *
-     *  @param[in] eventSource - Event source
-     *  @param[in] fd          - Inotify fd
-     *  @param[in] retEvents   - Events that matched for fd
-     *  @param[in] userData    - Pointer to Watch object
-     *
-     *  @returns 0 on success, -1 on fail
-     */
-    static int processEvents(sd_event_source* eventSource, int fd,
-                             uint32_t retEvents, void* userData);
-
-    /** @brief Initializes an inotify instance
-     *
-     *  @return Descriptor on success, -1 on failure
-     */
-    int inotifyInit();
-
-    /** @brief File path to be watched */
-    const fs::path path;
-
     /** @brief User callback function */
     UserCallBack userFunc;
 
-    /** @brief Inotify flags */
-    int flags;
+    /** @brief Event source */
+    sd_event_source* source = nullptr;
 
-    /** @brief Mask of events */
-    uint32_t mask;
-
-    /** @brief Events to be watched */
-    uint32_t events;
-
-    /** @brief Watch descriptor */
-    int wd = -1;
-
-    /** @brief File descriptor manager */
-    phosphor::Descriptor fd;
+    static int processEvent(sd_event_source* source,
+                            const struct inotify_event* event, void* userdata);
 };
 
 } // namespace inotify
