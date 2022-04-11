5aa2912961bda7a1b48df36052601d1f5a01aaa7
Jiaqing Zhao <jiaqing.zhao@intel.com>
2022-04-11 16:46:16 +0800

    Remove gateway properties in SystemConfiguration
    Now default gateway are per-interface properties, the system-wide one
    in the SystemConfiguration interface is deprecated. Updating the value
    also makes no difference, it will never be written to configuration.
    This patch removes the code of initializing and updating them.
    
    Tested:
    In SystemConfiguration interface, DefaultGateway and DefaultGateway6 are
    both empty strings, and setting these two properties gives NotAllowed
    error.
    
    Change-Id: I58ac52c5318943941dcc92abede7763bddbca596
    Signed-off-by: Jiaqing Zhao <jiaqing.zhao@intel.com>

--- src/system_configuration.cpp
+++ src/system_configuration.cpp
@@ -23,7 +23,7 @@ constexpr auto METHOD_SET = "SetStaticHostname";
 
 using namespace phosphor::logging;
 using namespace sdbusplus::xyz::openbmc_project::Common::Error;
-using InvalidArgumentMetadata = xyz::openbmc_project::Common::InvalidArgument;
+using Reason = xyz::openbmc_project::Common::NotAllowed::REASON;
 
 using SystemConfigIntf =
     sdbusplus::xyz::openbmc_project::Network::server::SystemConfiguration;
@@ -34,25 +34,7 @@ SystemConfiguration::SystemConfiguration(sdbusplus::bus::bus& bus,
     Iface(bus, objPath.c_str(), Iface::action::defer_emit),
     bus(bus), manager(parent)
 {
-    auto name = getHostNameFromSystem();
-
-    SystemConfigIntf::hostName(name);
-    const auto& gatewayList = manager.getRouteTable().getDefaultGateway();
-    const auto& gateway6List = manager.getRouteTable().getDefaultGateway6();
-    // Assign first entry of gateway list
-    std::string gateway;
-    std::string gateway6;
-    if (!gatewayList.empty())
-    {
-        gateway = gatewayList.begin()->second;
-    }
-    if (!gateway6List.empty())
-    {
-        gateway6 = gateway6List.begin()->second;
-    }
-
-    SystemConfigIntf::defaultGateway(gateway);
-    SystemConfigIntf::defaultGateway6(gateway6);
+    SystemConfigIntf::hostName(getHostNameFromSystem());
 
     this->emit_object_added();
 }
@@ -103,52 +85,16 @@ std::string SystemConfiguration::getHostNameFromSystem() const
     return "";
 }
 
-std::string SystemConfiguration::defaultGateway(std::string gateway)
+std::string SystemConfiguration::defaultGateway(std::string /*gateway*/)
 {
-    auto gw = SystemConfigIntf::defaultGateway();
-    if (gw == gateway)
-    {
-        return gw;
-    }
-
-    if (!isValidIP(AF_INET, gateway))
-    {
-        log<level::ERR>("Not a valid v4 Gateway",
-                        entry("GATEWAY=%s", gateway.c_str()));
-        elog<InvalidArgument>(
-            InvalidArgumentMetadata::ARGUMENT_NAME("GATEWAY"),
-            InvalidArgumentMetadata::ARGUMENT_VALUE(gateway.c_str()));
-    }
-    gw = SystemConfigIntf::defaultGateway(gateway);
-
-    manager.writeToConfigurationFile();
-    manager.reloadConfigs();
-
-    return gw;
+    elog<NotAllowed>(
+        Reason("Setting system-wide DefaultGateway is deprecated"));
 }
 
-std::string SystemConfiguration::defaultGateway6(std::string gateway)
+std::string SystemConfiguration::defaultGateway6(std::string /*gateway*/)
 {
-    auto gw = SystemConfigIntf::defaultGateway6();
-    if (gw == gateway)
-    {
-        return gw;
-    }
-
-    if (!isValidIP(AF_INET6, gateway))
-    {
-        log<level::ERR>("Not a valid v6 Gateway",
-                        entry("GATEWAY=%s", gateway.c_str()));
-        elog<InvalidArgument>(
-            InvalidArgumentMetadata::ARGUMENT_NAME("GATEWAY"),
-            InvalidArgumentMetadata::ARGUMENT_VALUE(gateway.c_str()));
-    }
-    gw = SystemConfigIntf::defaultGateway6(gateway);
-
-    manager.writeToConfigurationFile();
-    manager.reloadConfigs();
-
-    return gw;
+    elog<NotAllowed>(
+        Reason("Setting system-wide DefaultGateway6 is deprecated"));
 }
 
 } // namespace network
