d2606503bc6c9d6296396fdc26afea0335411974
Asmitha Karunanithi <asmitk01@in.ibm.com>
2022-04-21 00:06:20 -0500

    Add unit testcases for hypervisor nw manager
    Signed-off-by: Asmitha Karunanithi <asmitk01@in.ibm.com>
    Change-Id: I30ea30a071ae2f2bdee40f02913690790a45a47a

---
+++ test/ibm/hypervisor-network-mgr-test/test_hyp_ethernet_interface.cpp
@@ -0,0 +1,95 @@
+#include "mock_hyp_network_manager.hpp"
+#include "mock_hyp_ethernet_interface.hpp"
+
+#include <sdbusplus/bus.hpp>
+#include <xyz/openbmc_project/Common/error.hpp>
+
+namespace phosphor
+{
+namespace network
+{
+
+class TestHypEthernetInterface : public testing::Test
+{
+  public:
+    sdbusplus::bus::bus bus;
+    MockHypManager manager;
+    sdeventplus::Event event = sdeventplus::Event::get_default();
+    MockHypEthernetInterface interface;
+    TestHypEthernetInterface() :
+        bus(sdbusplus::bus::new_default()),
+        manager(bus, event, "/xyz/openbmc_test/network/hypervisor"),
+        interface(makeInterface(bus, manager))
+    {
+        createIPObj();
+    }
+
+    static MockHypEthernetInterface makeInterface(sdbusplus::bus::bus& bus,
+                                                  MockHypManager& manager)
+    {
+        return {bus, "/xyz/openbmc_test/network/hypervisor/eth0", "eth0", manager};
+    }
+
+    void createIPObj()
+    {
+        interface.createIPAddressObjects();
+    }
+
+    bool isIPObjectExist(const std::string& ipaddress)
+    {
+        auto it = interface.addrs.find(ipaddress);
+        if (it != interface.addrs.end())
+        {
+            return true;
+        }
+        return false;
+    }
+
+    bool deleteIPObject(const std::string& ipaddress)
+    {
+        auto it = interface.addrs.find(ipaddress);
+        if (it == interface.addrs.end())
+        {
+            return false;
+        }
+        interface.addrs.erase(it);
+        return true;
+    }
+
+    void createIPObject(IP::Protocol addressType, const std::string& ipaddress,
+                        uint8_t prefixLength, const std::string& gateway)
+    {
+        interface.ip(addressType, ipaddress, prefixLength, gateway);
+    }
+};
+
+TEST_F(TestHypEthernetInterface, AddIPAddress)
+{
+    IP::Protocol addressType = IP::Protocol::IPv4;
+    createIPObject(addressType, "10.10.10.10", 16, "10.10.10.1");
+    EXPECT_EQ(true, isIPObjectExist("10.10.10.10"));
+}
+
+TEST_F(TestHypEthernetInterface, AddMultipleAddress)
+{
+    IP::Protocol addressType = IP::Protocol::IPv4;
+    createIPObject(addressType, "10.10.10.10", 16, "10.10.10.1");
+    createIPObject(addressType, "20.20.20.20", 16, "20.20.20.1");
+    EXPECT_EQ(false, isIPObjectExist("10.10.10.10"));
+    EXPECT_EQ(true, isIPObjectExist("20.20.20.20"));
+}
+
+TEST_F(TestHypEthernetInterface, DeleteIPAddress)
+{
+    IP::Protocol addressType = IP::Protocol::IPv4;
+    createIPObject(addressType, "20.20.20.20", 16, "20.20.20.1");
+    EXPECT_EQ(true, deleteIPObject("20.20.20.20"));
+    EXPECT_EQ(false, isIPObjectExist("20.20.20.20"));
+}
+
+TEST_F(TestHypEthernetInterface, DeleteInvalidIPAddress)
+{
+    EXPECT_EQ(false, deleteIPObject("10.10.10.10"));
+}
+} // namespace network
+} // namespace phosphor
---
+++ test/ibm/hypervisor-network-mgr-test/test_hyp_network_manager.cpp
@@ -0,0 +1,48 @@
+#include "mock_hyp_network_manager.hpp"
+
+#include <sdbusplus/bus.hpp>
+#include <xyz/openbmc_project/Common/error.hpp>
+
+#include <net/if.h>
+
+namespace phosphor
+{
+namespace network
+{
+
+class TestHypNetworkManager : public testing::Test
+{
+  public:
+    sdbusplus::bus::bus bus;
+    MockHypManager manager;
+    sdeventplus::Event event = sdeventplus::Event::get_default();
+    TestHypNetworkManager() :
+        bus(sdbusplus::bus::new_default()),
+        manager(bus, event, "/xyz/openbmc_test/hyp/abc")
+    {
+        createIfObjects();
+    }
+
+    ~TestHypNetworkManager() = default;
+    void createIfObjects()
+    {
+        manager.interfaces.clear();
+        manager.createIfObjects();
+    }
+
+    int getIntfCount()
+    {
+        ethIntfMapType interfaces = manager.getEthIntfList();
+        return interfaces.size();
+    }
+};
+
+TEST_F(TestHypNetworkManager, CheckInterfaceCount)
+{
+    createIfObjects();
+    EXPECT_EQ(2, getIntfCount());
+}
+
+} // namespace network
+} // namespace phosphor
+
