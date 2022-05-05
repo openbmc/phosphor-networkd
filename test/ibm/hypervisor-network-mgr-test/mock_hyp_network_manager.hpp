e6315f6427d5ec24a76b4d20904fe186de38ccf8
Asmitha Karunanithi <asmitk01@in.ibm.com>
2022-05-05 03:19:45 -0500

    Add testcases for hypervisor nw manager
    This commit includes three changes:
    1. Add testcases for hypervisor nw manager
    2. Removal of vmi_if_count support, as it is no longer
       available in the bios table
    3. Add methods to set default values to bios table
       attributes
    
    Tested.
    
    Signed-off-by: Asmitha Karunanithi <asmitk01@in.ibm.com>
    Change-Id: I78397f653658c88097e4566632b0ce36c7b3861b

---
+++ test/ibm/hypervisor-network-mgr-test/mock_hyp_network_manager.hpp
@@ -0,0 +1,61 @@
+#pragma once
+
+#include "config.h"
+
+#include "hyp_network_manager.hpp"
+
+#include <gmock/gmock.h>
+
+namespace phosphor
+{
+namespace network
+{
+
+class MockHypManager : public phosphor::network::HypNetworkMgr
+{
+  public:
+    MockHypManager(sdbusplus::bus::bus& bus, sdeventplus::Event& event,
+                   const char* path) :
+        phosphor::network::HypNetworkMgr(bus, event, path)
+    {
+    }
+
+    void setDefaultBIOSTableAttrs()
+    {
+        biosTableAttrs.clear();
+        setIf0DefaultBIOSTableAttrs();
+        setIf1DefaultBIOSTableAttrs();
+        setDefaultHostnameInBIOSTableAttrs();
+    }
+
+    void setBIOSAttribute(std::string attrName, std::variant<std::string, int64_t> attrValue, std::string attrType)
+    {
+        auto findAttr = biosTableAttrs.find(attrName);
+        if (findAttr != biosTableAttrs.end())
+        {
+            if (attrType == "Integer")
+            {
+                int64_t value = std::get<int64_t>(attrValue);
+                if (value != std::get<int64_t>(findAttr->second))
+                {
+                    biosTableAttrs.erase(findAttr);
+                    biosTableAttrs.emplace(attrName, value);
+                }
+            }
+            else if (attrType == "String")
+            {
+                std::string value = std::get<std::string>(attrValue);
+                if (value != std::get<std::string>(findAttr->second))
+                {
+                    biosTableAttrs.erase(findAttr);
+                    biosTableAttrs.emplace(attrName, value);
+                }
+            }
+        }
+    }
+
+    friend class TestHypNetworkManager;
+};
+
+} // namespace network
+} // namespace phosphor
