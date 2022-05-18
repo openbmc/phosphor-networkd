17a626d4eb588532481e6da7a0ddfd772c080c6f
Jiaqing Zhao <jiaqing.zhao@intel.com>
2022-05-18 15:36:29 +0800

    routing_table: Remove unused Entry class
    The Entry class is no longer used.
    
    Tested:
    Build pass.
    
    Change-Id: Id111aeb6f53fde6a67d564ff0716aebf0b6f7fa3
    Signed-off-by: Jiaqing Zhao <jiaqing.zhao@intel.com>

--- src/routing_table.hpp
+++ src/routing_table.hpp
@@ -11,33 +11,6 @@ namespace network
 {
 namespace route
 {
-constexpr auto BUFSIZE = 4096;
-
-struct Entry
-{
-    // destination network
-    std::string destination;
-    // gateway for this network.
-    std::string gateway;
-    // interface for this route
-    std::string interface;
-    Entry(const std::string& dest, const std::string& gtw,
-          const std::string& intf) :
-        destination(dest),
-        gateway(gtw), interface(intf)
-    {
-    }
-
-    bool operator==(const Entry& rhs)
-    {
-        return this->destination == rhs.destination &&
-               this->gateway == rhs.gateway && this->interface == rhs.interface;
-    }
-};
-
-// Map of network address and the route entry
-using Map = std::map<std::string, struct Entry>;
-
 class Table
 {
   public:
@@ -74,7 +47,6 @@ class Table
 
     std::map<std::string, std::string> defaultGateway;  // default gateway list
     std::map<std::string, std::string> defaultGateway6; // default gateway list
-    Map routeList;                                      // List of routes
 };
 
 } // namespace route
