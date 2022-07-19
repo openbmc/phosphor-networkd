9dde616ae49608221084bb658a216a4a7da90706
Asmitha Karunanithi <asmitk01@in.ibm.com>
2022-07-19 01:32:55 -0500

    Add ip address objects for each ethernet intf & UTs
    In this commit, ip address dbus objects are created
    under the hypervisor network service.
    
    busctl tree xyz.openbmc_project.Network.Hypervisor
    └─/xyz
      └─/xyz/openbmc_project
        └─/xyz/openbmc_project/network
          └─/xyz/openbmc_project/network/hypervisor
            ├─/xyz/openbmc_project/network/hypervisor/config
            ├─/xyz/openbmc_project/network/hypervisor/eth0
            │ └─/xyz/openbmc_project/network/hypervisor/eth0/ipv4
            │   └─/xyz/openbmc_project/network/hypervisor/eth0/ipv4/addr0
            └─/xyz/openbmc_project/network/hypervisor/eth1
              └─/xyz/openbmc_project/network/hypervisor/eth1/ipv4
                └─/xyz/openbmc_project/network/hypervisor/eth1/ipv4/addr0
    
    The values for the properties of IP interfaces will be initialised
    by parsing the bios table.
    
    Tested By:
    
    busctl introspect xyz.openbmc_project.Network.Hypervisor \
    /xyz/openbmc_project/network/hypervisor/eth0/ipv4/addr0
    
    Signed-off-by: Asmitha Karunanithi <asmitk01@in.ibm.com>
    Change-Id: I8bbf75de7b535f27b3ec974e8774c54a3cfac145

---
+++ test/ibm/hypervisor-network-mgr-test/test_hyp_ethernet_interface.cpp
@@ -0,0 +1,59 @@
+#include "mock_hyp_ethernet_interface.hpp"
+
+#include <sdbusplus/bus.hpp>
+#include <xyz/openbmc_project/Common/error.hpp>
+
+#include <gtest/gtest.h>
+
+namespace phosphor
+{
+namespace network
+{
+
+class TestHypEthernetInterface : public testing::Test
+{
+  public:
+    sdbusplus::bus::bus bus;
+    HypNetworkMgr manager;
+
+    MockHypEthernetInterface interface;
+    TestHypEthernetInterface() :
+        bus(sdbusplus::bus::new_default()),
+        manager(bus, "/xyz/openbmc_test/network/hypervisor"),
+        interface(makeInterface(bus, manager))
+    {
+        manager.setDefaultBIOSTableAttrsOnIntf("if0");
+        manager.setDefaultBIOSTableAttrsOnIntf("if1");
+        manager.setDefaultHostnameInBIOSTableAttrs();
+        interface.createIPAddrObj();
+    }
+
+    static MockHypEthernetInterface makeInterface(sdbusplus::bus::bus& bus,
+                                                  HypNetworkMgr& manager)
+    {
+        return {bus, "/xyz/openbmc_test/network/hypervisor/eth0", "eth0",
+                manager};
+    }
+
+    bool isIPObjExist(const std::string& intf, const std::string& ipaddress)
+    {
+        auto it = interface.addrs.find(intf);
+        if (it != interface.addrs.end())
+        {
+            if (ipaddress == (it->second)->address())
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+};
+
+TEST_F(TestHypEthernetInterface, CheckIPAddress)
+{
+    EXPECT_EQ(true, isIPObjExist("eth0", "9.9.9.9"));
+    EXPECT_EQ(false, isIPObjExist("eth0", "10.10.10.10"));
+}
+
+} // namespace network
+} // namespace phosphor
