0aed38787224090da1bcb2a88a731b5e356efed6
Asmitha Karunanithi <asmitk01@in.ibm.com>
2022-07-19 01:32:55 -0500

    ibm/hyp-nw-mgr: Add ip objects for ethernet intf
    In this commit, ip address dbus objects are created
    under the hypervisor network service.
    
    busctl tree xyz.openbmc_project.Network.Hypervisor
    └─/xyz
      └─/xyz/openbmc_project
        └─/xyz/openbmc_project/network
          └─/xyz/openbmc_project/network/hypervisor
            ├─/xyz/openbmc_project/network/hypervisor/config
            ├─/xyz/openbmc_project/network/hypervisor/eth0
            │ └─/xyz/openbmc_project/network/hypervisor/eth0/ipv4
            │   └─/xyz/openbmc_project/network/hypervisor/eth0/ipv4/addr0
            └─/xyz/openbmc_project/network/hypervisor/eth1
              └─/xyz/openbmc_project/network/hypervisor/eth1/ipv4
                └─/xyz/openbmc_project/network/hypervisor/eth1/ipv4/addr0
    
    The values for the properties of IP interfaces will be initialized
    by parsing the bios table.
    
    Tested By:
    
    busctl introspect xyz.openbmc_project.Network.Hypervisor \
    /xyz/openbmc_project/network/hypervisor/eth0/ipv4/addr0
    
    Signed-off-by: Asmitha Karunanithi <asmitk01@in.ibm.com>
    Change-Id: I8bbf75de7b535f27b3ec974e8774c54a3cfac145

--- src/ibm/hypervisor-network-mgr-src/hyp_ethernet_interface.cpp
+++ src/ibm/hypervisor-network-mgr-src/hyp_ethernet_interface.cpp
@@ -1,5 +1,9 @@
 #include "hyp_ethernet_interface.hpp"
 
+#include <phosphor-logging/lg2.hpp>
+
+class HypEthInterface;
+
 namespace phosphor
 {
 namespace network
@@ -9,19 +13,223 @@ using namespace phosphor::logging;
 using namespace sdbusplus::xyz::openbmc_project::Common::Error;
 using Argument = xyz::openbmc_project::Common::InvalidArgument;
 
+constexpr auto BIOS_SERVICE = "xyz.openbmc_project.BIOSConfigManager";
+constexpr auto BIOS_OBJPATH = "/xyz/openbmc_project/bios_config/manager";
+constexpr auto BIOS_MGR_INTF = "xyz.openbmc_project.BIOSConfig.Manager";
+
+// The total number of vmi attributes defined in biosTableAttrs
+// currently is 9:
+// 4 attributes of interface 0
+// 4 attributes of interface 1
+// and 1 vmi_hostname attribute
+constexpr auto BIOS_ATTRS_SIZE = 9;
+
+template <typename Addr>
+static bool validIntfIP(Addr a) noexcept
+{
+    return a.isUnicast() && !a.isLoopback();
+}
+
+void HypEthInterface::setIpPropsInMap(
+    std::string attrName, std::variant<std::string, int64_t> attrValue,
+    std::string attrType)
+{
+    manager.get().setBIOSTableAttr(attrName, attrValue, attrType);
+}
+
 biosTableType HypEthInterface::getBiosAttrsMap()
 {
     return manager.get().getBIOSTableAttrs();
 }
 
-bool HypEthInterface::ipv6AcceptRA(bool value)
+void HypEthInterface::updateIPAddress(std::string ip, std::string updatedIp)
 {
-    auto currValue = ipv6AcceptRA();
-    if (currValue != value)
+    auto it = addrs.find(ip);
+    if (it != addrs.end())
     {
-        HypEthernetIntf::ipv6AcceptRA(value);
+        auto& ipObj = it->second;
+
+        // Delete the ip address from the local copy (addrs)
+        // and update it with the new ip and ip address object
+        if (deleteObject(ip))
+        {
+            addrs.emplace(updatedIp, std::move(ipObj));
+            lg2::info("Successfully updated ip address");
+            return;
+        }
+        lg2::error("Updation of ip address not successful");
+        return;
     }
-    return value;
+}
+
+bool HypEthInterface::deleteObject(const std::string& ipaddress)
+{
+    auto it = addrs.find(ipaddress);
+    if (it == addrs.end())
+    {
+        lg2::error("DeleteObject:Unable to find the object.");
+        return false;
+    }
+    addrs.erase(it);
+    lg2::info("Successfully deleted the ip address object");
+    return true;
+}
+
+std::string HypEthInterface::getIntfLabel()
+{
+    // This method returns if0/if1 based on the eth
+    // interface label eth0/eth1 in the object path
+    const std::string ethIntfLabel =
+        objectPath.str.substr(objectPath.str.rfind("/") + 1);
+    if (ethIntfLabel == "eth0")
+    {
+        return "if0";
+    }
+    else if (ethIntfLabel == "eth1")
+    {
+        return "if1";
+    }
+    return "";
+}
+
+void HypEthInterface::createIPAddressObjects()
+{
+    // Access the biosTableAttrs of the parent object to create the ip address
+    // object
+    const std::string intfLabel = getIntfLabel();
+    if (intfLabel == "")
+    {
+        lg2::error("Wrong interface name");
+        return;
+    }
+    std::string ipAddr;
+    HypIP::AddressOrigin ipOrigin;
+    uint8_t ipPrefixLength;
+    std::string ipGateway;
+
+    auto biosTableAttrs = manager.get().getBIOSTableAttrs();
+
+    if (biosTableAttrs.size() < BIOS_ATTRS_SIZE)
+    {
+        lg2::info("Creating ip address object with default values");
+        std::optional<stdplus::InAnyAddr> addr;
+        addr.emplace(stdplus::fromStr<stdplus::In4Addr>("0.0.0.0"));
+
+        std::optional<stdplus::SubnetAny> ifaddr;
+        ifaddr.emplace(*addr, 0);
+        if (intfLabel == "if0")
+        {
+            // set the default values for interface 0 in the local
+            // copy of the bios table - biosTableAttrs
+            manager.get().setDefaultBIOSTableAttrsOnIntf(intfLabel);
+            addrs.emplace("eth0", std::make_unique<HypIPAddress>(
+                                      bus,
+                                      sdbusplus::message::object_path(
+                                          objectPath.str + "/ipv4/addr0"),
+                                      *this, *ifaddr, "0.0.0.0",
+                                      HypIP::AddressOrigin::Static, intfLabel));
+        }
+        else if (intfLabel == "if1")
+        {
+            // set the default values for interface 0 in the local
+            // copy of the bios table - biosTableAttrs
+            manager.get().setDefaultBIOSTableAttrsOnIntf(intfLabel);
+            addrs.emplace("eth1", std::make_unique<HypIPAddress>(
+                                      bus,
+                                      sdbusplus::message::object_path(
+                                          objectPath.str + "/ipv4/addr0"),
+                                      *this, *ifaddr, "0.0.0.0",
+                                      HypIP::AddressOrigin::Static, intfLabel));
+        }
+        return;
+    }
+
+    // Protocol list will be extended to ipv6 in future commit
+    for (std::string protocol : {"ipv4"})
+    {
+        std::string vmi_prefix = "vmi_" + intfLabel + "_" + protocol + "_";
+
+        auto biosTableItr = biosTableAttrs.find(vmi_prefix + "method");
+        if (biosTableItr != biosTableAttrs.end())
+        {
+            std::string ipType = std::get<std::string>(biosTableItr->second);
+            if (ipType.find("Static") != std::string::npos)
+            {
+                ipOrigin = HypIP::AddressOrigin::Static;
+                // update the dhcp enabled property of the eth interface
+                if (protocol == "ipv4")
+                {
+                    dhcp4(false);
+                }
+            }
+            else if (ipType.find("DHCP") != std::string::npos)
+            {
+                ipOrigin = HypIP::AddressOrigin::DHCP;
+                // update the dhcp enabled property of the eth interface
+                if (protocol == "ipv4")
+                {
+                    dhcp4(true);
+                }
+            }
+            else
+            {
+                lg2::error("Error - Neither Static/DHCP");
+            }
+        }
+        else
+        {
+            continue;
+        }
+
+        biosTableItr = biosTableAttrs.find(vmi_prefix + "ipaddr");
+        if (biosTableItr != biosTableAttrs.end())
+        {
+            ipAddr = std::get<std::string>(biosTableItr->second);
+        }
+
+        biosTableItr = biosTableAttrs.find(vmi_prefix + "prefix_length");
+        if (biosTableItr != biosTableAttrs.end())
+        {
+            ipPrefixLength =
+                static_cast<uint8_t>(std::get<int64_t>(biosTableItr->second));
+        }
+
+        biosTableItr = biosTableAttrs.find(vmi_prefix + "gateway");
+        if (biosTableItr != biosTableAttrs.end())
+        {
+            ipGateway = std::get<std::string>(biosTableItr->second);
+        }
+
+        std::optional<stdplus::InAnyAddr> addr;
+        try
+        {
+            if (protocol == "ipv4")
+            {
+                addr.emplace(stdplus::fromStr<stdplus::In4Addr>(ipAddr));
+            }
+            else
+            {
+                throw std::logic_error("Exhausted protocols");
+            }
+        }
+        catch (const std::exception& e)
+        {
+            lg2::error("Invalid IP {NET_IP}: {ERROR}", "NET_IP", ipAddr,
+                       "ERROR", e);
+            elog<InvalidArgument>(Argument::ARGUMENT_NAME("ipAddr"),
+                                  Argument::ARGUMENT_VALUE(ipAddr.c_str()));
+        }
+        std::optional<stdplus::SubnetAny> ifaddr;
+        ifaddr.emplace(*addr, ipPrefixLength);
+        std::string ipObjId = "addr0";
+
+        addrs.emplace(ipAddr,
+                      std::make_unique<HypIPAddress>(
+                          bus,
+                          sdbusplus::message::object_path(
+                              objectPath.str + "/" + protocol + "/" + ipObjId),
+                          *this, *ifaddr, ipGateway, ipOrigin, intfLabel));
+    }
 }
 
 bool HypEthInterface::dhcp4(bool value)
@@ -34,60 +242,27 @@ bool HypEthInterface::dhcp4(bool value)
     return value;
 }
 
-bool HypEthInterface::dhcp6(bool value)
-{
-    auto currValue = dhcp6();
-    if (currValue != value)
-    {
-        HypEthernetIntf::dhcp6(value);
-    }
-    return value;
-}
-
-bool HypEthInterface::dhcpIsEnabled(HypIP::Protocol family)
-{
-    switch (family)
-    {
-        case HypIP::Protocol::IPv6:
-            return dhcp6();
-        case HypIP::Protocol::IPv4:
-            return dhcp4();
-    }
-    throw std::logic_error("Unreachable");
-}
-
 HypEthInterface::DHCPConf HypEthInterface::dhcpEnabled(DHCPConf value)
 {
     auto old4 = HypEthernetIntf::dhcp4();
-    auto new4 = HypEthernetIntf::dhcp4(value == DHCPConf::v4 ||
-                                       value == DHCPConf::v4v6stateless ||
-                                       value == DHCPConf::both);
-    auto old6 = HypEthernetIntf::dhcp6();
-    auto new6 = HypEthernetIntf::dhcp6(value == DHCPConf::v6 ||
-                                       value == DHCPConf::both);
-    auto oldra = HypEthernetIntf::ipv6AcceptRA();
-    auto newra = HypEthernetIntf::ipv6AcceptRA(
-        value == DHCPConf::v6stateless || value == DHCPConf::v4v6stateless ||
-        value == DHCPConf::v6 || value == DHCPConf::both);
+    auto new4 = HypEthernetIntf::dhcp4(value == DHCPConf::v4);
 
-    if (old4 != new4 || old6 != new6 || oldra != newra)
+    if (old4 == new4)
     {
-        HypEthernetIntf::dhcpEnabled(value);
+        // if new value is the same as old value
+        return value;
     }
+    HypEthernetIntf::dhcpEnabled(value);
     return value;
 }
 
 HypEthInterface::DHCPConf HypEthInterface::dhcpEnabled() const
 {
-    if (dhcp6())
+    if (dhcp4())
     {
-        return dhcp4() ? DHCPConf::both : DHCPConf::v6;
+        return DHCPConf::v4;
     }
-    else if (dhcp4())
-    {
-        return ipv6AcceptRA() ? DHCPConf::v4v6stateless : DHCPConf::v4;
-    }
-    return ipv6AcceptRA() ? DHCPConf::v6stateless : DHCPConf::none;
+    return DHCPConf::none;
 }
 
 } // namespace network
---
+++ test/ibm/hypervisor-network-mgr-test/mock_hyp_ethernet_interface.hpp
@@ -0,0 +1,54 @@
+#pragma once
+
+#include "hyp_ethernet_interface.hpp"
+#include "hyp_ip_interface.hpp"
+
+namespace phosphor
+{
+namespace network
+{
+
+class MockHypEthernetInterface : public HypEthInterface
+{
+  public:
+    MockHypEthernetInterface(stdplus::PinnedRef<sdbusplus::bus_t> bus,
+                             sdbusplus::message::object_path path,
+                             std::string_view intfName,
+                             stdplus::PinnedRef<HypNetworkMgr> parent) :
+        HypEthInterface(bus, path, intfName, parent)
+    {}
+
+    void createIPAddrObj()
+    {
+        std::optional<stdplus::InAnyAddr> addr_eth0, addr_eth1;
+        addr_eth0.emplace(stdplus::fromStr<stdplus::In4Addr>("9.9.9.9"));
+
+        std::optional<stdplus::SubnetAny> ifaddr_eth0, ifaddr_eth1;
+        ifaddr_eth0.emplace(*addr_eth0, 0);
+
+        addrs.emplace(
+            "eth0",
+            std::make_unique<HypIPAddress>(
+                bus,
+                sdbusplus::message::object_path(
+                    "/xyz/openbmc_test/network/hypervisor/eth0/ipv4/addr0"),
+                *this, *ifaddr_eth0, "9.9.9.1", HypIP::AddressOrigin::Static,
+                "if0"));
+
+        addr_eth1.emplace(stdplus::fromStr<stdplus::In4Addr>("8.8.8.8"));
+        ifaddr_eth1.emplace(*addr_eth1, 0);
+
+        addrs.emplace(
+            "eth1",
+            std::make_unique<HypIPAddress>(
+                bus,
+                sdbusplus::message::object_path(
+                    "/xyz/openbmc_test/network/hypervisor/eth1/ipv4/addr0"),
+                *this, *ifaddr_eth1, "8.8.8.1", HypIP::AddressOrigin::Static,
+                "if1"));
+    }
+
+    friend class TestHypEthernetInterface;
+};
+} // namespace network
+} // namespace phosphor
