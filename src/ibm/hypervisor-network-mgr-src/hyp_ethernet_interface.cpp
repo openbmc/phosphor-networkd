68dda4603462aaa0b9460705bdb10bc45479646c
Asmitha Karunanithi <asmitk01@in.ibm.com>
2022-07-19 06:25:07 -0500

    Add DHCP functionalities to hypervisor eth interfaces
    This commit adds methods that provides dhcp functionalities on
    the ethernet interface like - disable dhcp on an ethernet interface,
    to check if dhcp is enabled on an interface.
    
    Signed-off-by: Asmitha Karunanithi <asmitk01@in.ibm.com>
    Change-Id: Ic713782d48c4c3cc1e1d029212b3ecf7ff5f39ad

--- src/ibm/hypervisor-network-mgr-src/hyp_ethernet_interface.cpp
+++ src/ibm/hypervisor-network-mgr-src/hyp_ethernet_interface.cpp
@@ -26,5 +26,107 @@ biosTableType HypEthInterface::getBiosAttrsMap()
     return manager.getBIOSTableAttrs();
 }
 
+void HypEthInterface::disableDHCP(HypIP::Protocol protocol)
+{
+    DHCPConf dhcpState = HypEthernetIntf::dhcpEnabled();
+    if (dhcpState == HypEthInterface::DHCPConf::both)
+    {
+        if (protocol == HypIP::Protocol::IPv4)
+        {
+            dhcpEnabled(HypEthInterface::DHCPConf::v6);
+        }
+        else if (protocol == HypIP::Protocol::IPv6)
+        {
+            dhcpEnabled(HypEthInterface::DHCPConf::v4);
+        }
+    }
+    else if ((dhcpState == HypEthInterface::DHCPConf::v4) &&
+             (protocol == HypIP::Protocol::IPv4))
+    {
+        dhcpEnabled(HypEthInterface::DHCPConf::none);
+    }
+    else if ((dhcpState == HypEthInterface::DHCPConf::v6) &&
+             (protocol == HypIP::Protocol::IPv6))
+    {
+        dhcpEnabled(HypEthInterface::DHCPConf::none);
+    }
+}
+
+bool HypEthInterface::ipv6AcceptRA(bool value)
+{
+    auto currValue = ipv6AcceptRA();
+    if (currValue != value)
+    {
+        HypEthernetIntf::ipv6AcceptRA(value);
+    }
+    return value;
+}
+
+bool HypEthInterface::dhcp4(bool value)
+{
+    auto currValue = dhcp4();
+    if (currValue != value)
+    {
+        HypEthernetIntf::dhcp4(value);
+    }
+    return value;
+}
+
+bool HypEthInterface::dhcp6(bool value)
+{
+    auto currValue = dhcp6();
+    if (currValue != value)
+    {
+        HypEthernetIntf::dhcp6(value);
+    }
+    return value;
+}
+
+bool HypEthInterface::dhcpIsEnabled(IP::Protocol family)
+{
+    switch (family)
+    {
+        case IP::Protocol::IPv6:
+            return dhcp6();
+        case IP::Protocol::IPv4:
+            return dhcp4();
+    }
+    throw std::logic_error("Unreachable");
+}
+
+HypEthInterface::DHCPConf HypEthInterface::dhcpEnabled(DHCPConf value)
+{
+    auto old4 = HypEthernetIntf::dhcp4();
+    auto new4 = HypEthernetIntf::dhcp4(value == DHCPConf::v4 ||
+                                       value == DHCPConf::v4v6stateless ||
+                                       value == DHCPConf::both);
+    auto old6 = HypEthernetIntf::dhcp6();
+    auto new6 = HypEthernetIntf::dhcp6(value == DHCPConf::v6 ||
+                                       value == DHCPConf::both);
+    auto oldra = HypEthernetIntf::ipv6AcceptRA();
+    auto newra = HypEthernetIntf::ipv6AcceptRA(
+        value == DHCPConf::v6stateless || value == DHCPConf::v4v6stateless ||
+        value == DHCPConf::v6 || value == DHCPConf::both);
+
+    if (old4 != new4 || old6 != new6 || oldra != newra)
+    {
+        HypEthernetIntf::dhcpEnabled(value);
+    }
+    return value;
+}
+
+HypEthInterface::DHCPConf HypEthInterface::dhcpEnabled() const
+{
+    if (dhcp6())
+    {
+        return dhcp4() ? DHCPConf::both : DHCPConf::v6;
+    }
+    else if (dhcp4())
+    {
+        return ipv6AcceptRA() ? DHCPConf::v4v6stateless : DHCPConf::v4;
+    }
+    return ipv6AcceptRA() ? DHCPConf::v6stateless : DHCPConf::none;
+}
+
 } // namespace network
 } // namespace phosphor
