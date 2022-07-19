3f009b10ff969cc54e4d96803e309adabecdfeff
Asmitha Karunanithi <asmitk01@in.ibm.com>
2022-07-19 01:32:55 -0500

    Add ip address objects for each ethernet intf & UTs
    In this commit, ip address dbus objects are created
    under the hypervisor network service.
    
    busctl tree xyz.openbmc_project.Network.Hypervisor
    └─/xyz
      └─/xyz/openbmc_project
        └─/xyz/openbmc_project/network
          └─/xyz/openbmc_project/network/hypervisor
            ├─/xyz/openbmc_project/network/hypervisor/config
            ├─/xyz/openbmc_project/network/hypervisor/eth0
            │ └─/xyz/openbmc_project/network/hypervisor/eth0/ipv4
            │   └─/xyz/openbmc_project/network/hypervisor/eth0/ipv4/addr0
            └─/xyz/openbmc_project/network/hypervisor/eth1
              └─/xyz/openbmc_project/network/hypervisor/eth1/ipv4
                └─/xyz/openbmc_project/network/hypervisor/eth1/ipv4/addr0
    
    The values for the properties of IP interfaces will be initialised
    by parsing the bios table.
    
    Tested By:
    
    busctl introspect xyz.openbmc_project.Network.Hypervisor \
    /xyz/openbmc_project/network/hypervisor/eth0/ipv4/addr0
    
    * Set the Dbus object property:
    busctl set-property xyz.openbmc_project.Network.Hypervisor \
    /xyz/openbmc_project/network/hypervisor/eth0/ipv4/addr0 \
    xyz.openbmc_project.Network.IP PrefixLength y 20
    
    * Get corresponding bios table attribute - the updated value will be reflected
    busctl call xyz.openbmc_project.BIOSConfigManager /xyz/openbmc_project/bios_config/manager
    xyz.openbmc_project.BIOSConfig.Manager GetAttribute s vmi_eth0_ipv4_prefix_length
    svv "xyz.openbmc_project.BIOSConfig.Manager.AttributeType.Integer" x 20 x 0
    
    The delete method will reset all the ip address properties
    to its defualt.
    
    The create method will update the ip address object with user-given
    values.
    
    PATCH -d '{"DHCPv4": {"DHCPEnabled" :true}}' https://${bmc}/redfish/v1/Systems/hypervisor/EthernetInterfaces/eth1
    
    GET https://${bmc}/redfish/v1/Systems/hypervisor/EthernetInterfaces/eth1
    
    Signed-off-by: Asmitha Karunanithi <asmitk01@in.ibm.com>
    Change-Id: I8bbf75de7b535f27b3ec974e8774c54a3cfac145

---
+++ test/ibm/hypervisor-network-mgr-test/mock_hyp_ethernet_interface.hpp
@@ -0,0 +1,99 @@
+#pragma once
+
+#include "hyp_ethernet_interface.hpp"
+#include "hyp_ip_interface.hpp"
+
+namespace phosphor
+{
+namespace network
+{
+
+class MockHypEthernetInterface : public HypEthInterface
+{
+  public:
+    MockHypEthernetInterface(sdbusplus::bus::bus& bus, const char* path,
+                             const std::string& intfName,
+                             HypNetworkMgr& parent) :
+        HypEthInterface(bus, path, intfName, parent)
+    {
+    }
+
+    void createIPAddrObj()
+    {
+        addrs.emplace(
+            "eth0",
+            std::make_unique<HypIPAddress>(
+                bus, "/xyz/openbmc_test/network/hypervisor/eth0/ipv4/addr0",
+                *this, HypIP::Protocol::IPv4, "0.0.0.0",
+                HypIP::AddressOrigin::Static, 0, "0.0.0.0", "if0"));
+        addrs.emplace(
+            "eth1",
+            std::make_unique<HypIPAddress>(
+                bus, "/xyz/openbmc_test/network/hypervisor/eth1/ipv4/addr0",
+                *this, HypIP::Protocol::IPv4, "0.0.0.0",
+                HypIP::AddressOrigin::Static, 0, "0.0.0.0", "if1"));
+    }
+
+    bool createIP(MockHypEthernetInterface& interface, std::string intfLabel,
+                  HypIP::Protocol protType, const std::string& ipaddress,
+                  uint8_t prefixLength, const std::string& gateway)
+    {
+        if (interface.dhcpIsEnabled(protType))
+        {
+            switch (protType)
+            {
+                case HypIP::Protocol::IPv4:
+                    interface.dhcp4(false);
+                    break;
+                case HypIP::Protocol::IPv6:
+                    interface.dhcp6(false);
+                    break;
+            }
+        }
+
+        HypIP::AddressOrigin origin = HypIP::AddressOrigin::Static;
+
+        if (!isValidIP(AF_INET, ipaddress) && !isValidIP(AF_INET6, ipaddress))
+        {
+            // Not a valid IP address
+            return false;
+        }
+
+        if (!isValidIP(AF_INET, gateway) && !isValidIP(AF_INET6, gateway))
+        {
+            // Not a valid gateway
+            return false;
+        }
+
+        if (!isValidPrefix(AF_INET, prefixLength) &&
+            !isValidPrefix(AF_INET6, prefixLength))
+        {
+            // PrefixLength is not correct
+            return false;
+        }
+
+        const std::string ipObjId = "addr0";
+        std::string protocol;
+        if (protType == HypIP::Protocol::IPv4)
+        {
+            protocol = "ipv4";
+        }
+        else if (protType == HypIP::Protocol::IPv6)
+        {
+            protocol = "ipv6";
+        }
+
+        std::string objPath = objectPath + "/" + protocol + "/" + ipObjId;
+
+        addrs.erase(intfLabel);
+
+        addrs[intfLabel] = std::make_unique<HypIPAddress>(
+            bus, (objPath).c_str(), *this, protType, ipaddress, origin,
+            prefixLength, gateway, "if0");
+        return true;
+    }
+
+    friend class TestHypEthernetInterface;
+};
+} // namespace network
+} // namespace phosphor
