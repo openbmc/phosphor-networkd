7269e42614dfbfb4161ac8085ef35c664b4d0bdf
William A. Kennington III <wak@google.com>
2022-11-03 12:50:00 -0700

    types: Add constexpr ether_addr parser
    Change-Id: I0ad21490239ad9b0f25dfff84726b16a9cfda927
    Signed-off-by: William A. Kennington III <wak@google.com>

--- test/test_types.cpp
+++ test/test_types.cpp
@@ -87,6 +87,39 @@ TEST(EqualOperator, InAddrAny)
     EXPECT_NE(InAddrAny(in6_addr{1}), InAddrAny(in6_addr{}));
 }
 
+TEST(ToAddr, EtherAddr)
+{
+    EXPECT_THROW(ToAddr<ether_addr>{}("0x:00:00:00:00:00"),
+                 std::invalid_argument);
+    EXPECT_THROW(ToAddr<ether_addr>{}("00:00:00:00:00"), std::invalid_argument);
+    EXPECT_THROW(ToAddr<ether_addr>{}("00:00:00:00:00:"),
+                 std::invalid_argument);
+    EXPECT_THROW(ToAddr<ether_addr>{}("00:00:00:00::00"),
+                 std::invalid_argument);
+    EXPECT_THROW(ToAddr<ether_addr>{}(":00:00:00:00:00"),
+                 std::invalid_argument);
+    EXPECT_THROW(ToAddr<ether_addr>{}("00::00:00:00:00"),
+                 std::invalid_argument);
+    EXPECT_THROW(ToAddr<ether_addr>{}(":::::"), std::invalid_argument);
+    EXPECT_THROW(ToAddr<ether_addr>{}("00:0:0:0:0"), std::invalid_argument);
+    EXPECT_THROW(ToAddr<ether_addr>{}("00:00:00:00:00:00:00"),
+                 std::invalid_argument);
+    EXPECT_THROW(ToAddr<ether_addr>{}(""), std::invalid_argument);
+    EXPECT_THROW(ToAddr<ether_addr>{}("123456789XYZ"), std::invalid_argument);
+    EXPECT_THROW(ToAddr<ether_addr>{}("123456789AB"), std::overflow_error);
+    EXPECT_THROW(ToAddr<ether_addr>{}("123456789ABCD"), std::overflow_error);
+
+    EXPECT_EQ((ether_addr{}), ToAddr<ether_addr>{}("00:00:00:00:00:00"));
+    EXPECT_EQ((ether_addr{0xff, 0xee, 0xdd, 0xcc, 0xbb, 0xaa}),
+              ToAddr<ether_addr>{}("FF:EE:DD:cc:bb:aa"));
+    EXPECT_EQ((ether_addr{0x00, 0x01, 0x02, 0x03, 0x04, 0x05}),
+              ToAddr<ether_addr>{}("0:1:2:3:4:5"));
+    EXPECT_EQ((ether_addr{0x01, 0x23, 0x45, 0x67, 0x89, 0xab}),
+              ToAddr<ether_addr>{}("0123456789AB"));
+    EXPECT_EQ((ether_addr{0xff, 0xee, 0xdd, 0xcc, 0xbb, 0xaa}),
+              ToAddr<ether_addr>{}("FFEEDDccbbaa"));
+}
+
 namespace detail
 {
 
