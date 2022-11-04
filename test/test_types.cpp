3336ade240fdb3d290daeeaa6b6fa314b817ed59
William A. Kennington III <wak@google.com>
2022-11-04 15:19:36 -0700

    types: Add constexpr int encoder
    Change-Id: I4a3117f442c5a829faffbb5897eb2a0ea013f63a
    Signed-off-by: William A. Kennington III <wak@google.com>

--- test/test_types.cpp
+++ test/test_types.cpp
@@ -74,6 +74,31 @@ TEST(DecodeInt, uint8_16)
     EXPECT_THROW(d("100"), std::overflow_error);
 }
 
+TEST(EncodeInt, uint8_10)
+{
+    EncodeInt<uint8_t, 10> e;
+    static_assert(e.buf_size == 3);
+    char buf[e.buf_size];
+    EXPECT_EQ("0", std::string_view(buf, e(buf, 0)));
+    EXPECT_EQ("42", std::string_view(buf, e(buf, 42)));
+    EXPECT_EQ("255", std::string_view(buf, e(buf, 255)));
+    EXPECT_EQ("000", std::string_view(buf, e(buf, 0, 3)));
+    EXPECT_EQ("255", std::string_view(buf, e(buf, 255, 3)));
+}
+
+TEST(EncodeInt, uint8_16)
+{
+    EncodeInt<uint8_t, 16> e;
+    static_assert(e.buf_size == 2);
+    char buf[e.buf_size];
+    EXPECT_EQ("0", std::string_view(buf, e(buf, 0)));
+    EXPECT_EQ("2a", std::string_view(buf, e(buf, 42)));
+    EXPECT_EQ("ff", std::string_view(buf, e(buf, 255)));
+    EXPECT_EQ("00", std::string_view(buf, e(buf, 0, 2)));
+    EXPECT_EQ("02", std::string_view(buf, e(buf, 2, 2)));
+    EXPECT_EQ("ff", std::string_view(buf, e(buf, 255, 2)));
+}
+
 TEST(EqualOperator, InAddrAny)
 {
     EXPECT_EQ(InAddrAny(in6_addr{0xff, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
