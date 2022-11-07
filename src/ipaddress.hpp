eff29697f4aa69667d172750b7fee57dcddbaafe
William A. Kennington III <wak@google.com>
2022-11-06 18:17:33 -0800

    rtnetlink: Migrate IP functions
    Change-Id: I4c87b59306911df4c5a73e441b2b962fbaab072c
    Signed-off-by: William A. Kennington III <wak@google.com>

--- src/ipaddress.hpp
+++ src/ipaddress.hpp
@@ -1,16 +1,10 @@
 #pragma once
 #include "types.hpp"
 
-#include <linux/netlink.h>
-
-#include <cstdint>
-#include <optional>
 #include <sdbusplus/bus.hpp>
 #include <sdbusplus/message/native_types.hpp>
 #include <sdbusplus/server/object.hpp>
-#include <string>
 #include <string_view>
-#include <vector>
 #include <xyz/openbmc_project/Network/IP/server.hpp>
 #include <xyz/openbmc_project/Object/Delete/server.hpp>
 
@@ -26,30 +20,6 @@ using IPIfaces = sdbusplus::server::object_t<
 using IP = sdbusplus::xyz::openbmc_project::Network::server::IP;
 
 class EthernetInterface;
-
-/* @class AddressFilter
- */
-struct AddressFilter
-{
-    unsigned interface = 0;
-    std::optional<uint8_t> scope;
-};
-
-/** @class AddressInfo
- *  @brief Information about a addresses from the kernel
- */
-struct AddressInfo
-{
-    unsigned interface;
-    InAddrAny address;
-    uint8_t prefix;
-    uint8_t scope;
-    uint32_t flags;
-};
-
-/** @brief Returns a list of the current system neighbor table
- */
-std::vector<AddressInfo> getCurrentAddresses(const AddressFilter& filter);
 
 /** @class IPAddress
  *  @brief OpenBMC IPAddress implementation.
@@ -109,12 +79,5 @@ class IPAddress : public IPIfaces
               EthernetInterface& parent, IfAddr addr, IP::AddressOrigin origin);
 };
 
-namespace detail
-{
-
-void parseAddress(const AddressFilter& filter, const nlmsghdr& hdr,
-                  std::string_view msg, std::vector<AddressInfo>& addresses);
-
-} // namespace detail
 } // namespace network
 } // namespace phosphor
