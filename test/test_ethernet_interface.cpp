de1f8d5c2ee6816911329277450e849ecabae53a
Ravi Teja <raviteja28031990@gmail.com>
2023-03-22 11:39:31 -0500

    Add standalone DHCPv6 configuration support
    Currently dhcpv6 IP gets assigned only when there is a RA router
    attached in the network.
    
    This commit Configures "WithoutRA=solicit" option when dhcpv6 enabled,
    so dhcpv6 client sends dhcpv6 solicit packets even when there is no RA
    router in the network.
    
    Tested By: Enable DHCPv6, Disable DHCPv6 & configure v6stateless
    
    Change-Id: I3b1ce4cef1e450c154909d440ea986d8715e14bc
    Signed-off-by: Ravi Teja <raviteja28031990@gmail.com>

--- test/test_ethernet_interface.cpp
+++ test/test_ethernet_interface.cpp
@@ -203,10 +203,10 @@ TEST_F(TestEthernetInterface, DHCPEnabled)
     set_test(DHCPConf::v4, /*dhcp4=*/true, /*dhcp6=*/false, /*ra=*/false);
     set_test(DHCPConf::v6stateless, /*dhcp4=*/false, /*dhcp6=*/false,
              /*ra=*/true);
-    set_test(DHCPConf::v6, /*dhcp4=*/false, /*dhcp6=*/true, /*ra=*/true);
+    set_test(DHCPConf::v6, /*dhcp4=*/false, /*dhcp6=*/true, /*ra=*/false);
     set_test(DHCPConf::v4v6stateless, /*dhcp4=*/true, /*dhcp6=*/false,
              /*ra=*/true);
-    set_test(DHCPConf::both, /*dhcp4=*/true, /*dhcp6=*/true, /*ra=*/true);
+    set_test(DHCPConf::both, /*dhcp4=*/true, /*dhcp6=*/true, /*ra=*/false);
 
     auto ind_test = [&](DHCPConf conf, bool dhcp4, bool dhcp6, bool ra) {
         EXPECT_EQ(dhcp4, interface.dhcp4(dhcp4));
@@ -219,12 +219,11 @@ TEST_F(TestEthernetInterface, DHCPEnabled)
     ind_test(DHCPConf::v6stateless, /*dhcp4=*/false, /*dhcp6=*/false,
              /*ra=*/true);
     ind_test(DHCPConf::v6, /*dhcp4=*/false, /*dhcp6=*/true, /*ra=*/false);
-    set_test(DHCPConf::v6, /*dhcp4=*/false, /*dhcp6=*/true, /*ra=*/true);
+    set_test(DHCPConf::v6, /*dhcp4=*/false, /*dhcp6=*/true, /*ra=*/false);
     ind_test(DHCPConf::v4v6stateless, /*dhcp4=*/true, /*dhcp6=*/false,
              /*ra=*/true);
     ind_test(DHCPConf::both, /*dhcp4=*/true, /*dhcp6=*/true, /*ra=*/false);
-    set_test(DHCPConf::both, /*dhcp4=*/true, /*dhcp6=*/true, /*ra=*/true);
+    set_test(DHCPConf::both, /*dhcp4=*/true, /*dhcp6=*/true, /*ra=*/false);
 }
-
 } // namespace network
 } // namespace phosphor
