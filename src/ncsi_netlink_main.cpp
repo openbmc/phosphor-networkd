adfac307f512b404afcb81c5d8292aa80b9326df
Jagpal Singh Gill <paligill@gmail.com>
2023-04-18 11:28:03 -0700

    logging: switch ncsi-netlink to lg2
    Tested: Build passes with "ninja -C build".
    Change-Id: Iac053348b1125d15bcf65e27487c6814fa85650a
    Signed-off-by: Jagpal Singh Gill <paligill@gmail.com>

--- src/ncsi_netlink_main.cpp
+++ src/ncsi_netlink_main.cpp
@@ -16,14 +16,15 @@
 #include "argument.hpp"
 #include "ncsi_util.hpp"
 
-#include <iostream>
+#include <phosphor-logging/lg2.hpp>
 #include <string>
 #include <vector>
 
 static void exitWithError(const char* err, char** argv)
 {
+    PHOSPHOR_LOG2_USING_WITH_FLAGS;
     phosphor::network::ncsi::ArgumentParser::usage(argv);
-    std::cerr << "ERROR: " << err << "\n";
+    error("ERROR: {ERROR}", "ERROR", err);
     exit(EXIT_FAILURE);
 }
 
