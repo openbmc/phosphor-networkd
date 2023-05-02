c5e728a914f1161db9d56bd8b17a2a1da250dfb0
Ravi Teja <raviteja28031990@gmail.com>
2023-05-02 10:01:53 -0500

    Add Network Static Route support
    This commit enables static route configuration on EthernetInterface
    Implements CreateStaticRoute method which creates a new d-bus object
    with StaticRoute interface.
    
    Tested By:
    Run StaticRoute D-bus method and verified D-bus object and configuration
    Delete StaticRoute object
    
    Change-Id: I3fbc6f85ede00b6c1949a0ac85f501037a69c831
    Signed-off-by: Ravi Teja <raviteja28031990@gmail.com>

--- src/ethernet_interface.hpp
+++ src/ethernet_interface.hpp
@@ -1,9 +1,11 @@
 #pragma once
 #include "ipaddress.hpp"
 #include "neighbor.hpp"
+#include "static_route.hpp"
 #include "types.hpp"
 #include "xyz/openbmc_project/Network/IP/Create/server.hpp"
 #include "xyz/openbmc_project/Network/Neighbor/CreateStatic/server.hpp"
+#include "xyz/openbmc_project/Network/StaticRoute/Create/server.hpp"
 
 #include <optional>
 #include <sdbusplus/bus.hpp>
@@ -15,6 +17,7 @@
 #include <xyz/openbmc_project/Collection/DeleteAll/server.hpp>
 #include <xyz/openbmc_project/Network/EthernetInterface/server.hpp>
 #include <xyz/openbmc_project/Network/MACAddress/server.hpp>
+#include <xyz/openbmc_project/Network/StaticRoute/server.hpp>
 #include <xyz/openbmc_project/Network/VLAN/server.hpp>
 #include <xyz/openbmc_project/Object/Delete/server.hpp>
 
@@ -28,6 +31,7 @@ using Ifaces = sdbusplus::server::object_t<
     sdbusplus::xyz::openbmc_project::Network::server::MACAddress,
     sdbusplus::xyz::openbmc_project::Network::IP::server::Create,
     sdbusplus::xyz::openbmc_project::Network::Neighbor::server::CreateStatic,
+    sdbusplus::xyz::openbmc_project::Network::StaticRoute::server::Create,
     sdbusplus::xyz::openbmc_project::Collection::server::DeleteAll>;
 
 using VlanIfaces = sdbusplus::server::object_t<
@@ -42,6 +46,8 @@ using EthernetInterfaceIntf =
     sdbusplus::xyz::openbmc_project::Network::server::EthernetInterface;
 using MacAddressIntf =
     sdbusplus::xyz::openbmc_project::Network::server::MACAddress;
+using StaticRouteIntf =
+    sdbusplus::xyz::openbmc_project::Network::server::StaticRoute;
 
 using ServerList = std::vector<std::string>;
 using ObjectPath = sdbusplus::message::object_path;
@@ -90,8 +96,12 @@ class EthernetInterface : public Ifaces
     /** @brief Persistent map of Neighbor dbus objects and their names */
     std::unordered_map<InAddrAny, std::unique_ptr<Neighbor>> staticNeighbors;
 
+    /** @brief Persistent map of static route dbus objects and their names */
+    std::unordered_map<std::string, std::unique_ptr<StaticRoute>> staticRoutes;
+
     void addAddr(const AddressInfo& info);
     void addStaticNeigh(const NeighborInfo& info);
+    void addStaticRoute(const StaticRouteInfo& info);
 
     /** @brief Updates the interface information based on new InterfaceInfo */
     void updateInfo(const InterfaceInfo& info, bool skipSignal = false);
@@ -118,6 +128,15 @@ class EthernetInterface : public Ifaces
      *  @param[in] macAddress - Low level MAC address.
      */
     ObjectPath neighbor(std::string ipAddress, std::string macAddress) override;
+
+    /** @brief Function to create static route dbus object.
+     *  @param[in] destination - Destination IP address.
+     *  @param[in] gateway - Gateway
+     *  @parma[in] prefixLength - Number of network bits.
+     */
+    ObjectPath staticRoute(std::string destination, std::string gateway,
+                           size_t prefixLength,
+                           IP::Protocol protocolType) override;
 
     /** Set value of DHCPEnabled */
     DHCPConf dhcpEnabled() const override;
