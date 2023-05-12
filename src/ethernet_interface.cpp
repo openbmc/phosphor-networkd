f80f8dfee58cc50abc9fc92ae9b4ba77f80e79b2
nitinkotania <gitnkotania@gmail.com>
2023-05-12 06:49:59 -0500

    meta-ibm: Enable Stateful DHCPv6 at factory reset
    Enable Stateful DHCPv6 at factory reset configuration.
    Added WithoutRA flag in 60-phosphor-networkd-default.network.in
    to enable this by default.
    
    A RA message(which has managed flag) is required to start the DHCPv6
    client.
    Now, after this change, the DHCPv6 client starts without RA message.
    There will not be any dependence on RA message to start the DHCPv6
    client.
    
    Tested By: After Reset BMC and server settings,
    60-phosphor-networkd-default.network file's output.
    
    [Match]
    Name=*
    Name=!lo
    [DHCPv6]
    WithoutRA=solicit         -->new entry
    [Network]
    DHCP=true
    LinkLocalAddressing=True
    IPv6AcceptRA=False
    [DHCP]
    ClientIdentifier=mac
    UseDNS=true
    UseDomains=true
    UseNTP=true
    UseHostname=true
    SendHostname=true
    [IPv6AcceptRA]
    DHCPv6Client=true
    
    Change-Id: I3f1414fff838182f55366bea26b1127e3aeeafe9
    Signed-off-by: Nitin Kumar Kotania <gitnkotania@gmail.com>

--- src/ethernet_interface.cpp
+++ src/ethernet_interface.cpp
@@ -651,6 +651,11 @@ void EthernetInterface::writeConfigurationFile()
         lla.emplace_back("no");
 #endif
         network["IPv6AcceptRA"].emplace_back(ipv6AcceptRA() ? "true" : "false");
+        if (dhcp6() && ("solicit" == std::string(ENABLE_DHCP6_WITHOUT_RA)))
+        {
+            config.map["DHCPv6"].emplace_back()["WithoutRA"].emplace_back(
+                "solicit");
+        }
         network["DHCP"].emplace_back(dhcp4() ? (dhcp6() ? "true" : "ipv4")
                                              : (dhcp6() ? "ipv6" : "false"));
         {
