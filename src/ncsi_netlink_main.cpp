f0c9af2a89f45ab72f307925aa557e0659ed89a6
Ksharanabasappa <ksharanabasappa@meta.com>
2024-07-17 05:05:17 -0700

    Implement ncsi-netlink show statistics
    This change implements the command to show statistics.
    
    Tested:
    NIC statistics:
    Response: 0
    Reason: 0
    Counters cleared last read (MSB): 0
    Counters cleared last read (LSB): 0
    Total Bytes Received: 1637142486
    Total Bytes Transmitted: 447852509
    Total Unicast Packet Received: 2428913
    Total Multicast Packet Received: 3343291
    Total Broadcast Packet Received: 297655
    Total Unicast Packet Transmitted: 2219338
    Total Multicast Packet Transmitted: 30285
    Total Broadcast Packet Transmitted: 53929
    FCS Receive Errors: 0
    Alignment Errors: 1
    False Carrier Detections: 0
    Runt Packets Received: 0
    Jabber Packets Received: 0
    Pause XON Frames Received: 0
    Pause XOFF Frames Received: 0
    Pause XON Frames Transmitted: 0
    Pause XOFF Frames Transmitted: 0
    Single Collision Transmit Frames: 0
    Multiple Collision Transmit Frames: 0
    Late Collision Frames: 0
    Excessive Collision Frames: 0
    Control Frames Received: 0
    64-Byte Frames Received: 822664
    65-127 Byte Frames Received: 2767348
    128-255 Byte Frames Received: 1257353
    256-511 Byte Frames Received: 612639
    512-1023 Byte Frames Received: 3967
    1024-1522 Byte Frames Received: 0
    1523-9022 Byte Frames Received: 0
    64-Byte Frames Transmitted: 140599
    65-127 Byte Frames Transmitted: 1622988
    128-255 Byte Frames Transmitted: 245771
    256-511 Byte Frames Transmitted: 3196
    512-1023 Byte Frames Transmitted: 368790
    1024-1522 Byte Frames Transmitted: 3700
    1523-9022 Byte Frames Transmitted: 0
    Valid Bytes Received: 0
    Error Runt Packets Received: 0
    Error Jabber Packets Received: 0
    
    Change-Id: I318d397bd435c62aa2b2b0a86b013c2ec862ca1f
    Signed-off-by: Sharanabasappa K  <ksharanabasappa@meta.com>

--- src/ncsi_netlink_main.cpp
+++ src/ncsi_netlink_main.cpp
@@ -157,6 +157,10 @@ int main(int argc, char** argv)
     {
         return ncsi::clearInterface(indexInt);
     }
+    else if ((options)["stats"] == "true")
+    {
+        return ncsi::getStats(indexInt, packageInt);
+    }
     else
     {
         exitWithError("No Command specified", argv);
--- src/ncsi_util.hpp
+++ src/ncsi_util.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <cstdint>
 #include <span>
 
 namespace phosphor
@@ -9,8 +10,76 @@ namespace network
 namespace ncsi
 {
 
+// NCSI PACKET TYPE
+// Control packet type for Get statistics
+static constexpr auto ncsiCmdGetStatistics = 0x18;
 constexpr auto DEFAULT_VALUE = -1;
 constexpr auto NONE = 0;
+
+namespace internal
+{
+
+struct NCSIPacketHeader
+{
+    uint8_t MCID;
+    uint8_t revision;
+    uint8_t reserved;
+    uint8_t id;
+    uint8_t type;
+    uint8_t channel;
+    uint16_t length;
+    uint32_t rsvd[2];
+};
+
+struct NCSIControllerPacketStatsResponse
+{
+    NCSIPacketHeader header;
+    uint16_t response;
+    uint16_t reason;
+    uint32_t countersClearedFromLastReadMSB;
+    uint32_t countersClearedFromLastReadLSB;
+    uint64_t totalBytesRcvd;
+    uint64_t totalBytesTx;
+    uint64_t totalUnicastPktsRcvd;
+    uint64_t totalMulticastPktsRcvd;
+    uint64_t totalBroadcastPktsRcvd;
+    uint64_t totalUnicastPktsTx;
+    uint64_t totalMulticastPktsTx;
+    uint64_t totalBroadcastPktsTx;
+    uint32_t fcsReceiveErrs;
+    uint32_t alignmentErrs;
+    uint32_t falseCarrierDetections;
+    uint32_t runtPktsRcvd;
+    uint32_t jabberPktsRcvd;
+    uint32_t pauseXOnFramesRcvd;
+    uint32_t pauseXOffFramesRcvd;
+    uint32_t pauseXOnFramesTx;
+    uint32_t pauseXOffFramesTx;
+    uint32_t singleCollisionTxFrames;
+    uint32_t multipleCollisionTxFrames;
+    uint32_t lateCollisionFrames;
+    uint32_t excessiveCollisionFrames;
+    uint32_t controlFramesRcvd;
+    uint32_t rxFrame_64;
+    uint32_t rxFrame_65_127;
+    uint32_t rxFrame_128_255;
+    uint32_t rxFrame_256_511;
+    uint32_t rxFrame_512_1023;
+    uint32_t rxFrame_1024_1522;
+    uint32_t rxFrame_1523_9022;
+    uint32_t txFrame_64;
+    uint32_t txFrame_65_127;
+    uint32_t txFrame_128_255;
+    uint32_t txFrame_256_511;
+    uint32_t txFrame_512_1023;
+    uint32_t txFrame_1024_1522;
+    uint32_t txFrame_1523_9022;
+    uint64_t validBytesRcvd;
+    uint32_t errRuntPacketsRcvd;
+    uint32_t errJabberPacketsRcvd;
+} __attribute__((packed)); // DSP0222 NCSI Spec 8.4.50
+
+} // namespace internal
 
 /* @brief  This function will ask underlying NCSI driver
  *         to send an OEM command (command type 0x50) with
@@ -57,6 +126,14 @@ int clearInterface(int ifindex);
  * @returns 0 on success and negative value for failure.
  */
 int getInfo(int ifindex, int package);
+
+/* @brief  This function is used to get NCSI controller
+           packet stats.
+ * @param[in] ifindex - Interface Index.
+ * @param[in] package - NCSI Package.
+ * @returns 0 on success and negative value for failure.
+ */
+int getStats(int ifindex, int package);
 
 } // namespace ncsi
 } // namespace network
