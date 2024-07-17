0a178433596e9e82a6454b1c8cf4fc4dd0ce9f7f
Ksharanabasappa <ksharanabasappa@meta.com>
2024-07-17 05:05:17 -0700

    Make ncsi-netlink show statistics
    Change-Id: I318d397bd435c62aa2b2b0a86b013c2ec862ca1f
    Signed-off-by: Sharanabasappa K  <ksharanabasappa@meta.com>

--- src/argument.cpp
+++ src/argument.cpp
@@ -76,6 +76,7 @@ void ArgumentParser::usage(char** argv)
            "    --set   | -s      Set a specific package/channel.\n"
            "    --clear | -r      Clear all the settings on the interface.\n"
            "    --oem-payload=<hex data...> | -o <hex data...> Send an OEM command with payload.\n"
+           "    --show-stats      Show controller packet statistics.\n"
            "\n"
            "Example commands:\n"
            "    1) Retrieve topology information:\n"
@@ -92,6 +93,7 @@ void ArgumentParser::usage(char** argv)
 }
 
 const option ArgumentParser::options[] = {
+    {"show-stats", no_argument, NULL,'S'},
     {"info", no_argument, NULL, 'i'},
     {"set", no_argument, NULL, 's'},
     {"clear", no_argument, NULL, 'r'},
@@ -103,7 +105,7 @@ const option ArgumentParser::options[] = {
     {0, 0, 0, 0},
 };
 
-const char* ArgumentParser::optionStr = "irsx:o:p:c:h?";
+const char* ArgumentParser::optionStr = "Sirsx:o:p:c:h?";
 
 const std::string ArgumentParser::trueString = "true";
 const std::string ArgumentParser::emptyString = "";
--- src/ncsi_util.hpp
+++ src/ncsi_util.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <span>
+#include <cstdint>
 
 namespace phosphor
 {
@@ -9,8 +10,59 @@ namespace network
 namespace ncsi
 {
 
+#define   NCSI_GET_CONTROLLER_PACKET_STATISTICS 0x18
+
+typedef struct {
+  uint32_t counters_cleared_from_last_read_MSB;
+  uint32_t counters_cleared_from_last_read_LSB;
+  uint64_t total_bytes_rcvd;
+  uint64_t total_bytes_tx;
+  uint64_t total_unicast_pkts_rcvd;
+  uint64_t total_multicast_pkts_rcvd;
+  uint64_t total_broadcast_pkts_rcvd;
+  uint64_t total_unicast_pkts_tx;
+  uint64_t total_multicast_pkts_tx;
+  uint64_t total_broadcast_pkts_tx;
+  uint32_t fcs_receive_errs;
+  uint32_t alignment_errs;
+  uint32_t false_carrier_detections;
+  uint32_t runt_pkts_rcvd;
+  uint32_t jabber_pkts_rcvd;
+  uint32_t pause_xon_frames_rcvd;
+  uint32_t pause_xoff_frames_rcvd;
+  uint32_t pause_xon_frames_tx;
+  uint32_t pause_xoff_frames_tx;
+  uint32_t single_collision_tx_frames;
+  uint32_t multiple_collision_tx_frames;
+  uint32_t late_collision_frames;
+  uint32_t excessive_collision_frames;
+  uint32_t control_frames_rcvd;
+  uint32_t rx_frame_64;
+  uint32_t rx_frame_65_127;
+  uint32_t rx_frame_128_255;
+  uint32_t rx_frame_256_511;
+  uint32_t rx_frame_512_1023;
+  uint32_t rx_frame_1024_1522;
+  uint32_t rx_frame_1523_9022;
+  uint32_t tx_frame_64;
+  uint32_t tx_frame_65_127;
+  uint32_t tx_frame_128_255;
+  uint32_t tx_frame_256_511;
+  uint32_t tx_frame_512_1023;
+  uint32_t tx_frame_1024_1522;
+  uint32_t tx_frame_1523_9022;
+  uint64_t valid_bytes_rcvd;
+  uint32_t err_runt_packets_rcvd;
+  uint32_t err_jabber_packets_rcvd;
+} __attribute__((packed)) NCSI_Controller_Packet_Stats_Response;
+
 constexpr auto DEFAULT_VALUE = -1;
 constexpr auto NONE = 0;
+
+
+
+
+
 
 /* @brief  This function will ask underlying NCSI driver
  *         to send an OEM command (command type 0x50) with
@@ -57,6 +109,21 @@ int clearInterface(int ifindex);
  * @returns 0 on success and negative value for failure.
  */
 int getInfo(int ifindex, int package);
+
+/* @brief  This function is used to get NCSI controller 
+           packet stats.
+ * @param[in] ifindex - Interface Index.
+ * @param[in] package - NCSI Package.
+ * @returns 0 on success and negative value for failure.
+ */
+int getStats(int ifindex, int package);
+
+/* @brief  This function is used to print the NCSI
+ *         controller packet stats.
+ * @param[in] rcv_buf - NCSI response buffer.
+ * @returns void.
+ */
+void print_ncsi_controller_stats(unsigned char *rcv_buf);
 
 } // namespace ncsi
 } // namespace network
