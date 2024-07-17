0b11525c977939663143a39c7ca6c619dc492cc6
Ksharanabasappa <ksharanabasappa@meta.com>
2024-07-17 05:05:17 -0700

    Implement ncsi-netlink show statistics.
    This change implements the command to show statistics.
    
    Tested:
    NC-SI Command Response:
    Response: 0x0000 Reason: 0x0000
    NIC statistics
    Counters cleared last read (MSB): 0
    Counters cleared last read (LSB): 0
    Total Bytes Received: 615195748
    Total Bytes Transmitted: 103060901
    Total Unicast Packet Received: 705349
    Total Multicast Packet Received: 851549
    Total Broadcast Packet Received: 101032
    Total Unicast Packet Transmitted: 517514
    Total Multicast Packet Transmitted: 7292
    Total Broadcast Packet Transmitted: 158
    FCS Receive Errors: 0
    Alignment Errors: 0
    False Carrier Detections: 0
    Runt Packets Received: 0
    Jabber Packets Received: 0
    Pause XON Frames Received: 0
    Pause XOFF Frames Received: 0
    Pause XON Frames Transmitted: 0
    Pause XOFF Frames Transmitted: 0
    Single Collision Transmit Frames: 0
    Multiple Collision Transmit Frames: 0
    Late Collision Frames: 0
    Excessive Collision Frames: 0
    Control Frames Received: 0
    64-Byte Frames Received: 224388
    65-127 Byte Frames Received: 667185
    128-255 Byte Frames Received: 331530
    256-511 Byte Frames Received: 146160
    512-1023 Byte Frames Received: 822
    1024-1522 Byte Frames Received: 0
    1523-9022 Byte Frames Received: 0
    64-Byte Frames Transmitted: 4322
    65-127 Byte Frames Transmitted: 376235
    128-255 Byte Frames Transmitted: 56942
    256-511 Byte Frames Transmitted: 589
    512-1023 Byte Frames Transmitted: 86992
    1024-1522 Byte Frames Transmitted: 766
    1523-9022 Byte Frames Transmitted: 0
    Valid Bytes Received: 0
    Error Runt Packets Received: 0
    Error Jabber Packets Received: 0
    
    Change-Id: I318d397bd435c62aa2b2b0a86b013c2ec862ca1f
    Signed-off-by: Sharanabasappa K  <ksharanabasappa@meta.com>

--- src/ncsi_netlink_main.cpp
+++ src/ncsi_netlink_main.cpp
@@ -21,145 +21,112 @@
 #include <string>
 #include <vector>
 
-static void exitWithError(const char* err, char** argv)
-{
-    phosphor::network::ncsi::ArgumentParser::usage(argv);
-    lg2::error("ERROR: {ERROR}", "ERROR", err);
-    exit(EXIT_FAILURE);
+static void exitWithError(const char *err, char **argv) {
+  phosphor::network::ncsi::ArgumentParser::usage(argv);
+  lg2::error("ERROR: {ERROR}", "ERROR", err);
+  exit(EXIT_FAILURE);
 }
 
-int main(int argc, char** argv)
-{
-    using namespace phosphor::network;
-    using namespace phosphor::network::ncsi;
-    // Read arguments.
-    auto options = ArgumentParser(argc, argv);
-    int packageInt{};
-    int channelInt{};
-    int indexInt{};
-    int operationInt{DEFAULT_VALUE};
+int main(int argc, char **argv) {
+  using namespace phosphor::network;
+  using namespace phosphor::network::ncsi;
+  // Read arguments.
+  auto options = ArgumentParser(argc, argv);
+  int packageInt{};
+  int channelInt{};
+  int indexInt{};
+  int operationInt{DEFAULT_VALUE};
 
-    // Parse out interface argument.
-    auto ifIndex = (options)["index"];
-    try
-    {
-        indexInt = stoi(ifIndex, nullptr);
-    }
-    catch (const std::exception& e)
-    {
-        exitWithError("Interface not specified.", argv);
+  // Parse out interface argument.
+  auto ifIndex = (options)["index"];
+  try {
+    indexInt = stoi(ifIndex, nullptr);
+  } catch (const std::exception &e) {
+    exitWithError("Interface not specified.", argv);
+  }
+
+  if (indexInt < 0) {
+    exitWithError("Interface value should be greater than equal to 0", argv);
+  }
+
+  // Parse out package argument.
+  auto package = (options)["package"];
+  try {
+    packageInt = stoi(package, nullptr);
+  } catch (const std::exception &e) {
+    packageInt = DEFAULT_VALUE;
+  }
+
+  if (packageInt < 0) {
+    packageInt = DEFAULT_VALUE;
+  }
+
+  // Parse out channel argument.
+  auto channel = (options)["channel"];
+  try {
+    channelInt = stoi(channel, nullptr);
+  } catch (const std::exception &e) {
+    channelInt = DEFAULT_VALUE;
+  }
+
+  if (channelInt < 0) {
+    channelInt = DEFAULT_VALUE;
+  }
+
+  auto payloadStr = (options)["oem-payload"];
+  if (!payloadStr.empty()) {
+    std::string byte(2, '\0');
+    std::vector<unsigned char> payload;
+
+    if (payloadStr.size() % 2)
+      exitWithError("Payload invalid: specify two hex digits per byte.", argv);
+
+    // Parse the payload string (e.g. "50000001572100") to byte data
+    // The first two characters (i.e. "50") represent the Send Cmd Operation
+    // All remaining pairs, interpreted in hex radix, represent the command
+    // payload
+    int sendCmdSelect{};
+    for (unsigned int i = 1; i < payloadStr.size(); i += 2) {
+      byte[0] = payloadStr[i - 1];
+      byte[1] = payloadStr[i];
+
+      try {
+        sendCmdSelect = stoi(byte, nullptr, 16);
+      } catch (const std::exception &e) {
+        exitWithError("Payload invalid.", argv);
+      }
+      if (i == 1) {
+        operationInt = sendCmdSelect;
+      } else {
+        payload.push_back(sendCmdSelect);
+      }
     }
 
-    if (indexInt < 0)
-    {
-        exitWithError("Interface value should be greater than equal to 0",
-                      argv);
+    if (operationInt == DEFAULT_VALUE) {
+      exitWithError("No payload specified.", argv);
     }
 
-    // Parse out package argument.
-    auto package = (options)["package"];
-    try
-    {
-        packageInt = stoi(package, nullptr);
-    }
-    catch (const std::exception& e)
-    {
-        packageInt = DEFAULT_VALUE;
+    if (packageInt == DEFAULT_VALUE) {
+      exitWithError("Package not specified.", argv);
     }
 
-    if (packageInt < 0)
-    {
-        packageInt = DEFAULT_VALUE;
+    return ncsi::sendOemCommand(
+        indexInt, packageInt, channelInt, operationInt,
+        std::span<const unsigned char>(payload.begin(), payload.end()));
+  } else if ((options)["set"] == "true") {
+    // Can not perform set operation without package.
+    if (packageInt == DEFAULT_VALUE) {
+      exitWithError("Package not specified.", argv);
     }
-
-    // Parse out channel argument.
-    auto channel = (options)["channel"];
-    try
-    {
-        channelInt = stoi(channel, nullptr);
-    }
-    catch (const std::exception& e)
-    {
-        channelInt = DEFAULT_VALUE;
-    }
-
-    if (channelInt < 0)
-    {
-        channelInt = DEFAULT_VALUE;
-    }
-
-    auto payloadStr = (options)["oem-payload"];
-    if (!payloadStr.empty())
-    {
-        std::string byte(2, '\0');
-        std::vector<unsigned char> payload;
-
-        if (payloadStr.size() % 2)
-            exitWithError("Payload invalid: specify two hex digits per byte.",
-                          argv);
-
-        // Parse the payload string (e.g. "50000001572100") to byte data
-        // The first two characters (i.e. "50") represent the Send Cmd Operation
-        // All remaining pairs, interpreted in hex radix, represent the command
-        // payload
-        int sendCmdSelect{};
-        for (unsigned int i = 1; i < payloadStr.size(); i += 2)
-        {
-            byte[0] = payloadStr[i - 1];
-            byte[1] = payloadStr[i];
-
-            try
-            {
-                sendCmdSelect = stoi(byte, nullptr, 16);
-            }
-            catch (const std::exception& e)
-            {
-                exitWithError("Payload invalid.", argv);
-            }
-            if (i == 1)
-            {
-                operationInt = sendCmdSelect;
-            }
-            else
-            {
-                payload.push_back(sendCmdSelect);
-            }
-        }
-
-        if (operationInt == DEFAULT_VALUE)
-        {
-            exitWithError("No payload specified.", argv);
-        }
-
-        if (packageInt == DEFAULT_VALUE)
-        {
-            exitWithError("Package not specified.", argv);
-        }
-
-        return ncsi::sendOemCommand(
-            indexInt, packageInt, channelInt, operationInt,
-            std::span<const unsigned char>(payload.begin(), payload.end()));
-    }
-    else if ((options)["set"] == "true")
-    {
-        // Can not perform set operation without package.
-        if (packageInt == DEFAULT_VALUE)
-        {
-            exitWithError("Package not specified.", argv);
-        }
-        return ncsi::setChannel(indexInt, packageInt, channelInt);
-    }
-    else if ((options)["info"] == "true")
-    {
-        return ncsi::getInfo(indexInt, packageInt);
-    }
-    else if ((options)["clear"] == "true")
-    {
-        return ncsi::clearInterface(indexInt);
-    }
-    else
-    {
-        exitWithError("No Command specified", argv);
-    }
-    return 0;
+    return ncsi::setChannel(indexInt, packageInt, channelInt);
+  } else if ((options)["info"] == "true") {
+    return ncsi::getInfo(indexInt, packageInt);
+  } else if ((options)["clear"] == "true") {
+    return ncsi::clearInterface(indexInt);
+  } else if ((options)["show-stats"] == "true") {
+    return ncsi::getStats(indexInt, packageInt);
+  } else {
+    exitWithError("No Command specified", argv);
+  }
+  return 0;
 }
