ce4f74e42a4d6348fa38ee7508c3387d14ab21c1
Ksharanabasappa <ksharanabasappa@meta.com>
2024-07-17 05:05:17 -0700

    Implement ncsi-netlink show statistics
    This change implements the command to show statistics.
    
    Tested:
    NIC statistics:
    response: 0
    reason: 0
    Counters cleared last read (MSB): 0
    Counters cleared last read (LSB): 0
    Total Bytes Received: 1089026050
    Total Bytes Transmitted: 204247988
    Total Unicast Packet Received: 1277897
    Total Multicast Packet Received: 1529506
    Total Broadcast Packet Received: 139203
    Total Unicast Packet Transmitted: 969006
    Total Multicast Packet Transmitted: 13878
    Total Broadcast Packet Transmitted: 24143
    FCS Receive Errors: 0
    Alignment Errors: 1
    False Carrier Detections: 0
    Runt Packets Received: 0
    Jabber Packets Received: 0
    Pause XON Frames Received: 0
    Pause XOFF Frames Received: 0
    Pause XON Frames Transmitted: 0
    Pause XOFF Frames Transmitted: 0
    Single Collision Transmit Frames: 0
    Multiple Collision Transmit Frames: 0
    Late Collision Frames: 0
    Excessive Collision Frames: 0
    Control Frames Received: 0
    64-Byte Frames Received: 377076
    65-127 Byte Frames Received: 1208719
    128-255 Byte Frames Received: 575749
    256-511 Byte Frames Received: 277936
    512-1023 Byte Frames Received: 2513
    1024-1522 Byte Frames Received: 0
    1523-9022 Byte Frames Received: 0
    64-Byte Frames Transmitted: 107739
    65-127 Byte Frames Transmitted: 689882
    128-255 Byte Frames Transmitted: 119919
    256-511 Byte Frames Transmitted: 1997
    512-1023 Byte Frames Transmitted: 167144
    1024-1522 Byte Frames Transmitted: 1628
    1523-9022 Byte Frames Transmitted: 0
    Valid Bytes Received: 0
    Error Runt Packets Received: 0
    Error Jabber Packets Received: 0
    
    Change-Id: I318d397bd435c62aa2b2b0a86b013c2ec862ca1f
    Signed-off-by: Sharanabasappa K  <ksharanabasappa@meta.com>

--- src/ncsi_util.hpp
+++ src/ncsi_util.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <cstdint>
 #include <span>
 
 namespace phosphor
@@ -9,8 +10,55 @@ namespace network
 namespace ncsi
 {
 
+#define NCSI_CMD_GET_CONTROLLER_PACKET_STATISTICS 0x18
 constexpr auto DEFAULT_VALUE = -1;
 constexpr auto NONE = 0;
+struct NCSIControllerPacketStatsResponse
+{
+    uint16_t response;
+    uint16_t reason;
+    uint32_t countersClearedFromLastReadMSB;
+    uint32_t countersClearedFromLastReadLSB;
+    uint64_t totalBytesRcvd;
+    uint64_t totalBytesTx;
+    uint64_t totalUnicastPktsRcvd;
+    uint64_t totalMulticastPktsRcvd;
+    uint64_t totalBroadcastPktsRcvd;
+    uint64_t totalUnicastPktsTx;
+    uint64_t totalMulticastPktsTx;
+    uint64_t totalBroadcastPktsTx;
+    uint32_t fcsReceiveErrs;
+    uint32_t alignmentErrs;
+    uint32_t falseCarrierDetections;
+    uint32_t runtPktsRcvd;
+    uint32_t jabberPktsRcvd;
+    uint32_t pauseXOnFramesRcvd;
+    uint32_t pauseXOffFramesRcvd;
+    uint32_t pauseXOnFramesTx;
+    uint32_t pauseXOffFramesTx;
+    uint32_t singleCollisionTxFrames;
+    uint32_t multipleCollisionTxFrames;
+    uint32_t lateCollisionFrames;
+    uint32_t excessiveCollisionFrames;
+    uint32_t controlFramesRcvd;
+    uint32_t rxFrame_64;
+    uint32_t rxFrame_65_127;
+    uint32_t rxFrame_128_255;
+    uint32_t rxFrame_256_511;
+    uint32_t rxFrame_512_1023;
+    uint32_t rxFrame_1024_1522;
+    uint32_t rxFrame_1523_9022;
+    uint32_t txFrame_64;
+    uint32_t txFrame_65_127;
+    uint32_t txFrame_128_255;
+    uint32_t txFrame_256_511;
+    uint32_t txFrame_512_1023;
+    uint32_t txFrame_1024_1522;
+    uint32_t txFrame_1523_9022;
+    uint64_t validBytesRcvd;
+    uint32_t errRuntPacketsRcvd;
+    uint32_t errJabberPacketsRcvd;
+}__attribute__((packed)); // DSP0222 NCSI Spec 8.4.50
 
 /* @brief  This function will ask underlying NCSI driver
  *         to send an OEM command (command type 0x50) with
@@ -57,6 +105,28 @@ int clearInterface(int ifindex);
  * @returns 0 on success and negative value for failure.
  */
 int getInfo(int ifindex, int package);
+
+/* @brief  This function is used to get NCSI controller
+           packet stats.
+ * @param[in] ifindex - Interface Index.
+ * @param[in] package - NCSI Package.
+ * @returns 0 on success and negative value for failure.
+ */
+int getStats(int ifindex, int package);
+
+/* @brief  This function is used to print the NCSI
+ *         controller packet stats.
+ * @param[in] pResp - NCSI response buffer.
+ * @returns void.
+ */
+void printNCSIControllerStats(const NCSIControllerPacketStatsResponse* pResp);
+
+/* @brief  This function is used to converts a 64-bit/32-bit 
+ *         big-endian integer to a host-endian integer.
+ * @param[in] raw_data - NCSI response data.
+ * @returns void.
+ */
+void convertStatsToHostEndianess(unsigned char* raw_data);
 
 } // namespace ncsi
 } // namespace network
