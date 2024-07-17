de03a4e07b75586a81b04fced929ce6a35de6d6f
Ksharanabasappa <ksharanabasappa@meta.com>
2024-07-17 05:05:17 -0700

    Implement ncsi-netlink show statistics
    This change implements the command to show statistics.
    
    Tested:
    NIC statistics:
    Response: 0
    Reason: 0
    Counters cleared last read (MSB): 0
    Counters cleared last read (LSB): 0
    Total Bytes Received: 1637142486
    Total Bytes Transmitted: 447852509
    Total Unicast Packet Received: 2428913
    Total Multicast Packet Received: 3343291
    Total Broadcast Packet Received: 297655
    Total Unicast Packet Transmitted: 2219338
    Total Multicast Packet Transmitted: 30285
    Total Broadcast Packet Transmitted: 53929
    FCS Receive Errors: 0
    Alignment Errors: 1
    False Carrier Detections: 0
    Runt Packets Received: 0
    Jabber Packets Received: 0
    Pause XON Frames Received: 0
    Pause XOFF Frames Received: 0
    Pause XON Frames Transmitted: 0
    Pause XOFF Frames Transmitted: 0
    Single Collision Transmit Frames: 0
    Multiple Collision Transmit Frames: 0
    Late Collision Frames: 0
    Excessive Collision Frames: 0
    Control Frames Received: 0
    64-Byte Frames Received: 822664
    65-127 Byte Frames Received: 2767348
    128-255 Byte Frames Received: 1257353
    256-511 Byte Frames Received: 612639
    512-1023 Byte Frames Received: 3967
    1024-1522 Byte Frames Received: 0
    1523-9022 Byte Frames Received: 0
    64-Byte Frames Transmitted: 140599
    65-127 Byte Frames Transmitted: 1622988
    128-255 Byte Frames Transmitted: 245771
    256-511 Byte Frames Transmitted: 3196
    512-1023 Byte Frames Transmitted: 368790
    1024-1522 Byte Frames Transmitted: 3700
    1523-9022 Byte Frames Transmitted: 0
    Valid Bytes Received: 0
    Error Runt Packets Received: 0
    Error Jabber Packets Received: 0
    
    Change-Id: I318d397bd435c62aa2b2b0a86b013c2ec862ca1f
    Signed-off-by: Sharanabasappa K  <ksharanabasappa@meta.com>

--- src/argument.cpp
+++ src/argument.cpp
@@ -76,6 +76,7 @@ void ArgumentParser::usage(char** argv)
            "    --set   | -s      Set a specific package/channel.\n"
            "    --clear | -r      Clear all the settings on the interface.\n"
            "    --oem-payload=<hex data...> | -o <hex data...> Send an OEM command with payload.\n"
+           "    --stats      Show controller packet statistics.\n"
            "\n"
            "Example commands:\n"
            "    1) Retrieve topology information:\n"
@@ -88,11 +89,14 @@ void ArgumentParser::usage(char** argv)
            "         ncsi-netlink -x 3 -p 0 -r\n"
            "    5) Send NCSI Command\n"
            "         ncsi-netlink -x 3 -p 0 -c 0 -o 50000001572100\n"
+           "    6) Get Statistics\n"
+           "         ncsi-netlink -x 2 -p 0 -c 0 -S\n"
            "\n";
 }
 
 const option ArgumentParser::options[] = {
     {"info", no_argument, NULL, 'i'},
+    {"stats", no_argument, NULL, 'S'},
     {"set", no_argument, NULL, 's'},
     {"clear", no_argument, NULL, 'r'},
     {"oem-payload", required_argument, NULL, 'o'},
@@ -103,7 +107,7 @@ const option ArgumentParser::options[] = {
     {0, 0, 0, 0},
 };
 
-const char* ArgumentParser::optionStr = "irsx:o:p:c:h?";
+const char* ArgumentParser::optionStr = "iSrsx:o:p:c:h?";
 
 const std::string ArgumentParser::trueString = "true";
 const std::string ArgumentParser::emptyString = "";
