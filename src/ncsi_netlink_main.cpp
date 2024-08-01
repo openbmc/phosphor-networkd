bf3e110c04a5630538b747f164cf6053e985a456
bvaisali <bvaisali@meta.com>
2024-07-31 23:01:06 -0700

    Implement AEN Enable (0x08) command in ncsi-netlink
    Updated code to parse control packet type(Enable AEN)
    and added call back to print NCSI response and reason codes.
    
    Tested:
    ncsi-netlink -x 2 -p 0 -c 0 -a
    <7> Get Info , PACKAGE : 0x0, INTERFACE_INDEX: 0x2
    <6> NCSI AEN RESPONSE CODE : 0x0
    <6> NCSI AEN REASON CODE: 0x0
    
    Change-Id: I32a03b31af27883fab480a3eb89888decccb9c1e
    Signed-off-by: bvaisali <bvaisali@meta.com>

--- src/ncsi_netlink_main.cpp
+++ src/ncsi_netlink_main.cpp
@@ -157,6 +157,10 @@ int main(int argc, char** argv)
     {
         return ncsi::clearInterface(indexInt);
     }
+    else if ((options)["AEN"] == "true")
+    {
+        return ncsi::aenEnable(indexInt, packageInt, channelInt);
+    }
     else
     {
         exitWithError("No Command specified", argv);
