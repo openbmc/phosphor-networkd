f69a2be9d97f3840bb400b3614047e7c50fbe271
bvaisali <bvaisali@meta.com>
2024-07-31 23:01:06 -0700

    Implement AEN enable (0x08) command in ncsi-netlink
    Updated code to parse control packet type Enable/Disable AEN
    and added call back to print NCSI response and reason codes.
    
    Tested:
    
    ncsi-netlink -x 2 -p 0 -c 0 -a 0
    <7> AEN Enable, AEN VALUE : 0x0, PACKAGE : 0x0,
    INTERFACE_INDEX: 0x2, CHANNEL : 0x0
    <6> NCSI AEN RESPONSE CODE : 0x0
    <6> NCSI AEN REASON CODE: 0x0
    
    ncsi-netlink -x 2 -p 0 -c 0 --AEN 1
    <7> AEN Enable, AEN VALUE : 0x1, PACKAGE : 0x0,
    INTERFACE_INDEX: 0x2, CHANNEL : 0x0
    <6> NCSI AEN RESPONSE CODE : 0x0
    <6> NCSI AEN REASON CODE: 0x0
    
    Change-Id: I32a03b31af27883fab480a3eb89888decccb9c1e
    Signed-off-by: bvaisali <bvaisali@meta.com>

--- src/ncsi_util.hpp
+++ src/ncsi_util.hpp
@@ -12,6 +12,24 @@ namespace ncsi
 constexpr auto DEFAULT_VALUE = -1;
 constexpr auto NONE = 0;
 
+//AEN Interrupts
+constexpr auto disableAENMask = 0x00000000;
+constexpr auto enableLinkAENMask = 0x00000001;
+constexpr auto enableLinkConfigAENMask = 0x00000010;
+constexpr auto enableLinkNCdriverAENMask = 0x00000011;
+constexpr auto enableConfigAENMask = 0x00000100;
+constexpr auto enableConfigNCdriverAENMask = 0x00000101;
+constexpr auto enableHostNCdriverAENMask = 0x00000110;
+constexpr auto enableAENMask = 0x00000111;
+
+//AEN Response and Reason Codes
+constexpr auto AEN_RESPONSE_CODE = 18;
+constexpr auto AEN_REASON_CODE = 20;
+
+// NCSI PACKET TYPE
+// Control packet type for Asynchronous Event Notification
+constexpr auto NCSI_CMD_AEN_ENABLE = 0x8;
+
 /* @brief  This function will ask underlying NCSI driver
  *         to send an OEM command (command type 0x50) with
  *         the specified payload as the OEM data.
@@ -57,6 +75,18 @@ int clearInterface(int ifindex);
  * @returns 0 on success and negative value for failure.
  */
 int getInfo(int ifindex, int package);
+
+/* @brief  This function will ask underlying NCSI driver
+ *         to enable or disable Asynchronous Event Notification
+ *         (command type 0x8).
+ *         This function talks with the NCSI driver over
+ *         netlink messages.
+ * @param[in] ifindex - Interface Index.
+ * @param[in] package - NCSI Package.
+ * @param[in] channel - Channel number with in the package.
+ * @param[in] AENInt - AEN Interrupt number.
+*/
+int aenEnable(int ifindex, int package, int channel, int AENInt);
 
 } // namespace ncsi
 } // namespace network
