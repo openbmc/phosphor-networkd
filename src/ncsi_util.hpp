789170bb2572a81500b07dfb24cc7609667208fd
jinojgodwin <jinojgodwin@meta.com>
2024-08-20 00:08:04 -0700

    Implement Disable Global Multicast Filter(0x13) command in ncsi-netlink
    This changes implements the command to disable global multicast filtering
    
    Tested:
    Observed the response packet type is 0x93
    ./ncsi-netlink -x 2 -p 0 -c 0 -m
    <7> Send NCSI Command, CHANNEL : 0x0 , PACKAGE : 0x0, INTERFACE_INDEX: 0x2
    <7> NCSI Response packet type : 0x93
    <7> NCSI Response length : 0x4
    
    Change-Id: I7a804e950c9ffccd5416d55972d4d3803fc0d66e
    Signed-off-by: jinojgodwin <jinojgodwin@meta.com>

--- src/ncsi_util.hpp
+++ src/ncsi_util.hpp
@@ -58,6 +58,18 @@ int clearInterface(int ifindex);
  */
 int getInfo(int ifindex, int package);
 
+
+/* @brief  This function will ask underlying NCSI driver
+ *         to disable global multicast filtering for the channel.
+ *         This function talks with the NCSI driver over
+ *         netlink messages.
+ * @param[in] ifindex - Interface Index.
+ * @param[in] package - NCSI Package.
+ * @param[in] channel - Channel number with in the package.
+ * @returns 0 on success and negative value for failure.
+ */
+int disableGlobalMulticastFiltering(int ifindex, int package, int channel);
+
 } // namespace ncsi
 } // namespace network
 } // namespace phosphor
