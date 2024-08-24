c7012f4e817b81acdae64a33997c69904a27b99a
foneshenry <foneshenry@meta.com>
2024-08-23 17:19:58 -0700

    Implementation of set mac address in ncsi_netlink for OpenBMC
    Parent:     e010e562 (build: upgrade to C++23)
    Author:     H Thomas Fones <foneshenry@meta.com>
    AuthorDate: 2024-08-23 14:35:04 -0700
    Commit:     foneshenry <foneshenry@meta.com>
    CommitDate: 2024-08-23 14:24:28 -0700
    
    Enhancements to ncsi-netlink to implement set mac address
    
    Command line:
             ncsi-netlink -x 2 -p 0 -c 0 -F 1 --set-mac=0123456789ab
    
    Summary:
    
    Test Plan:
      ./nsci-netlink --help
      ./nsci-netlink -x 2 -p 0 -c 0 -F 1 --set-mac=ba9876543210
    
    Results:
        ./ncsi-netlink-sm -x 2 -p 0 -c 0 -F 1 --set-mac=0123456789ab
        <7> Set Mac Address:
            INTERFACE_INDEX:2 MAC_ADDRESS:0123456789AB FILTER:1 FLAGS:0x1
        <7> Display Response Data:
            CTL_MSG_TYPE:0x8e RESPONSE_CODE:0x0 REASON:0x0
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    Change-Id: I4e4a73596871d71f4ee7801fe006a0d95053b4fc
    Signed-off-by: Henry Fones <foneshenry@meta.com>

---
+++ src/ncsi_util_setmac.hpp
@@ -0,0 +1,69 @@
+#pragma once
+
+#include "ncsi_util.hpp"
+#include <cstdint>
+
+namespace phosphor
+{
+namespace network
+{
+namespace ncsi
+{
+namespace internal
+{
+struct setMacAddrData
+{
+    uint8_t macAddr[6];
+    uint8_t macAddrNum;
+    uint8_t macAddrFlags;
+    uint32_t checksum;
+    uint8_t padding[18];
+};
+
+struct setMacAddrRespData
+{
+    uint32_t checksum;
+    uint8_t padding[22];
+};
+
+struct setMacAddrResponse
+{
+    NCSIPacketHeader ncsiRespHdr;
+    ncsiCompletionCodes ncsiCCodes;
+    struct setMacAddrRespData macAddrRespData;
+};
+
+struct setMacAddressResponsePacket
+{
+    NCSIPacketHeader header;
+    uint16_t response;
+    uint16_t reason;
+    uint32_t checksum;
+    uint8_t pad[22];
+
+    template <class Archive>
+    void serialize(Archive& ar)
+    {
+        ar(header, response, reason, checksum, pad[0], pad[1], pad[2], pad[3],
+           pad[4], pad[5], pad[6], pad[7], pad[8], pad[9], pad[10], pad[11],
+           pad[12], pad[13], pad[14], pad[15], pad[16], pad[17], pad[18],
+           pad[19], pad[20], pad[21]);
+    }
+};
+
+} // namespace internal
+
+/* @brief  This function sets the mac address for a
+ *         specific interface & channel
+ *         the package.
+ * @param[in] ifindex - Interface Index.
+ * @param[in] channel - channel
+ * @param[in] macAddr - the new mac address
+ * @returns 0 on success and negative value for failure.
+ */
+int setMacAddr(int ifindex, int package, int channel,
+               const std::string& macAddr, const uint8_t& filter,
+               const uint8_t& maFlags);
+} // namespace ncsi
+} // namespace network
+} // namespace phosphor
