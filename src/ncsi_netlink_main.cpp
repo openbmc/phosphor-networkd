65568c31bd148e4a9221b9014f2768146c74f739
Shubham Ashruba Jogdand <ashrubajshubham@fb.com>
2024-08-28 23:39:03 -0700

    Implement Enable Global Multicast Filter(0x12)
    This changes implements the command to
    enable global multicast filtering in ncsi-netlink
    
    ncsi-netlink -x 2 -p 0 -c 0 -e
    <7> Send NCSI Command, CHANNEL : 0x0 , PACKAGE : 0x0, INTERFACE_INDEX: 0x2
    <7> NCSI Response packet type : 0x92
    <7> NCSI Response length : 0x4
    <7> NCSI Response Code : 0x0
    <7> NCSI Reason Code : 0x0
    
    Change-Id: I36dabe217327f9dea36ef3ab6a96d9c3e8fe4031
    Signed-off-by: shubhamjogdand <ashrubajshubham@meta.com>

--- src/ncsi_netlink_main.cpp
+++ src/ncsi_netlink_main.cpp
@@ -197,6 +197,11 @@ int main(int argc, char** argv)
         }
         return ncsi::setChannelMask(indexInt, packageInt, mask);
     }
+    else if ((options)["egmf"] == "true")
+    {
+        return ncsi::enableGlobalMulticastFilter(indexInt, packageInt,
+                                                 channelInt);
+    }
     else
     {
         exitWithError("No Command specified", argv);
