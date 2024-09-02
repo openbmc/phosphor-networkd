9e9cd8c6e92920ea695aa686e858e2c679f4b7a4
Sunder Ganesan <ganesansunder@meta.com>
2024-09-02 00:23:48 -0700

    Implement Get Capabilities(0x16) command in ncsi-netlink
    Updated code to parse control packet type(ex 0x16
    -Get Capabilities) and Added call back to print
    NCSI completion codes and  data for Capabilities command.
    
    Tested:
    Observed  that response packet type  is 0x96 and
    capabilities info received as expected.
    ./ncsi-netlink --index=2 --package=0 --channel=0 -C
    <7> Send NCSI Command, CHANNEL:0x0 , PACKAGE:0x0, INTERFACE_INDEX:0x2
    <7> NCSI Response packet type : 0x96
    <7> NCSI Response length : 0x20
    cmd: GET_CAPABILITIES(0x16)
    NC-SI Command Response:
    Response: COMMAND_COMPLETED (0x0)
    Reason: NO_ERROR (0x0)
    
    Get Capabilities response
      capabilities_flags = 0x12
      broadcast_packet_filter_capabilities = 0x3
      multicast_packet_filter_capabilities = 0x8
      buffering_capabilities = 0x0
      aen_control_support = 0x7
      unicast_filter_cnt = 1
      multicast_filter_cnt = 0
      mixed_filter_cnt = 3
      vlan_filter_cnt = 1
      channel_cnt = 4
      vlan_mode_support = 0
    
    Change-Id: I5531e2527c2eb8cf3acff76740fbf282223b5644
    Signed-off-by: Sunder Ganesan <ganesansunder@meta.com>

--- src/ncsi_netlink_main.cpp
+++ src/ncsi_netlink_main.cpp
@@ -197,6 +197,10 @@ int main(int argc, char** argv)
         }
         return ncsi::setChannelMask(indexInt, packageInt, mask);
     }
+    else if ((options)["capabilities"] == "true")
+    {
+        return ncsi::getCapabilities(indexInt, packageInt, channelInt);
+    }
     else
     {
         exitWithError("No Command specified", argv);
