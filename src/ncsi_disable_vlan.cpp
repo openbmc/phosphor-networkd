152058c0d938dbafb12f36bd1e54e9f789ea9bc4
Ahsan Baig Mirza <baigmirzaahsan@meta.com>
2024-09-04 06:46:39 -0700

    Implement.Disable VLAN(0x0D) NC-SI Command
    Implement the NC-SI command Disable VLAN(0x0D)
    
    Tested:
    ./ncsi-netlink -x 2 0 -p 0 -c 0 -d vlan
    <7> Send NCSI Command, CHANNEL : 0x0 , PACKAGE : 0x0, INTERFACE_INDEX: 0x2, NCSI_CMD : 0xd
    <7> Disable Vlan Response type : 0x8d
    <7> Disable Vlan Response length : 0x4
    <7> Disable Vlan Response : 0x0 Reason : 0x0
    
    Signed-off-by: Ahsan Baig Mirza <baigmirzaahsan@meta.com>
    Change-Id: I91b56c4cafa75f6fd99b6c4f1dd6fc190ec9774b

---
+++ src/ncsi_disable_vlan.cpp
@@ -0,0 +1,77 @@
+
+#include "ncsi_disable_vlan.hpp"
+
+#include <linux/ncsi.h>
+#include <netlink/genl/ctrl.h>
+#include <netlink/genl/genl.h>
+#include <netlink/netlink.h>
+
+#include <cereal/archives/binary.hpp>
+#include <cereal/types/vector.hpp>
+#include <phosphor-logging/lg2.hpp>
+
+#include <sstream>
+#include <vector>
+
+namespace phosphor
+{
+namespace network
+{
+namespace ncsi
+{
+namespace internal
+{
+
+CallBack disableVlanCallBack = [](struct nl_msg* msg, void* arg) {
+    std::span<const unsigned char> payload;
+    auto ret = getNcsiCommandPayload(msg, arg, payload);
+
+    if (!ret)
+    {
+        std::vector<unsigned char> payloadVec(payload.begin(), payload.end());
+        std::istringstream iss(
+            std::string(payloadVec.begin(), payloadVec.end()));
+        cereal::BinaryInputArchive ar(iss);
+        DisableVlanResponsePacket packet;
+        ar(packet);
+
+        auto type = packet.header.type;
+        lg2::debug("Disable Vlan Response type : {RESPONSE_TYPE}",
+                   "RESPONSE_TYPE", lg2::hex, type);
+
+        auto headerLength = htons(packet.header.length);
+        lg2::debug("Disable Vlan Response length : {RESPONSE_LEN}",
+                   "RESPONSE_LEN", lg2::hex, headerLength);
+
+        auto response = ntohs(packet.response);
+        auto reason = ntohs(packet.reason);
+
+        lg2::debug("Disable Vlan Response : {RESPONSE} Reason : {REASON}",
+                   "RESPONSE", lg2::hex, response, "REASON", lg2::hex, reason);
+    }
+
+    return ret;
+};
+
+} // namespace internal
+
+int disableVlan(int ifindex, int package, int channel)
+{
+    constexpr auto ncsiCmdDisableVlan = 0x0D;
+
+    lg2::debug("Send NCSI Command, CHANNEL : {CHANNEL} , PACKAGE : {PACKAGE}, "
+               "INTERFACE_INDEX: {INTERFACE_INDEX}, NCSI_CMD : {NCSI_CMD}",
+               "CHANNEL", lg2::hex, channel, "PACKAGE", lg2::hex, package,
+               "INTERFACE_INDEX", lg2::hex, ifindex, "NCSI_CMD", lg2::hex,
+               ncsiCmdDisableVlan);
+
+    return internal::applyCmd(
+        ifindex,
+        internal::Command(ncsi_nl_commands::NCSI_CMD_SEND_CMD,
+                          ncsiCmdDisableVlan),
+        package, channel, NONE, internal::disableVlanCallBack);
+}
+
+} // namespace ncsi
+} // namespace network
+} // namespace phosphor
