3cfad3e8585b109057d064d85282ac014d62acc9
Ahsan Baig Mirza <baigmirzaahsan@meta.com>
2024-09-04 06:46:39 -0700

    Implement Disable VLAN(0x0D) NC-SI Command
    Provide a way to send Disable VLAN command (0x0D) using ncsi-netlink.
    Added an option --disable with sub-option vlan to send the command.
    
    Tested:
    ./ncsi-netlink -x 2 0 -p 0 -c 0 -d vlan
    <7> Send NCSI Command, CHANNEL : 0x0 , PACKAGE : 0x0, INTERFACE_INDEX: 0x2, NCSI_CMD : 0xd
    <7> Disable Vlan Response type : 0x8d
    <7> Disable Vlan Response length : 0x4
    <7> Disable Vlan Response : 0x0 Reason : 0x0
    
    Signed-off-by: Ahsan Baig Mirza <baigmirzaahsan@meta.com>
    Change-Id: I91b56c4cafa75f6fd99b6c4f1dd6fc190ec9774b

---
+++ src/ncsi_disable_vlan.hpp
@@ -0,0 +1,46 @@
+#pragma once
+
+#include "ncsi_util.hpp"
+
+namespace phosphor
+{
+namespace network
+{
+namespace ncsi
+{
+namespace internal
+{
+
+struct DisableVlanResponsePacket
+{
+    NCSIPacketHeader header;
+    uint16_t response;
+    uint16_t reason;
+    uint32_t checksum;
+    uint8_t pad[22];
+
+    template <class Archive>
+    void serialize(Archive& ar)
+    {
+        ar(header, response, reason, checksum, pad[0], pad[1], pad[2], pad[3],
+           pad[4], pad[5], pad[6], pad[7], pad[8], pad[9], pad[10], pad[11],
+           pad[12], pad[13], pad[14], pad[15], pad[16], pad[17], pad[18],
+           pad[19], pad[20], pad[21]);
+    }
+};
+
+} // namespace internal
+
+/* @brief This function will ask underlying NCSI driver
+ *        to send a Disable VLAN (command type 0x0D)
+ *        This function talks with the NCSI driver over
+ *        netlink messages.
+ * @param[in] ifindex - Interface Index.
+ * @param[in] package - NCSI Package.
+ * @param[in] channel - Channel number with in the package.
+ * @returns 0 on success and negative value for failure.
+ */
+int disableVlan(int ifindex, int package, int channel);
+} // namespace ncsi
+} // namespace network
+} // namespace phosphor
