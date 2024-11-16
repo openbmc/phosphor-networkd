openbmc/phosphor-networkd

commit d4ff731e20735f8b1e97a10d51d30a449b695f3f

ethernet_interface: Refactor gateway setting
Reduce duplication and re-use code.

Change-Id: Id0919c03f439af60f3e4e65860f22e4b61e609a1
Signed-off-by: William A. Kennington III <wak@google.com>

src/ethernet_interface.cpp
@@ -811,63 +811,44 @@ void EthernetInterface::deleteAll()
     manager.get().reloadConfigs();
 }
 
-std::string EthernetInterface::defaultGateway(std::string gateway)
+template <typename Addr>
+static void normalizeGateway(std::string& gw)
 {
+    if (gw.empty())
+    {
+        return;
+    }
     try
     {
-        if (!gateway.empty())
-        {
-            gateway =
-                stdplus::toStr(stdplus::fromStr<stdplus::In4Addr>(gateway));
-        }
+        gw = stdplus::toStr(stdplus::fromStr<Addr>(gw));
     }
     catch (const std::exception& e)
     {
-        lg2::error("Invalid v4 GW {NET_GW}: {ERROR}", "NET_GW", gateway,
-                   "ERROR", e);
+        lg2::error("Invalid GW `{NET_GW}`: {ERROR}", "NET_GW", gw, "ERROR", e);
         elog<InvalidArgument>(Argument::ARGUMENT_NAME("GATEWAY"),
-                              Argument::ARGUMENT_VALUE(gateway.c_str()));
+                              Argument::ARGUMENT_VALUE(gw.c_str()));
     }
+}
 
-    if (EthernetInterfaceIntf::defaultGateway() == gateway)
+std::string EthernetInterface::defaultGateway(std::string gateway)
+{
+    normalizeGateway<stdplus::In4Addr>(gateway);
+    if (gateway != defaultGateway())
     {
-        return gateway;
+        gateway = EthernetInterfaceIntf::defaultGateway(std::move(gateway));
+        manager.get().reloadConfigs();
     }
-    EthernetInterfaceIntf::defaultGateway(gateway);
-
-    writeConfigurationFile();
-    manager.get().reloadConfigs();
-
     return gateway;
 }
 
 std::string EthernetInterface::defaultGateway6(std::string gateway)
 {
-    try
+    normalizeGateway<stdplus::In6Addr>(gateway);
+    if (gateway != defaultGateway6())
     {
-        if (!gateway.empty())
-        {
-            gateway =
-                stdplus::toStr(stdplus::fromStr<stdplus::In6Addr>(gateway));
-        }
+        gateway = EthernetInterfaceIntf::defaultGateway6(std::move(gateway));
+        manager.get().reloadConfigs();
     }
-    catch (const std::exception& e)
-    {
-        lg2::error("Invalid v6 GW {NET_GW}: {ERROR}", "NET_GW", gateway,
-                   "ERROR", e);
-        elog<InvalidArgument>(Argument::ARGUMENT_NAME("GATEWAY"),
-                              Argument::ARGUMENT_VALUE(gateway.c_str()));
-    }
-
-    if (EthernetInterfaceIntf::defaultGateway6() == gateway)
-    {
-        return gateway;
-    }
-    EthernetInterfaceIntf::defaultGateway6(gateway);
-
-    writeConfigurationFile();
-    manager.get().reloadConfigs();
-
     return gateway;
 }
 

commit 7614171d6c4fe834a686b953198e349db42d27a6

Write Gateway to Network section
Currently the Gateway values are written to the Route section.  This
causes a problem for implementations that want to route traffic to the
greater network, because the default route does not get applied, and
therefore the BMC cannot route beyond the USB link.

This patch adds the gateway section.

This looks like a regression caused by
d4ff731e20735f8b1e97a10d51d30a449b695f3f but I'd like some maintainer
input on if this is the right approach, or if the Route section was
added intentionally.

Tested:
```
curl -s -k -X PATCH hmc_0/redfish/v1/Managers/HGX_BMC_0/EthernetInterfaces/usb0  -d '{"IPv4StaticAddresses": [{"Gateway": "172.31.13.241"}]}'
```

Shows the default route is now created:
root@gb200nvl-hmc:~# ip r
default via 172.31.13.241 dev usb0

Change-Id: Ie69ecc39087c2e67525a3df5ec00c97801cf9093
Signed-off-by: Ed Tanous <etanous@nvidia.com>

src/ethernet_interface.cpp
@@ -692,6 +692,8 @@ void EthernetInterface::writeConfigurationFile()
     {
         auto& network = config.map["Network"].emplace_back();
         auto& lla = network["LinkLocalAddressing"];
+        auto gateway4 = EthernetInterfaceIntf::defaultGateway();
+        network["Gateway"].emplace_back(gateway4);
 #ifdef LINK_LOCAL_AUTOCONFIGURATION
         lla.emplace_back("yes");
 #else