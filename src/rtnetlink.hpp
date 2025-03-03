b0de2f049de607186c72b969a47d21de6948e806
Patrick Williams <patrick@stwcx.xyz>
2025-03-03 11:19:22 -0500

    clang-format: update latest spec and reformat
    Copy the latest format file from the docs repository and apply.
    
    Change-Id: I29c7168fd2fe69b1738a026e1d1360a6de7b5f5e
    Signed-off-by: Patrick Williams <patrick@stwcx.xyz>

--- src/rtnetlink.hpp
+++ src/rtnetlink.hpp
@@ -10,8 +10,8 @@ namespace phosphor::network::netlink
 
 InterfaceInfo intfFromRtm(std::string_view msg);
 
-std::optional<std::tuple<unsigned, stdplus::InAnyAddr>>
-    gatewayFromRtm(std::string_view msg);
+std::optional<std::tuple<unsigned, stdplus::InAnyAddr>> gatewayFromRtm(
+    std::string_view msg);
 
 AddressInfo addrFromRtm(std::string_view msg);
 
