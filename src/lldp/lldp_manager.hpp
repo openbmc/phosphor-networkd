59684da51ba09e1678b822ce390c4e922d1871a9
Asmitha Karunanithi <asmitk01@in.ibm.com>
2025-09-12 23:17:33 -0500

    networkd: Add LLDP neighbor discovery
    This commit integrates LLDP (Link Layer Discovery Protocol) support into
    phosphor-lldpd (a new app that is created) using liblldpctl. It creates
    a new systemd service "xyz.openbmc_project.LLDP.service" that enables
    neighbor discovery. It helps parse lldp packets on active interfaces
    and discover the neighbor BMC details, including its IP and MAC address.
    
    For each detected neighbor, a DBus object is created under corresponding
    interfaces that hosts these properties for other apps to use.
    
    [1] Design doc: https://gerrit.openbmc.org/c/openbmc/docs/+/82739
    [2] Dbus Interfaces: https://gerrit.openbmc.org/c/openbmc/phosphor-dbus-interfaces/+/83533
    
    The work is split into multiple commits:
    * Create LLDP service and host the dbus object:
      /xyz/openbmc_project/network/lldp --> This is the current commit
    
    * Create ethernet objects under
      /xyz/openbmc_project/network/lldp/eth[01];
      Implement the interface that hosts "EnableLLDP" on each ethernet
      interface.
    
    * When enabling LLDP, parse the LLDP packets from the socket using
      liblldpctl APIs and update the lldp ethernet objects with the TLVs
      received and parsed.
    
    Change-Id: I29e008345022b901863ee5922a2fc07fdf42e097
    Signed-off-by: Asmitha Karunanithi <asmitk01@in.ibm.com>

---
+++ src/lldp/lldp_manager.hpp
@@ -0,0 +1,26 @@
+#pragma once
+#include <sdbusplus/bus.hpp>
+#include <sdeventplus/event.hpp>
+
+namespace phosphor
+{
+namespace network
+{
+namespace lldp
+{
+
+class LLDPManager
+{
+  public:
+    LLDPManager(sdbusplus::bus_t& bus, sdeventplus::Event& event);
+
+    static constexpr auto objPath = "/xyz/openbmc_project/network/lldp";
+
+  protected:
+    sdbusplus::bus_t& bus;
+    sdeventplus::Event& event;
+};
+
+} // namespace lldp
+} // namespace network
+} // namespace phosphor
