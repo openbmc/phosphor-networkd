2b3d2cb8b0f0c029719c2b931ab6da8876af032e
Asmitha Karunanithi <asmitk01@in.ibm.com>
2025-09-15 13:14:30 -0500

    LLDP: Add ethernet interface dbus objects
    This commit parses the availableinterfaces exposed by networkd and
    create corresponding LLDP DBus objects under
    /xyz/openbmc_project/network/lldp/<interface>.
    
    This ensures LLDP objects are always in sync with the interfaces
    managed by networkd.
    
    Tested By:
    Verified ethernet object creation on bmc under LLDP service:
    '''
    busctl tree xyz.openbmc_project.LLDP
    └─ /xyz
      └─ /xyz/openbmc_project
        └─ /xyz/openbmc_project/network
          └─ /xyz/openbmc_project/network/lldp
            ├─ /xyz/openbmc_project/network/lldp/eth0
            └─ /xyz/openbmc_project/network/lldp/eth1
    '''
    
    Change-Id: Idbd0e2edb1d1079cb5a0ee358bdedef7e9f0a815
    Signed-off-by: Asmitha Karunanithi <asmitk01@in.ibm.com>

---
+++ src/lldp/lldp_interface.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <sdbusplus/server/object.hpp>
+
+#include <string>
+
+namespace phosphor
+{
+namespace network
+{
+namespace lldp
+{
+
+class Manager;
+
+class Interface
+{
+  public:
+    Interface() = delete;
+    Interface(const Interface&) = delete;
+    Interface& operator=(const Interface&) = delete;
+
+    Interface(sdbusplus::bus_t& bus, Manager& manager,
+              const std::string& objPath, const std::string& ifname);
+
+    ~Interface() = default;
+
+  private:
+    Manager& manager;
+    sdbusplus::bus_t& busRef;
+    std::string objPath;
+    std::string ifname;
+};
+} // namespace lldp
+} // namespace network
+} // namespace phosphor
