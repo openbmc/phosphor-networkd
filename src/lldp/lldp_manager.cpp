074806b6d1571ad835dc80c49f31feabd61fc57c
Asmitha Karunanithi <asmitk01@in.ibm.com>
2025-09-15 13:14:30 -0500

    LLDP: Add ethernet interface dbus objects
    This commit parses the availableinterfaces exposed by networkd and
    create corresponding LLDP DBus objects under
    /xyz/openbmc_project/network/lldp/<interface>.
    
    This ensures LLDP objects are always in sync with the interfaces
    managed by networkd.
    
    Tested By:
    Verified ethernet object creation on bmc under LLDP service:
    '''
    busctl tree xyz.openbmc_project.LLDP
    └─ /xyz
      └─ /xyz/openbmc_project
        └─ /xyz/openbmc_project/network
          └─ /xyz/openbmc_project/network/lldp
            ├─ /xyz/openbmc_project/network/lldp/eth0
            └─ /xyz/openbmc_project/network/lldp/eth1
    '''
    
    Change-Id: Idbd0e2edb1d1079cb5a0ee358bdedef7e9f0a815
    Signed-off-by: Asmitha Karunanithi <asmitk01@in.ibm.com>

--- src/lldp/lldp_manager.cpp
+++ src/lldp/lldp_manager.cpp
@@ -1,11 +1,19 @@
 #include "lldp_manager.hpp"
 
+#include "lldp_interface.hpp"
+
 #include <arpa/inet.h>
 #include <lldpctl.h>
 #include <netinet/in.h>
 #include <sys/socket.h>
 
 #include <phosphor-logging/lg2.hpp>
+#include <sdbusplus/exception.hpp>
+#include <sdbusplus/message/types.hpp>
+#include <sdbusplus/server.hpp>
+
+#include <string>
+#include <vector>
 
 namespace phosphor
 {
@@ -14,9 +22,81 @@ namespace network
 namespace lldp
 {
 
+using DBusProp = std::variant<std::string, bool, uint8_t, int16_t, int32_t,
+                              int64_t, uint16_t, uint32_t, uint64_t, double,
+                              std::vector<std::string>>;
+
 LLDPManager::LLDPManager(sdbusplus::bus_t& bus, sdeventplus::Event& event) :
     bus(bus), event(event)
-{}
+{
+    createIntfTimer = std::make_unique<
+        sdeventplus::utility::Timer<sdeventplus::ClockId::Monotonic>>(
+        event,
+        [this](auto& timer) {
+            timer.setEnabled(false);
+            this->createIntfDbusObjects();
+        },
+        std::chrono::seconds(0));
+
+    createIntfTimer->setEnabled(true);
+}
+
+void LLDPManager::createIntfDbusObjects()
+{
+    auto interfaces = getInterfaces();
+    for (const auto& ifname : interfaces)
+    {
+        if (ifaces.contains(ifname))
+        {
+            continue;
+        }
+
+        const std::string path =
+            std::string(objPath) + "/" + ifname;
+
+        ifaces.emplace(ifname,
+            std::make_unique<Interface>(bus, *this, path, ifname));
+
+        lg2::info("Created Interface object for {IF} at {PATH}",
+                  "IF", ifname, "PATH", path);
+    }
+}
+
+std::vector<std::string> LLDPManager::getInterfaces()
+{
+    std::vector<std::string> ifnames;
+
+    try
+    {
+        auto method = bus.new_method_call(
+            "xyz.openbmc_project.Network", "/xyz/openbmc_project/network",
+            "org.freedesktop.DBus.ObjectManager", "GetManagedObjects");
+
+        auto reply = bus.call(method);
+
+        std::map<sdbusplus::message::object_path,
+                 std::map<std::string, std::map<std::string, DBusProp>>>
+            objects;
+
+        reply.read(objects);
+
+        for (const auto& [path, ifacesMap] : objects)
+        {
+            if (ifacesMap.find(
+                    "xyz.openbmc_project.Network.EthernetInterface") !=
+                ifacesMap.end())
+            {
+                ifnames.push_back(path.filename());
+            }
+        }
+    }
+    catch (const std::exception& e)
+    {
+        lg2::error("Failed to call GetManagedObjects: {ERR}", "ERR", e.what());
+    }
+
+    return ifnames;
+}
 
 } // namespace lldp
 } // namespace network
