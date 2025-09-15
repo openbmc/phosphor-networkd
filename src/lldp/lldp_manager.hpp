af426452073829327baf8640018b152099ce598b
Asmitha Karunanithi <asmitk01@in.ibm.com>
2025-09-15 13:14:30 -0500

    LLDP: Add ethernet interface dbus objects
    This commit parses the availableinterfaces exposed by networkd and
    create corresponding LLDP DBus objects under
    /xyz/openbmc_project/network/lldp/<interface>.
    
    This ensures LLDP objects are always in sync with the interfaces
    managed by networkd.
    
    Tested By:
    Verified ethernet object creation on bmc under LLDP service:
    '''
    busctl tree xyz.openbmc_project.LLDP
    └─ /xyz
      └─ /xyz/openbmc_project
        └─ /xyz/openbmc_project/network
          └─ /xyz/openbmc_project/network/lldp
            ├─ /xyz/openbmc_project/network/lldp/eth0
            └─ /xyz/openbmc_project/network/lldp/eth1
    '''
    
    Change-Id: Idbd0e2edb1d1079cb5a0ee358bdedef7e9f0a815
    Signed-off-by: Asmitha Karunanithi <asmitk01@in.ibm.com>

--- src/lldp/lldp_manager.hpp
+++ src/lldp/lldp_manager.hpp
@@ -1,6 +1,15 @@
 #pragma once
+
+#include "lldp_interface.hpp"
+
 #include <sdbusplus/bus.hpp>
 #include <sdeventplus/event.hpp>
+#include <sdeventplus/utility/timer.hpp>
+
+#include <map>
+#include <string>
+#include <unordered_map>
+#include <vector>
 
 namespace phosphor
 {
@@ -8,6 +17,10 @@ namespace network
 {
 namespace lldp
 {
+class Interface;
+
+using TimerType =
+        sdeventplus::utility::Timer<sdeventplus::ClockId::Monotonic>;
 
 class LLDPManager
 {
@@ -17,9 +30,16 @@ class LLDPManager
     static constexpr std::string_view objPath =
         "/xyz/openbmc_project/network/lldp";
 
+    std::vector<std::string> getInterfaces();
+
   protected:
+    void createIntfDbusObjects();
+
     sdbusplus::bus_t& bus;
     sdeventplus::Event& event;
+    std::unique_ptr<TimerType> createIntfTimer;
+
+    std::unordered_map<std::string, std::unique_ptr<Interface>> ifaces;
 };
 
 } // namespace lldp
