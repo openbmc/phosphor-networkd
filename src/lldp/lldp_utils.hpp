af46196e7a052e81132d0769ca63bdf7e9111a29
Asmitha Karunanithi <asmitk01@in.ibm.com>
2025-09-29 09:50:09 -0500

    Update Ethernet objects to implement Settings Intf
    The ethernet objects created under lldp service will now implement the
    Settings interface which provides a property to enable or disable LLDP
    on each Ethernet interface.
    
    On service startup, when the ethernet dbus object is created, the lldp
    config file is parsed and EnableLLDP property is initialized
    accordingly. Users can also update this property at runtime, and the
    changes are persisted to the config file and the service is restarted
    to apply them.
    
    Tested By:
    
    '''
    busctl introspect xyz.openbmc_project.LLDP /xyz/openbmc_project/network/lldp/eth1
    xyz.openbmc_project.Network.LLDP.Settings interface -         -            -
    .EnableLLDP                               property  b         false        emits-change writable
    '''
    
    Change-Id: Iadc0f8762a99ad1fd87a21753c925f520c842880
    Signed-off-by: Asmitha Karunanithi <asmitk01@in.ibm.com>

---
+++ src/lldp/lldp_utils.hpp
@@ -0,0 +1,32 @@
+#pragma once
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+namespace phosphor
+{
+namespace lldp_utils
+{
+
+using ConfigEntry = std::vector<std::string>;
+using ConfigList = std::vector<ConfigEntry>;
+
+class LLDPUtils
+{
+  public:
+    // Parse .conf files into vector of vector<string>
+    static ConfigList deserialize(const std::string& filepath);
+
+    // Write vector of vector<string> into .conf file
+    static void serialize(const std::string& filepathStr,
+                          const ConfigList& config);
+
+    static ConfigList parseAllConfigs(const std::string& globalPath,
+                                      const std::string& dirPath);
+};
+
+} // namespace lldp_utils
+} // namespace phosphor
