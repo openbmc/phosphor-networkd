9971cfab9b8f6e94fd14e9bfb137993d3d4b8841
Ethan Wei <ethanwei@ami.com>
2025-10-28 10:25:55 +0800

    Adding channel specific privilege to network
      - Adding the channel access information to the network
        interface object. This privilege will be used in
        channel specific authorization.
      - Get supported priv from user manager service dynamically.
      - Signal handling for capturing the supported priv list
        changes from user management.
    
    Tested-by:
    Verified channel access through ipmitool get/set channel
    access command
    
    '''
      $ ipmitool -H <IP> -U root -P 0penBmc -I lanplus -C 17 raw 0x06 0x40 0x01 0x52 0x42
    
      $ ipmitool -H <IP> -U root -P 0penBmc -I lanplus -C 17 raw 0x06 0x41 0x01 0x40
       12 02
      $ ipmitool -H <IP> -U root -P 0penBmc -I lanplus -C 17 raw 0x06 0x40 0x01 0x52 0x43
    
      $ ipmitool -H <IP> -U root -P 0penBmc -I lanplus -C 17 raw 0x06 0x41 0x01 0x40
       12 03
      $ ipmitool -H <IP> -U root -P 0penBmc -I lanplus -C 17 raw 0x06 0x40 0x01 0x52 0x44
    
      $ ipmitool -H <IP> -U root -P 0penBmc -I lanplus -C 17 raw 0x06 0x41 0x01 0x40
       12 04
    '''
    Change-Id: Ifca86faf2a946a2b3f5867270c0f31617dc5a3ef
    Signed-off-by: Ethan Wei <ethanwei@ami.com>

--- src/network_manager.hpp
+++ src/network_manager.hpp
@@ -129,6 +129,12 @@ class Manager : public ManagerIface
         reloadPostHooks.push_back(std::move(hook));
     }
 
+    /** supported privilege list **/
+    std::vector<std::string> supportedPrivList;
+
+    /** @brief initializes the supportedPrivilege List */
+    void initSupportedPrivilges();
+
   protected:
     /** @brief Handle to the object used to trigger reloads of networkd. */
     stdplus::PinnedRef<DelayedExecutor> reload;
@@ -167,6 +173,9 @@ class Manager : public ManagerIface
 
     /** @brief Creates the interface in the maps */
     void createInterface(const AllIntfInfo& info, bool enabled);
+
+    /** Get the user management service name dynamically **/
+    std::string getUserServiceName();
 };
 
 } // namespace network
