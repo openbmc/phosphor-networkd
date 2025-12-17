f02fc4a5adaf8c9d1b8e45980b50c12174d23bb4
Ravi Teja <raviteja28031990@gmail.com>
2025-12-17 00:59:21 -0600

    Remove IPv4 Linklocal address when interface has static IP
    This commit removes IPv4 linklocal IP when ethernet interface already
    has routable ip address assigned on the interface.
    
    Keeping linklocal ip address along with static ip addresses causing
    network routing issues.
    
    systemd-networkd must drop linklocal IP when interface has routable
    static IP address, but currently its not implemented.
    Here is systemd issue systemd/systemd#25424
    
    Tested By:
    Verified both interfaces in different static subnets
    Change-Id: I908cde6e695a0c72fdf80b706150ee3d654e5e42
    Signed-off-by: Ravi Teja <raviteja28031990@gmail.com>

--- src/system_queries.hpp
+++ src/system_queries.hpp
@@ -22,4 +22,7 @@ void setNICUp(std::string_view ifname, bool up);
 
 void deleteIntf(unsigned idx);
 
+bool deleteLinkLocalIPv4ViaNetlink(unsigned ifidx,
+                                   const stdplus::SubnetAny& ip);
+
 } // namespace phosphor::network::system
